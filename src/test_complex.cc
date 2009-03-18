f5d3a2da860f8b60f8aaf7cfca822e15d0b66d06
Rhys Ulerich <rhys@ices.utexas.edu>
2009-03-18 17:59:15 +0000

    Memory layout test added
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@1816 0bd85f0b-c151-43b9-a1e9-27231104551f

--- src/test_complex.cc
+++ src/test_complex.cc
@@ -6,6 +6,7 @@
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/test/included/unit_test.hpp>
+#include <boost/foreach.hpp>
 #include <complex>
 #include <log4cxx/logger.h>
 #include <typeinfo>
@@ -16,8 +17,32 @@ namespace ublas = boost::numeric::ublas;
 
 BOOST_AUTO_TEST_CASE( check_complex_double_is_two_doubles )
 {
-  BOOST_CHECK_EQUAL(
-    sizeof(std::complex<double>), 2*sizeof(double) );
+  BOOST_REQUIRE_EQUAL( sizeof(std::complex<double>), 2*sizeof(double) );
+}
+
+BOOST_AUTO_TEST_CASE( shared_c_array )
+{
+  const std::size_t N = 6;
+  double carray_double[N] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
+  std::complex<double> *carray_complex
+    = reinterpret_cast<std::complex<double> *>(carray_double);
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      BOOST_CHECK_EQUAL(carray_double[2*i],   carray_complex[i].real());
+      BOOST_CHECK_EQUAL(carray_double[2*i+1], carray_complex[i].imag());
+    }
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      carray_complex[i] += carray_complex[1];
+    }
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      BOOST_CHECK_EQUAL(carray_double[2*i],   carray_complex[i].real());
+      BOOST_CHECK_EQUAL(carray_double[2*i+1], carray_complex[i].imag());
+    }
 }
 
 BOOST_AUTO_TEST_CASE( shallow_array_adaptor )
@@ -43,7 +68,7 @@ BOOST_AUTO_TEST_CASE( shallow_array_adaptor_complex )
   std::complex<double> raw[N];
 
   ublas::shallow_array_adaptor<std::complex<double> > adaptor(N, raw);
-  ublas::vector<std::complex<double>, 
+  ublas::vector<std::complex<double>,
     ublas::shallow_array_adaptor<std::complex<double> > > vec(N, adaptor);
 
   BOOST_CHECK_EQUAL( vec.size(), N );
