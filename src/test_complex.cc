bf835fc2d8fac3d0a749067201cbf246b1d103bd
Rhys Ulerich <rhys@ices.utexas.edu>
2009-03-18 19:01:25 +0000

    Completed overlap tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@1817 0bd85f0b-c151-43b9-a1e9-27231104551f

--- src/test_complex.cc
+++ src/test_complex.cc
@@ -8,24 +8,25 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/foreach.hpp>
 #include <complex>
-#include <log4cxx/logger.h>
 #include <typeinfo>
-
-log4cxx::LoggerPtr logger = log4cxx::Logger::getRootLogger();
 
 namespace ublas = boost::numeric::ublas;
 
-BOOST_AUTO_TEST_CASE( check_complex_double_is_two_doubles )
-{
-  BOOST_REQUIRE_EQUAL( sizeof(std::complex<double>), 2*sizeof(double) );
-}
+typedef std::complex<double>                            complex;
+typedef ublas::shallow_array_adaptor<double>            shallow_adaptor_double;
+typedef ublas::vector<double, shallow_adaptor_double>   shallow_vector_double;
+typedef ublas::shallow_array_adaptor<complex>           shallow_adaptor_complex;
+typedef ublas::vector<complex, shallow_adaptor_complex> shallow_vector_complex;
 
+// Ensure we can use std::complex<double> as two consecutive doubles
 BOOST_AUTO_TEST_CASE( shared_c_array )
 {
+  // Assumption must hold true for any of this scheme to work
+  BOOST_REQUIRE_EQUAL( sizeof(complex), 2*sizeof(double) );
+
   const std::size_t N = 6;
   double carray_double[N] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
-  std::complex<double> *carray_complex
-    = reinterpret_cast<std::complex<double> *>(carray_double);
+  complex *carray_complex = reinterpret_cast<complex *>(carray_double);
 
   for (std::size_t i = 0; i < N/2; ++i)
     {
@@ -35,7 +36,7 @@ BOOST_AUTO_TEST_CASE( shared_c_array )
 
   for (std::size_t i = 0; i < N/2; ++i)
     {
-      carray_complex[i] += carray_complex[1];
+      carray_complex[i] += carray_complex[0];
     }
 
   for (std::size_t i = 0; i < N/2; ++i)
@@ -45,13 +46,14 @@ BOOST_AUTO_TEST_CASE( shared_c_array )
     }
 }
 
-BOOST_AUTO_TEST_CASE( shallow_array_adaptor )
+// Ensure we can use a C-array of doubles as a ublas::vector.
+BOOST_AUTO_TEST_CASE( shallow_array_adaptor_double )
 {
   const std::size_t N = 3;
   double raw[N];
 
-  ublas::shallow_array_adaptor<double> adaptor(N, raw);
-  ublas::vector<double, ublas::shallow_array_adaptor<double> > vec(N, adaptor);
+  shallow_adaptor_double adaptor(N, raw);
+  shallow_vector_double  vec(N, adaptor);
 
   BOOST_CHECK_EQUAL( vec.size(), N );
 
@@ -62,20 +64,62 @@ BOOST_AUTO_TEST_CASE( shallow_array_adaptor )
   BOOST_CHECK_EQUAL_COLLECTIONS(&raw[0], &raw[N], vec.begin(), vec.end());
 }
 
+// Ensure we can use a C-array of std::complex<double> as a ublas::vector.
 BOOST_AUTO_TEST_CASE( shallow_array_adaptor_complex )
 {
   const std::size_t N = 3;
-  std::complex<double> raw[N];
+  complex raw[N];
 
-  ublas::shallow_array_adaptor<std::complex<double> > adaptor(N, raw);
-  ublas::vector<std::complex<double>,
-    ublas::shallow_array_adaptor<std::complex<double> > > vec(N, adaptor);
+  shallow_adaptor_complex adaptor(N, raw);
+  shallow_vector_complex  vec(N, adaptor);
 
   BOOST_CHECK_EQUAL( vec.size(), N );
 
-  std::fill(&raw[0], &raw[N], std::complex<double>(1.0,-1.0));
+  std::fill(&raw[0], &raw[N], complex(1.0, -1.0));
   BOOST_CHECK_EQUAL_COLLECTIONS(&raw[0], &raw[N], vec.begin(), vec.end());
 
-  std::fill(vec.begin(), vec.end(), std::complex<double>(2.0,-2.0));
+  std::fill(vec.begin(), vec.end(), complex(2.0, -2.0));
   BOOST_CHECK_EQUAL_COLLECTIONS(&raw[0], &raw[N], vec.begin(), vec.end());
+}
+
+// Ensure we can use the same data as either double or complex ublas::vector
+BOOST_AUTO_TEST_CASE( shallow_array_adaptor_shared )
+{
+  const std::size_t N = 6;
+  double carray_double[N] = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
+  complex *carray_complex = reinterpret_cast<complex *>(carray_double);
+
+  shallow_adaptor_double adaptor_double(N, carray_double);
+  shallow_vector_double  vec_double(N, adaptor_double);
+
+  shallow_adaptor_complex adaptor_complex(N, carray_complex);
+  shallow_vector_complex  vec_complex(N, adaptor_complex);
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      BOOST_CHECK_EQUAL(vec_double[2*i],   vec_complex[i].real());
+      BOOST_CHECK_EQUAL(vec_double[2*i+1], vec_complex[i].imag());
+    }
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      vec_complex[i] += vec_complex[0];
+    }
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      BOOST_CHECK_EQUAL(vec_double[2*i],   vec_complex[i].real());
+      BOOST_CHECK_EQUAL(vec_double[2*i+1], vec_complex[i].imag());
+    }
+
+  for (std::size_t i = 0; i < N; ++i)
+    {
+      vec_double[i] += vec_double[N-1];
+    }
+
+  for (std::size_t i = 0; i < N/2; ++i)
+    {
+      BOOST_CHECK_EQUAL(vec_double[2*i],   vec_complex[i].real());
+      BOOST_CHECK_EQUAL(vec_double[2*i+1], vec_complex[i].imag());
+    }
 }
