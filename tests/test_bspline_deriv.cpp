db247aba5489584f5600fd52cdda95a4a30a57ef
Rhys Ulerich <rhys@ices.utexas.edu>
2009-06-14 17:20:05 +0000

    Update test code to reflect submission to GSL project
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@3392 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bspline_deriv.cpp
+++ tests/test_bspline_deriv.cpp
@@ -62,64 +62,64 @@ gsl_bspline_basis_product(const double x,
     return GSL_SUCCESS;
 }
 
-int
-gsl_bspline_greville_abscissae(gsl_bspline_workspace *w, gsl_vector *abscissae)
+/* Return the number of Greville abscissae for this basis */
+// Contributed to gsl-discuss@sourceware.org
+size_t
+gsl_bspline_greville_nabscissae(gsl_bspline_workspace *w)
 {
-    const size_t k      = gsl_bspline_order(w);
-    const size_t n      = gsl_bspline_ncoeffs(w);
-    const size_t np1    = n + 1;
-    double * data       = w->knots->data;
-    const size_t stride = w->knots->stride;
-    size_t i;
+  return w->knots->size - w->km1;
+}
 
-    if (abscissae->size < np1) {
-        GSL_ERROR("abscissae->size too small for number of basis functions",
-                  GSL_EINVAL);
+/* Return the location of the i-th Greville abscissa */
+// Contributed to gsl-discuss@sourceware.org
+double
+gsl_bspline_greville_abscissa(size_t i, gsl_bspline_workspace *w)
+{
+#if GSL_RANGE_CHECK
+  if (GSL_RANGE_COND(i >= gsl_bspline_greville_nabscissae(w)))
+    {
+      GSL_ERROR_VAL ("Greville abscissa index out of range", GSL_EINVAL, 0);
     }
+#endif
+  const size_t stride = w->knots->stride;
+  const double * data = w->knots->data + i*stride;
 
-    for (i = 0; i < np1; ++i) {
-        gsl_vector_set(abscissae, i, gsl_stats_mean(data, stride, k));
-        data += stride;
-    }
-
-    return GSL_SUCCESS;
+  return gsl_stats_mean(data, stride, w->k);
 }
 
 BOOST_AUTO_TEST_CASE( main_test )
 {
-/*
     // Greville abscissa example taken from R. W. Johnson paper
     // Applied Numerical Mathematics vol 52 (2005) pages 63--75
-    const size_t k = 4;
+/*
+    const size_t k = 3;
     const double bpoint_data[]    = { 0.0, 0.2, 0.5, 0.75, 1.0 };
 */
-/*
-    const size_t k = 1;
+    const size_t k = 2;
     const double bpoint_data[]    = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
-*/
 
+/*
     const size_t k = 3;
     const double bpoint_data[]    = { 0.0, 2.0, 4.0 };
+*/
+
     const size_t nbreak           = sizeof(bpoint_data)/sizeof(bpoint_data[0]);
     gsl_vector_const_view bpoints = gsl_vector_const_view_array(bpoint_data, nbreak);
-
     gsl_bspline_workspace *bw = gsl_bspline_alloc(k, nbreak);
     gsl_bspline_deriv_workspace *bdw = gsl_bspline_deriv_alloc(k);
     gsl_bspline_knots((const gsl_vector *) &bpoints, bw);
 
     if (logger->isDebugEnabled()) {
         for (size_t i = 0; i < bw->knots->size; ++i) {
-            LOG4CXX_DEBUG(logger, "knot[" << i << "] at " 
+            LOG4CXX_DEBUG(logger, "knot[" << i << "] = " 
                                   << gsl_vector_get(bw->knots, i));
         }
     }
 
-    gsl_vector *abscissae = gsl_vector_alloc(gsl_bspline_ncoeffs(bw)+1);
-    gsl_bspline_greville_abscissae(bw, abscissae);
     if (logger->isDebugEnabled()) {
-        for (size_t i = 0; i < abscissae->size; ++i) {
-            LOG4CXX_DEBUG(logger, "abscissa[" << i << "] at " 
-                                  << gsl_vector_get(abscissae, i));
+        for (size_t i = 0; i < gsl_bspline_greville_nabscissae(bw); ++i) {
+            LOG4CXX_DEBUG(logger, "abscissa[" << i << "] = " 
+                                  << gsl_bspline_greville_abscissa(i, bw));
         }
     }
 
@@ -127,7 +127,7 @@ BOOST_AUTO_TEST_CASE( main_test )
     gsl_matrix *dB = gsl_matrix_alloc(k, 2);
 
     for (size_t i = 0; i < A->size1; ++i) {
-        double x = gsl_vector_get(abscissae, i);
+        double x = gsl_bspline_greville_abscissa(i, bw);
         for (size_t j = 0; j < A->size2; ++j) {
             double result = 0.0;
             if (abs(i - j) < k) {
@@ -140,7 +140,6 @@ BOOST_AUTO_TEST_CASE( main_test )
         printf("\n");
     }
 
-    gsl_vector_free(abscissae);
     gsl_matrix_free(dB);
     gsl_matrix_free(A);
     gsl_bspline_deriv_free(bdw);
