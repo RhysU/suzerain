91d563736dbf6057fec59ca1f635b9e4c2cd77ce
Rhys Ulerich <rhys@ices.utexas.edu>
2009-06-15 04:06:25 +0000

    Getting there, slowly
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@3400 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bspline_deriv.cpp
+++ tests/test_bspline_deriv.cpp
@@ -20,48 +20,6 @@ using namespace log4cxx;
 
 LoggerPtr logger = Logger::getRootLogger();
 
-int
-gsl_bspline_basis_product(const double x,
-                          const size_t i_index,
-                          const size_t i_nderiv,
-                          const size_t j_index,
-                          const size_t j_nderiv,
-                          double * result,
-                          gsl_matrix * dB,
-                          gsl_bspline_workspace * w,
-                          gsl_bspline_deriv_workspace * dw) {
-
-    const size_t nderiv = (i_nderiv > j_nderiv) ? i_nderiv : j_nderiv;
-    size_t nonzero_start, nonzero_end;
-    double i_value = 0.0, j_value = 0.0;
-
-    if (dB->size1 < gsl_bspline_order(w)) {
-        GSL_ERROR("dB->size1 too small for given B-spline order",
-                  GSL_EINVAL);
-    }
-
-    if (dB->size2 < nderiv + 1) {
-        GSL_ERROR("dB->size2 too small for number of requested derivatives",
-                  GSL_EINVAL);
-    }
-
-    gsl_bspline_deriv_eval_nonzero(x, nderiv, dB,
-                                   &nonzero_start, &nonzero_end,
-                                   w, dw);
-
-    if (i_index >= nonzero_start && i_index <= nonzero_end) {
-        i_value = gsl_matrix_get(dB, i_index - nonzero_start, i_nderiv);
-    }
-
-    if (j_index >= nonzero_start && j_index <= nonzero_end) {
-        j_value = gsl_matrix_get(dB, j_index - nonzero_start, j_nderiv);
-    }
-
-    *result = i_value * j_value;
-
-    return GSL_SUCCESS;
-}
-
 /* Return the number of Greville abscissae for this basis */
 // Contributed to gsl-discuss@sourceware.org
 size_t
@@ -89,19 +47,16 @@ gsl_bspline_greville_abscissa(size_t i, gsl_bspline_workspace *w)
 
 BOOST_AUTO_TEST_CASE( main_test )
 {
-    // Greville abscissa example taken from R. W. Johnson paper
-    // Applied Numerical Mathematics vol 52 (2005) pages 63--75
 /*
     const size_t k = 3;
     const double bpoint_data[]    = { 0.0, 0.2, 0.5, 0.75, 1.0 };
 */
-    const size_t k = 2;
-    const double bpoint_data[]    = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0 };
-
 /*
     const size_t k = 3;
     const double bpoint_data[]    = { 0.0, 2.0, 4.0 };
 */
+    const size_t k = 4;
+    const double bpoint_data[]    = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0 };
 
     const size_t nbreak           = sizeof(bpoint_data)/sizeof(bpoint_data[0]);
     gsl_vector_const_view bpoints = gsl_vector_const_view_array(bpoint_data, nbreak);
@@ -123,25 +78,31 @@ BOOST_AUTO_TEST_CASE( main_test )
         }
     }
 
-    gsl_matrix *A = gsl_matrix_alloc(gsl_bspline_ncoeffs(bw), gsl_bspline_ncoeffs(bw));
-    gsl_matrix *dB = gsl_matrix_alloc(k, 2);
+    const size_t nderiv = 2;
+    gsl_matrix *M  = gsl_matrix_alloc(gsl_bspline_ncoeffs(bw), gsl_bspline_ncoeffs(bw));
+    gsl_matrix *D1 = gsl_matrix_alloc(gsl_bspline_ncoeffs(bw), gsl_bspline_ncoeffs(bw));
+    gsl_matrix *D2 = gsl_matrix_alloc(gsl_bspline_ncoeffs(bw), gsl_bspline_ncoeffs(bw));
+    gsl_matrix *dB = gsl_matrix_alloc(gsl_bspline_ncoeffs(bw), nderiv+1);
 
-    for (size_t i = 0; i < A->size1; ++i) {
-        double x = gsl_bspline_greville_abscissa(i, bw);
-        for (size_t j = 0; j < A->size2; ++j) {
-            double result = 0.0;
-            if (abs(i - j) < k) {
-                gsl_bspline_basis_product(x, i, 0, j, 0, &result, dB, bw, bdw);
-            }
-            gsl_matrix_set(A, i, j, result);
+    for (size_t j = 0; j < M->size1; ++j) {
+        double x = gsl_bspline_greville_abscissa(j, bw);
+        gsl_bspline_deriv_eval(x, nderiv, dB, bw, bdw);
+        gsl_matrix_set_col(M,  j, (const gsl_vector *) &gsl_matrix_column(dB, 0));
+        gsl_matrix_set_col(D1, j, (const gsl_vector *) &gsl_matrix_column(dB, 1));
+        gsl_matrix_set_col(D2, j, (const gsl_vector *) &gsl_matrix_column(dB, 2));
+    }
 
-            printf(" %10g", result);
+    for (size_t i = 0; i < M->size1; ++i) {
+        for (size_t j = 0; j < M->size2; ++j) {
+            printf(" %10g", gsl_matrix_get(M, i, j));
         }
         printf("\n");
     }
 
     gsl_matrix_free(dB);
-    gsl_matrix_free(A);
+    gsl_matrix_free(M);
+    gsl_matrix_free(D1);
+    gsl_matrix_free(D2);
     gsl_bspline_deriv_free(bdw);
     gsl_bspline_free(bw);
 }
