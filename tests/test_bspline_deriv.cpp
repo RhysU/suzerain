6bdcb777e9f173e16053ae11d795663dafa530f5
Rhys Ulerich <rhys@ices.utexas.edu>
2009-06-21 00:35:42 +0000

    Working through collocation derivative tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@3462 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bspline_deriv.cpp
+++ tests/test_bspline_deriv.cpp
@@ -7,9 +7,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <gsl/gsl_bspline.h>
+#include <gsl/gsl_blas.h>
 #include <gsl/gsl_errno.h>
+#include <gsl/gsl_linalg.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_permutation.h>
 #include <gsl/gsl_statistics.h>
 #include <gsl/gsl_vector.h>
 #include <log4cxx/logger.h>
@@ -17,8 +20,19 @@
 #include <boost/test/included/unit_test.hpp>
 
 using namespace log4cxx;
+using boost::shared_ptr;
 
 LoggerPtr logger = Logger::getRootLogger();
+
+void print_gsl_matrix(FILE * stream, gsl_matrix * m)
+{
+    for (size_t i = 0; i < m->size1; ++i) {
+        for (size_t j = 0; j < m->size2; ++j) {
+            fprintf(stream, " %10g", gsl_matrix_get(m, i, j));
+        }
+        printf("\n");
+    }
+}
 
 BOOST_AUTO_TEST_CASE( main_test )
 {
@@ -30,8 +44,8 @@ BOOST_AUTO_TEST_CASE( main_test )
     const size_t k = 3;
     const double bpoint_data[]    = { 0.0, 2.0, 4.0 };
 */
-    const size_t k = 4;
-    const double bpoint_data[]    = { 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0 };
+    const size_t k = 2;
+    const double bpoint_data[]    = { 0.0, 1.0, 2.0, 3.0 };
 
     const size_t nbreak = sizeof(bpoint_data)/sizeof(bpoint_data[0]);
     gsl_vector_const_view bpoints = gsl_vector_const_view_array(bpoint_data, nbreak);
@@ -68,14 +82,76 @@ BOOST_AUTO_TEST_CASE( main_test )
         gsl_matrix_set_col(D2, j, (const gsl_vector *) &gsl_matrix_column(dB, 2));
     }
 
-    for (size_t i = 0; i < M->size1; ++i) {
-        for (size_t j = 0; j < M->size2; ++j) {
-            printf(" %10g", gsl_matrix_get(M, i, j));
+    LOG4CXX_INFO(logger, "M follows... ");
+    print_gsl_matrix(stdout, M);
+
+    LOG4CXX_INFO(logger, "D1 follows... ");
+    print_gsl_matrix(stdout, D1);
+
+    int signum;
+    gsl_permutation *p = gsl_permutation_calloc(M->size1);
+    gsl_matrix *MLU    = gsl_matrix_alloc(M->size1, M->size2);
+    gsl_matrix_memcpy(MLU, M);
+    gsl_linalg_LU_decomp(MLU, p, &signum);
+    LOG4CXX_INFO(logger, "MLU follows... ");
+    print_gsl_matrix(stdout, MLU);
+
+    {
+        // Vector of function coefficients
+        const int i = 0;
+        gsl_vector * c  = gsl_vector_calloc(ncoeff);
+        gsl_vector_set(c, i, 1.0);
+
+        // Form rhs = D1 c
+        gsl_vector * rhs  = gsl_vector_calloc(ncoeff);
+        gsl_blas_dgemv(CblasNoTrans, 1.0, D1, c, 0.0, rhs);
+        // Solve MLU dc = rhs
+        gsl_vector *dc = gsl_vector_alloc(ncoeff);
+        gsl_linalg_LU_solve(MLU, p, rhs, dc);
+
+        if (logger->isDebugEnabled()) {
+            for (size_t i = 0; i < dc->size; ++i) {
+                LOG4CXX_DEBUG(logger, "dc[" << i << "] = "
+                                      << gsl_vector_get(dc, i));
+            }
         }
-        printf("\n");
+
+        // Check residual for M dc - D1 c = 0
+        gsl_vector * residual = gsl_vector_alloc(ncoeff);
+        gsl_blas_dgemv(CblasNoTrans, -1.0, D1, c, 0.0, residual);
+        gsl_blas_dgemv(CblasNoTrans,  1.0, M, dc, 1.0, residual);
+        if (logger->isDebugEnabled()) {
+            for (size_t i = 0; i < residual->size; ++i) {
+                LOG4CXX_DEBUG(logger, "residual[" << i << "] = "
+                                      << gsl_vector_get(residual, i));
+            }
+        }
+        gsl_vector_free(residual);
+
+        const int nsample = 5;
+        const size_t nderiv = 2;
+        for (size_t i = 0; i < nbreak-1; ++i) {
+            for (size_t j = 0; j < nsample; ++j) {
+                double direct_d1, collocation_d1;
+                const double x = bpoint_data[i]
+                    + j/((double)nsample)*(bpoint_data[i+1]-bpoint_data[i]);
+                gsl_bspline_deriv_eval(x, nderiv, dB, bw, bdw);
+                gsl_blas_ddot(c,  (const gsl_vector *) &gsl_matrix_column(dB, 1),
+                              &direct_d1);
+                gsl_blas_ddot(dc, (const gsl_vector *) &gsl_matrix_column(dB, 0),
+                              &collocation_d1);
+                LOG4CXX_DEBUG(logger, "At x = " << x << " direct (" << direct_d1 << ") versus collocation (" << collocation_d1 << ") gives error " << direct_d1 - collocation_d1);
+            }
+        }
+
+        gsl_vector_free(c);
+        gsl_vector_free(dc);
+        gsl_vector_free(rhs);
     }
 
     gsl_matrix_free(dB);
+    gsl_matrix_free(MLU);
+    gsl_permutation_free(p);
     gsl_matrix_free(M);
     gsl_matrix_free(D1);
     gsl_matrix_free(D2);
