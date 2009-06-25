e59282c5e8dda9ccf9df65c0046ef2860e31381e
Rhys Ulerich <rhys@ices.utexas.edu>
2009-06-25 19:59:32 +0000

    Added memory layout unit test
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@3511 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bspline_operator.cpp
+++ tests/test_bspline_operator.cpp
@@ -21,18 +21,49 @@ BOOST_AUTO_TEST_CASE( allocation_okay )
 }
 
 
+// Check a simple piecewise linear case's general banded storage
 BOOST_AUTO_TEST_CASE( memory_layout )
 {
-    const double breakpoints[] = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+    const double breakpoints[] = { 0.0, 1.0, 2.0, 3.0 };
     const int nbreak = sizeof(breakpoints)/sizeof(breakpoints[0]);
-    const int order  = 3;
-    const int nderiv = 2;
+    const int order  = 2;
+    const int nderiv = 1;
 
     suzerain_bspline_operator_workspace * w
         = suzerain_bspline_operator_alloc(order, nbreak, nderiv,
             SUZERAIN_BSPLINE_OPERATOR_COLLOCATION_GREVILLE);
 
     suzerain_bspline_operator_create(breakpoints, w);
+
+    /* Check w->D[0], the mass matrix, against known good solution:
+     *   1              0              0              0
+     *   0              1              0              0
+     *   0              0              1              0
+     *   0              0              0              1
+     * Known good is in general banded matrix column-major order.
+     */
+    const double good_D0[] = { /*DK*/   1,     0,
+                                   0,   1,     0,
+                                   0,   1,     0,
+                                   0,   1  /*DK*/ };
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+        good_D0, good_D0 + sizeof(good_D0)/sizeof(good_D0[0]),
+        w->D[0] + 1, w->D[0] + w->storagesize - 1);
+
+    /* Check w->D[1], the first derivative matrix, against known good:
+     *       -1              1              0              0
+     *        0             -1              1              0
+     *        0              0             -1              1
+     *        0              0             -1              1
+     * Known good is in general banded matrix column-major order.
+     */
+    const double good_D1[] = { /*DK*/  -1,     0,
+                                   1,  -1,     0,
+                                   1,  -1,    -1,
+                                   1,   1  /*DK*/ };
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+        good_D1, good_D1 + sizeof(good_D1)/sizeof(good_D1[0]),
+        w->D[1] + 1, w->D[1] + w->storagesize - 1);
 
     suzerain_bspline_operator_free(w);
 }
