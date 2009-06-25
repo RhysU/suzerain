d941b181abc60acfc693a41f9c7ce5c8fc006b7e
Rhys Ulerich <rhys@ices.utexas.edu>
2009-06-25 00:45:44 +0000

    Removed stale logging
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@3507 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bspline_operator.cpp
+++ tests/test_bspline_operator.cpp
@@ -28,17 +28,9 @@ BOOST_AUTO_TEST_CASE( memory_layout )
     const int order  = 3;
     const int nderiv = 2;
 
-    LOG4CXX_DEBUG(logger, format("nbreak=%d, order=%d, nderiv=%d")
-                          % nbreak % order % nderiv);
-
     suzerain_bspline_operator_workspace * w
         = suzerain_bspline_operator_alloc(order, nbreak, nderiv,
             SUZERAIN_BSPLINE_OPERATOR_COLLOCATION_GREVILLE);
-
-    LOG4CXX_DEBUG(logger, format("w->nbreakpoints=%d") % w->nbreakpoints);
-    LOG4CXX_DEBUG(logger, format("w->order       =%d") % w->order);
-    LOG4CXX_DEBUG(logger, format("w->nderivatives=%d") % w->nderivatives);
-    LOG4CXX_DEBUG(logger, format("w->n           =%d") % w->n);
 
     suzerain_bspline_operator_create(breakpoints, w);
 
