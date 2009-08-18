8dc5aa8f2e914a5946e7e10fec641822ec609d12
Rhys Ulerich <rhys@ices.utexas.edu>
2009-08-18 20:10:37 +0000

    Added build skeleton for the SMR91 time stepper
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@4328 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/timestepper.h
@@ -0,0 +1,60 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2009 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * timestepper.h: SMR91 timestepping implementation
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+#ifndef PECOS_SUZERAIN_TIMESTEPPER_H
+#define PECOS_SUZERAIN_TIMESTEPPER_H
+
+/** @file
+ * FIXME
+ */
+
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+/** Marks beginning of public declarations using C linkage for C++ compiler */
+# define __BEGIN_DECLS extern "C" {
+/** Marks ending of public declarations using C linkage for C++ compiler */
+# define __END_DECLS }
+#else
+/** Marks beginning of public declarations for C compiler */
+# define __BEGIN_DECLS /* empty */
+/** Marks ending of public declarations for C compiler */
+# define __END_DECLS /* empty */
+#endif
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+__BEGIN_DECLS
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+__END_DECLS
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#endif // PECOS_SUZERAIN_TIMESTEPPER_H
