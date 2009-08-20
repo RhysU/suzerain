a15f39154f24407ba3201075c8d0553723bdc5ec
Rhys Ulerich <rhys@ices.utexas.edu>
2009-08-20 20:06:06 +0000

    Add build and test scaffolding for generalized Richardson extrapolation routines
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@4408 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/richardson.h
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2009 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * richardson.h: generalized Richardson extrapolation routines
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_RICHARDSON_H__
+#define __SUZERAIN_RICHARDSON_H__
+
+/** @file
+ * Provides generalized Richardson extrapolation built atop GSL.
+ */
+
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+/** Marks beginning of public declarations using C linkage for C++ compiler */
+# define __BEGIN_DECLS extern "C" {
+/** Marks ending of public declarations using C linkage for C++ compiler */
+# define __END_DECLS }
+#else
+/** Marks beginning of public declarations for C compiler */
+# define __BEGIN_DECLS /* empty */
+/** Marks ending of public declarations for C compiler */
+# define __END_DECLS /* empty */
+#endif
+
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+__END_DECLS
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#endif /* __SUZERAIN_RICHARDSON_H__ */
---
+++ tests/test_richardson.c
@@ -0,0 +1,15 @@
+#include <suzerain/config.h>
+
+#include <stdlib.h>
+#include <gsl/gsl_ieee_utils.h>
+#include <gsl/gsl_test.h>
+#include <suzerain/richardson.h>
+
+
+int
+main(int argc, char **argv)
+{
+    gsl_ieee_env_setup();
+
+    exit(gsl_test_summary());
+}
