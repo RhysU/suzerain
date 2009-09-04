7a904ca2401b0e3adb57f07c1a60b0036424d7a2
Rhys Ulerich <rhys@ices.utexas.edu>
2009-09-04 21:43:22 +0000

    Added header for derived classical quantity computation
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@4653 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/classical.hpp
@@ -0,0 +1,43 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2009 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * classical.hpp: Obtains classical state from conservative variables
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+#ifndef PECOS_SUZERAIN_CLASSICAL_H
+#define PECOS_SUZERAIN_CLASSICAL_H
+
+#include <Eigen/Core>
+
+/** @file
+ * Provides routines that obtain derived classical state expressions (based on
+ * \f$p\f$, \f$T\f$, \f$\vec{u}\f$, \f$e\f$) from conservative state quantities
+ * (based on \f$\rho\f$, \f$\vec{m}$, \f$e\f$) using the ideal gas equations of
+ * state.
+ */
+
+
+#endif // PECOS_SUZERAIN_CLASSICAL_H
