2479a34eb6e8a4ba3fdb1bae3eabe443c77906f4
Rhys Ulerich <rhys@ices.utexas.edu>
2009-09-05 00:31:04 +0000

    Added p_T_mu_lambda test case
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@4662 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_classical.cpp
+++ tests/test_classical.cpp
@@ -2,11 +2,29 @@
 
 #include <suzerain/config.h>
 
+#include <limits>
+#include <Eigen/Core>
 #include <boost/test/included/unit_test.hpp>
-
 #include <suzerain/classical.hpp>
 
-BOOST_AUTO_TEST_CASE( NOP )
+
+BOOST_AUTO_TEST_CASE( rhome_p_T_mu_lambda )
 {
-    /* NOP */
+    const double close_enough = std::numeric_limits<double>::epsilon() * 1.0e4;
+    using namespace pecos::suzerain::cartesian::rhome;
+
+    const double beta  = 2.0/3.0;
+    const double gamma = 1.4;
+    const double rho   = 2.0;
+    Eigen::Vector3d m;
+    m << 3.0, 5.0, 7.0;
+    const double e = 163.0;
+    double p, T, mu, lambda;
+
+    p_T_mu_lambda(beta, gamma, rho, m, e, p, T, mu, lambda);
+
+    BOOST_CHECK_CLOSE(p,       56.9,             close_enough);
+    BOOST_CHECK_CLOSE(T,       39.83,            close_enough);
+    BOOST_CHECK_CLOSE(mu,      11.6629085673383, close_enough);
+    BOOST_CHECK_CLOSE(lambda,  -7.7752723782255, close_enough);
 }
