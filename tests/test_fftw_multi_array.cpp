5683d44c9e0c73ac5fa9d9293b4f5e9346f7eb2d
Rhys Ulerich <rhys@ices.utexas.edu>
2009-10-29 22:09:08 +0000

    fftw_multi_array: prep to split off multiD tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@6112 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_fftw_multi_array.cpp
+++ tests/test_fftw_multi_array.cpp
@@ -1252,24 +1252,31 @@ BOOST_PP_SEQ_FOR_EACH_PRODUCT(\
         ((backward))(TRANSFORM_1D_SIZE_SEQ)(TRANSFORM_1D_SIZE_SEQ) );
 BOOST_AUTO_TEST_SUITE_END();
 
-BOOST_AUTO_TEST_CASE( c2c_2d_out_of_place )
+// Helper function testing directional transforms for a small 2D grid
+template<class ComplexMultiArray1, class ComplexMultiArray2>
+void c2c_2d_forward_4_by_3(ComplexMultiArray1 &in, ComplexMultiArray2 &out)
 {
-    typedef boost::multi_array<std::complex<double>,2> array_type;
     using fftw_multi_array::detail::assign_complex;
     using fftw_multi_array::detail::assign_components;
 
+    BOOST_STATIC_ASSERT(ComplexMultiArray1::dimensionality == 2);
+    BOOST_STATIC_ASSERT(ComplexMultiArray2::dimensionality == 2);
     const int M = 4, N = 3;
+    BOOST_REQUIRE_EQUAL(in.shape()[0], M);
+    BOOST_REQUIRE_EQUAL(in.shape()[1], N);
+    BOOST_REQUIRE_EQUAL(out.shape()[0], M);
+    BOOST_REQUIRE_EQUAL(out.shape()[1], N);
+
     const double data[M][N] = {1,2,3, 4,5,6, 7,8,9, 10,11,12};
     const double close_enough 
         = std::numeric_limits<double>::epsilon()*10*M*M*N*N;
 
-    array_type in(boost::extents[M][N]);
-    for (int i = 0; i < M; ++i)
-        for (int j = 0; j < N; ++j)
-            assign_complex(in[i][j], data[i][j], 0);
 
-    { // Transform each row and test against expected
-        array_type out(boost::extents[M][N]);
+    { // Transform zeroth dimension and test against expected
+        for (int i = 0; i < M; ++i)
+            for (int j = 0; j < N; ++j)
+                assign_complex(in[i][j], data[i][j], 0);
+
         typedef std::complex<double> z;
         const z expected0[M][N] = {
             z( 5.5, 0.),  z( 6.5, 0.),  z( 7.5, 0.),
@@ -1296,8 +1303,11 @@ BOOST_AUTO_TEST_CASE( c2c_2d_out_of_place )
         }
     }
 
-    { // Transform each column and test against expected
-        array_type out(boost::extents[M][N]);
+    { // Transform first dimension and test against expected
+        for (int i = 0; i < M; ++i)
+            for (int j = 0; j < N; ++j)
+                assign_complex(in[i][j], data[i][j], 0);
+
         typedef std::complex<double> z;
         const z expected1[M][N] = {
             z( 2.,0.), z(-0.5,0.288675134594813), z(-0.5,-0.288675134594813),
@@ -1323,5 +1333,15 @@ BOOST_AUTO_TEST_CASE( c2c_2d_out_of_place )
             }
         }
     }
+}
 
+BOOST_AUTO_TEST_CASE( c2c_2d_complex_forward )
+{
+    typedef boost::multi_array<std::complex<double>,2> array_type;
+
+    {
+        array_type in(boost::extents[4][3]);
+        array_type out(boost::extents[4][3]);
+        c2c_2d_forward_4_by_3(in, out);
+    }
 }
