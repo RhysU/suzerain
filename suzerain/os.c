8e4766fee33fac0f39226b56a035c34008ca1b0f
Rhys Ulerich <rhys@ices.utexas.edu>
2010-01-23 00:35:32 +0000

    suzerain: add os.{c,h} with POSIX utilities
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@7113 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/os.c
@@ -0,0 +1,83 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2009 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * os.c: POSIX-based operating system utilities
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#include <config.h>
+#include <suzerain/common.h>
+#include <suzerain/error.h>
+#pragma hdrstop
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int suzerain_fpipe(FILE **r, int rflags, FILE **w, int wflags)
+{
+    int pipefd[2]; /* read/write are pipefd[0/1] respectively */
+    int flags;
+
+    /* Create the pipe */
+    if (!pipe(pipefd)) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+
+    /* Set the flags on the read side */
+    flags = fcntl(pipefd[0], F_GETFL, 0);
+    if (flags == -1) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+    if (fcntl(pipefd[0], F_SETFL, flags | rflags) == -1) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+
+    /* Set the flags on the write side */
+    flags = fcntl(pipefd[1], F_GETFL, 1);
+    if (flags == -1) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+    if (fcntl(pipefd[1], F_SETFL, flags | wflags) == -1) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+
+    /* Get the file descriptor for the read side */
+    *r = fdopen(pipefd[0], "r");
+    if (*r == NULL) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+
+    /* Get the file descriptor for the write side */
+    *w = fdopen(pipefd[1], "w");
+    if (*w == NULL) {
+        SUZERAIN_ERROR_VAL(strerror(errno), SUZERAIN_FAILURE, errno);
+    }
+
+    return SUZERAIN_SUCCESS;
+}
---
+++ suzerain/os.h
@@ -0,0 +1,78 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2009 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * os.h: POSIX-based operating system utilities
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_OS_H__
+#define __SUZERAIN_OS_H__
+
+#include <suzerain/common.h>
+
+/** @file
+ * Provides utilities atop POSIX methods.
+ */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+#undef __BEGIN_DECLS
+#undef __END_DECLS
+#ifdef __cplusplus
+# define __BEGIN_DECLS extern "C" {
+# define __END_DECLS }
+#else
+# define __BEGIN_DECLS /* empty */
+# define __END_DECLS /* empty */
+#endif
+__BEGIN_DECLS
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+/**
+ * Create a POSIX pipe, set the provided flags on the read and write
+ * portions, and create <tt>FILE*</tt> handles to each end.
+ *
+ * @param[out] r Handle returned for the new pipe's read side.
+ * @param[in] rflags Flags to set for the new pipe's read side.
+ * @param[out] w Handle return for the new pipe's write side.
+ * @param[in] wflags Flags to set for the new pipe's write side.
+ *
+ * @see The manual pages for <tt>fcntl(2)</tt>, <tt>pipe(2)</tt>,
+ *      and <tt>pipe(7)</tt> for more information on creating pipes
+ *      and the effect of their flags.
+ * @see The manual page for <tt>fclose(3) for information on how
+ *      to close \c <tt>*read</tt> and \c <tt>*write</tt> after
+ *      you have finished using them.
+ *
+ * @return Zero on successful completion.  On non-zero returns \c
+ *         SUZERAIN_FAILURE and \c errno is set appropriately.
+ */
+int suzerain_fpipe(FILE **r, int rflags, FILE **w, int wflags);
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+__END_DECLS
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#endif /* __SUZERAIN_OS_H__ */
