20e892e733494aad563d5334195bd34f1dfac141
Rhys Ulerich <rhys@ices.utexas.edu>
2010-02-02 15:47:18 +0000

    suzerain: refactor test case for underling_fft
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@7360 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_underling.cpp
+++ tests/test_underling.cpp
@@ -4,21 +4,14 @@
 #include <suzerain/common.hpp>
 #pragma hdrstop
 #define BOOST_TEST_MODULE $Id$
-#include <boost/mpl/list_c.hpp>
 #include <boost/test/included/unit_test.hpp>
-#include <boost/test/test_case_template.hpp>
 #include <suzerain/error.h>
 #include <suzerain/mpi.hpp>
 #include <suzerain/underling.hpp>
+#include <suzerain/underling_fft.h>
 #include <fftw3-mpi.h>
 
-// Currently this test focuses on round-trip correctness
-// TODO Test that data which should be long is in fact long
-// TODO Test the provided stride information
-// TODO Test reuse of grids for multiple problems
-// TODO Test reuse of problems on multiple data sets
-// TODO Test unidirectional (i.e. down-only) transforms
-
+// A test fixture to setup and teardown MPI and FFTW MPI
 struct FFTWMPIFixture {
 
     FFTWMPIFixture() {
@@ -34,43 +27,70 @@ struct FFTWMPIFixture {
 
 BOOST_GLOBAL_FIXTURE(FFTWMPIFixture);
 
-void round_trip_test(const int howmany,
-                     const int NX, const int NY, const int NZ)
+// A test fixture to setup and teardown an underling use case
+struct UnderlingFixture {
+
+    UnderlingFixture(MPI_Comm comm,
+                     const int n0, const int n1, const int n2,
+                     const int howmany)
+        : grid(comm, n0, n1, n2),
+          problem(grid, howmany),
+          data((underling_real *) fftw_malloc(
+                    problem.local_memory()*sizeof(underling_real)),
+                &fftw_free),
+          plan(problem, data.get(),
+               suzerain::underling::transpose::all, FFTW_ESTIMATE)
+    {
+        BOOST_REQUIRE(grid);
+        BOOST_REQUIRE(problem);
+        BOOST_REQUIRE(data);
+        BOOST_REQUIRE(plan);
+    }
+
+    suzerain::underling::grid grid;
+    suzerain::underling::problem problem;
+    boost::shared_ptr<underling_real> data;
+    suzerain::underling::plan plan;
+
+};
+
+BOOST_AUTO_TEST_SUITE(RoundTrip)
+
+// Currently this test focuses on round-trip correctness
+// TODO Test that data which should be long is in fact long
+// TODO Test the provided stride information
+// TODO Test reuse of grids for multiple problems
+// TODO Test reuse of problems on multiple data sets
+// TODO Test unidirectional (i.e. down-only) transforms
+
+void test_round_trip(MPI_Comm comm,
+                     const int n0, const int n1, const int n2,
+                     const int howmany)
 {
     namespace underling = suzerain::underling;
 
     int procid;
-    BOOST_REQUIRE_EQUAL(MPI_SUCCESS, MPI_Comm_rank(MPI_COMM_WORLD, &procid));
+    BOOST_REQUIRE_EQUAL(MPI_SUCCESS, MPI_Comm_rank(comm, &procid));
 
     int nproc;
-    BOOST_REQUIRE_EQUAL(MPI_SUCCESS, MPI_Comm_size(MPI_COMM_WORLD, &nproc));
+    BOOST_REQUIRE_EQUAL(MPI_SUCCESS, MPI_Comm_size(comm, &nproc));
 
     if (!procid) {
         BOOST_TEST_MESSAGE("Testing howmany = " << howmany
-                           << " on " << NX << "x" << NY << "x" << NZ
+                           << " on " << n0 << "x" << n1 << "x" << n2
                            << " using " << nproc << " processor"
                            << (nproc > 1 ? "s" : "") );
     }
 
-    underling::grid grid(MPI_COMM_WORLD, NX, NY, NZ);
-    BOOST_REQUIRE(grid);
-
-    underling::problem problem(grid, howmany);
-    BOOST_REQUIRE(problem);
-
-    // Allocate space that will be automagically cleaned up
-    const size_t local_memory = problem.local_memory();
-    boost::shared_ptr<underling_real>
-        data((underling_real *) fftw_malloc(
-                    local_memory*sizeof(underling_real)),
-             &fftw_free);
-    BOOST_REQUIRE(data);
+    // Establish the test fixture
+    UnderlingFixture f(comm, n0, n1, n2, howmany);
+    const size_t local_memory = f.problem.local_memory();
 
     // Sanity check the buffer vs local extent information
     // Also gives us a reason to look up stride information
     underling_extents long_n[3];
     for (int i = 0; i < sizeof(long_n)/sizeof(long_n[0]); ++i) {
-        long_n[i] = problem.local_extents(i);
+        long_n[i] = f.problem.local_extents(i);
     }
     BOOST_REQUIRE_LE(long_n[0].extent, local_memory);
     BOOST_REQUIRE_LE(long_n[1].extent, local_memory);
@@ -78,7 +98,7 @@ void round_trip_test(const int howmany,
 
     // Sync up processors to output trace information
     for (int i = 0; i < nproc; ++i) {
-        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Barrier(comm);
         if (procid == i) {
             // Dump out the process' portion of the global grid
             std::ostringstream oss;
@@ -91,41 +111,63 @@ void round_trip_test(const int howmany,
         }
     }
 
-    underling::plan plan(problem,
-                         data.get(),
-                         underling::transpose::all,
-                         FFTW_ESTIMATE);
-    BOOST_REQUIRE(plan);
-
     // Initialize entire local memory buffer while long in n2
     std::copy(boost::make_counting_iterator(procid*10000.0),
               boost::make_counting_iterator(procid*10000.0 + local_memory),
-              data.get());
+              f.data.get());
 
     // Transform from long in n2 to long in n0
-    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, plan.execute_long_n2_to_long_n1());
-    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, plan.execute_long_n1_to_long_n0());
+    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, f.plan.execute_long_n2_to_long_n1());
+    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, f.plan.execute_long_n1_to_long_n0());
     // Transform from long in n0 to long in n2
-    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, plan.execute_long_n0_to_long_n1());
-    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, plan.execute_long_n1_to_long_n2());
+    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, f.plan.execute_long_n0_to_long_n1());
+    BOOST_REQUIRE_EQUAL(SUZERAIN_SUCCESS, f.plan.execute_long_n1_to_long_n2());
 
     // Ensure we successfully round-tripped back to long_n2 storage.
-    // Buffer reqions beyond total_extent are excluded from the check.
+    // Buffer regions beyond total_extent are excluded from the check.
     BOOST_REQUIRE_EQUAL_COLLECTIONS(
-            data.get(),
-            data.get() + long_n[2].extent,
+            f.data.get(),
+            f.data.get() + long_n[2].extent,
             boost::make_counting_iterator(procid*10000.0),
             boost::make_counting_iterator(procid*10000.0 + long_n[2].extent));
 }
 
-typedef boost::mpl::list_c<int,1,2,3,5,7,11> howmany_values;
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(roundtrip8x8x8, HOWMANY, howmany_values)
+BOOST_AUTO_TEST_CASE( roundtrip8x8x8 )
 {
-    round_trip_test(HOWMANY::type::value, 8, 8, 8);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8,  1);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8,  2);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8,  3);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8,  5);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8,  7);
+    test_round_trip(MPI_COMM_WORLD, 8, 8, 8, 11);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(roundtrip2x3x5, HOWMANY, howmany_values)
+BOOST_AUTO_TEST_CASE( roundtrip2x3x5 )
 {
-    round_trip_test(HOWMANY::type::value, 2, 3, 5);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5,  1);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5,  2);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5,  3);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5,  5);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5,  7);
+    test_round_trip(MPI_COMM_WORLD, 2, 3, 5, 11);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE( underling_fft )
+
+void test_c2c(MPI_Comm comm,
+              const int n0, const int n1, const int n2,
+              const int howmany,
+              const int long_i)
+{
+    UnderlingFixture f(comm, n0, n1, n2, howmany);
+}
+
+BOOST_AUTO_TEST_CASE( underling_fft_c2c )
+{
+    test_c2c(MPI_COMM_WORLD, 2, 3, 5, 1, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
