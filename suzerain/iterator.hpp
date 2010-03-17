112e3ec1806611738c5ce4d00de9b337838de128
Rhys Ulerich <rhys@ices.utexas.edu>
2010-03-17 22:08:43 +0000

    suzerain: add iterator utilities
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@8425 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/iterator.hpp
@@ -0,0 +1,84 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2010 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * iterator.hpp: iterator tools
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_ITERATOR_HPP
+#define __SUZERAIN_ITERATOR_HPP
+
+#include <suzerain/common.hpp>
+
+namespace suzerain {
+
+/**
+ * Provides iterator-related utilities.
+ */
+namespace iterator {
+
+/** An input_iterator that always returns a fixed constant. */
+template<typename T>
+class infinite_constant
+    : public std::iterator<std::input_iterator_tag, const T>
+{
+public:
+    infinite_constant(const T& t) : t_(t) {}
+
+    infinite_constant(const infinite_constant& other) : t_(other.t) {}
+
+    infinite_constant& operator++() { return *this; }
+    infinite_constant& operator++(int) { return *this; }
+
+    bool operator==(const infinite_constant& rhs) const { return t_ == rhs.t_; }
+    bool operator!=(const infinite_constant& rhs) const { return t_ == rhs.t_; }
+
+    const T& operator*() const { return t_; }
+
+private:
+    const T t_;
+};
+
+template< typename charT, typename traits, typename T >
+::std::basic_ostream<charT,traits>& operator<<(
+        ::std::basic_ostream<charT,traits> &os,
+        const infinite_constant<T> &ic)
+{
+    os << *ic;
+    return os;
+}
+
+template<typename T>
+infinite_constant<T> make_infinite_constant(const T& t)
+{
+    return infinite_constant<T>(t);
+}
+
+} // namespace iterator
+
+} // namespace suzerain
+
+#endif // __SUZERAIN_ITERATOR_HPP
---
+++ tests/test_iterator.cpp
@@ -0,0 +1,62 @@
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.hpp>
+#pragma hdrstop
+#define BOOST_TEST_MODULE $Id$
+#include <suzerain/iterator.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+#pragma warning(disable:383)
+
+BOOST_AUTO_TEST_SUITE( infinite_constant )
+
+using suzerain::iterator::infinite_constant;
+using suzerain::iterator::make_infinite_constant;
+
+BOOST_AUTO_TEST_CASE( boolean )
+{
+    infinite_constant<bool> i_true = make_infinite_constant(true);
+    BOOST_CHECK_EQUAL(true, *i_true);
+    BOOST_CHECK_EQUAL(true, *i_true++);
+    BOOST_CHECK_EQUAL(true, *i_true++);
+    BOOST_CHECK_EQUAL(true, *i_true++);
+    BOOST_CHECK_EQUAL(true, *(++i_true));
+    BOOST_CHECK_EQUAL(true, *(++i_true));
+    BOOST_CHECK_EQUAL(true, *(++i_true));
+
+    infinite_constant<bool> i_false = make_infinite_constant(false);
+    BOOST_CHECK_EQUAL(false, *i_false);
+    BOOST_CHECK_EQUAL(false, *i_false++);
+    BOOST_CHECK_EQUAL(false, *i_false++);
+    BOOST_CHECK_EQUAL(false, *i_false++);
+    BOOST_CHECK_EQUAL(false, *(++i_false));
+    BOOST_CHECK_EQUAL(false, *(++i_false));
+    BOOST_CHECK_EQUAL(false, *(++i_false));
+
+    BOOST_CHECK_EQUAL(i_true, i_true);
+    BOOST_CHECK_EQUAL(i_true, make_infinite_constant(true));
+    BOOST_CHECK_EQUAL(i_false, i_false);
+    BOOST_CHECK_EQUAL(i_false, make_infinite_constant(false));
+    BOOST_CHECK_NE(i_true, i_false);
+    BOOST_CHECK_NE(i_false, i_true);
+}
+
+BOOST_AUTO_TEST_CASE( integer )
+{
+    infinite_constant<int> ic = make_infinite_constant(4);
+    BOOST_CHECK_EQUAL(4, *ic);
+    BOOST_CHECK_EQUAL(4, *ic++);
+    BOOST_CHECK_EQUAL(4, *ic++);
+    BOOST_CHECK_EQUAL(4, *ic++);
+    BOOST_CHECK_EQUAL(4, *(++ic));
+    BOOST_CHECK_EQUAL(4, *(++ic));
+    BOOST_CHECK_EQUAL(4, *(++ic));
+
+    BOOST_CHECK_EQUAL(ic, ic);
+    BOOST_CHECK_EQUAL(ic, make_infinite_constant(4));
+    BOOST_CHECK_EQUAL(make_infinite_constant(4), ic);
+    BOOST_CHECK_NE(ic, make_infinite_constant(5));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
