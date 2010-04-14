c1cead0290eba0a5f8a8be67c2383240c70cb482
Rhys Ulerich <rhys@ices.utexas.edu>
2010-04-14 22:10:45 +0000

    suzerain: prep for general diffwave tests
    some test refactoring + move to gsl_complex to simplify test routines
    
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@9359 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_diffwave.cpp
+++ tests/test_diffwave.cpp
@@ -5,6 +5,8 @@
 #pragma hdrstop
 #define BOOST_TEST_MODULE $Id$
 #include <boost/test/included/unit_test.hpp>
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
 #include <suzerain/diffwave.h>
 #include "test_tools.hpp"
 
@@ -73,9 +75,11 @@ BOOST_AUTO_TEST_CASE( freqindex_dealiasing )
     }
 }
 
-static void test_y0x0z0_helper(const int Ny,
-                               const int Nx, const int dNx,
-                               const int Nz, const int dNz)
+static void test_accumulate_helper(const int dxcnt, const int dzcnt,
+                                   const double Lx, const double Lz,
+                                   const int Ny,
+                                   const int Nx, const int dNx,
+                                   const int Nz, const int dNz)
 {
     const int nelem = Ny*(dNx/2+1)*dNz;
     double (* const x)[2] = (double (*)[2]) malloc(nelem*sizeof(x[0]));
@@ -85,39 +89,50 @@ static void test_y0x0z0_helper(const int Ny,
         for (int n = 0; n < dNz; ++n) {
             for (int m = 0; m < (dNx/2+1); ++m) {
                 for (int l = 0; l < Ny; ++l) {
-                    (*p)[0] =  (l+1)*(m+1)*(n+1); // Fill x
-                    (*p)[1] = -(l+1)*(m+1)*(n+1);
+                    (*p)[0] =  (l+2)*(m+3)*(n+5);   // Fill x
+                    (*p)[1] = -(l+2)*(m+3)*(n+5);
                     p++;
-                    (*q)[0] =  (l+2)*(m+3)*(n+4); // Fill y
-                    (*q)[1] = -(l+2)*(m+3)*(n+4);
+                    (*q)[0] =  (l+7)*(m+11)*(n+13); // Fill y
+                    (*q)[1] = -(l+7)*(m+11)*(n+13);
                     q++;
                 }
             }
         }
     }
 
-    const double alpha[2] = {2, 0};
-    const double beta[2]  = {3, 0};
-    suzerain_diffwave_accumulate(0, 0, alpha, x, beta, y,
-            555, 777, Ny, Nx, dNx, 0, (dNx/2+1), Nz, dNz, 0, dNz);
+    const gsl_complex alpha = gsl_complex_rect(2, 0);
+    const gsl_complex beta  = gsl_complex_rect(3, 0);
+    suzerain_diffwave_accumulate(dxcnt, dzcnt, alpha.dat, x, beta.dat, y,
+            Lx, Lz, Ny, Nx, dNx, 0, (dNx/2+1), Nz, dNz, 0, dNz);
 
-    {
-        double (*q)[2] = y;
-        for (int n = 0; n < dNz; ++n) {
-            for (int m = 0; m < (dNx/2+1); ++m) {
-                for (int l = 0; l < Ny; ++l) {
-                    if (   (n <= (Nz+1)/2 || n >= (dNz - (Nz-1)/2))
-                        && (m <= (Nx+1)/2 || m >= (dNx - (Nx-1)/2))) {
-                        BOOST_CHECK_EQUAL(
-                            (*q)[0], 2*(l+1)*(m+1)*(n+1)+ 3*(l+2)*(m+3)*(n+4));
-                        BOOST_CHECK_EQUAL(
-                            (*q)[1],-2*(l+1)*(m+1)*(n+1)+-3*(l+2)*(m+3)*(n+4));
-                    } else {
-                        BOOST_CHECK_EQUAL((*q)[0],  3*(l+2)*(m+3)*(n+4));
-                        BOOST_CHECK_EQUAL((*q)[1], -3*(l+2)*(m+3)*(n+4));
-                    }
-                    ++q;
+    const double close_enough = std::numeric_limits<double>::epsilon()*100;
+    double (*q)[2] = y;
+    for (int n = 0; n < dNz; ++n) {
+        for (int m = 0; m < (dNx/2+1); ++m) {
+            for (int l = 0; l < Ny; ++l) {
+                const gsl_complex observed = gsl_complex_rect((*q)[0],(*q)[1]);
+
+                const gsl_complex xsrc = gsl_complex_rect(
+                            (l+2)*(m+3)*(n+5),   -(l+2)*(m+3)*(n+5));
+                const gsl_complex ysrc = gsl_complex_rect(
+                            (l+7)*(m+11)*(n+13), -(l+7)*(m+11)*(n+13));
+
+                gsl_complex expected;
+
+                if (   (n <= (Nz+1)/2 || n >= (dNz - (Nz-1)/2))
+                    && (m <= (Nx+1)/2 || m >= (dNx - (Nx-1)/2))) {
+
+                    expected = gsl_complex_add(gsl_complex_mul(alpha, xsrc),
+                                               gsl_complex_mul(beta,  ysrc));
+                } else {
+                    expected = gsl_complex_mul(beta, ysrc);
                 }
+
+                const double diff = gsl_complex_abs(
+                        gsl_complex_sub(expected, observed));
+                BOOST_CHECK_SMALL(diff, close_enough);
+
+                ++q;
             }
         }
     }
@@ -126,18 +141,20 @@ static void test_y0x0z0_helper(const int Ny,
     free(y);
 }
 
-BOOST_AUTO_TEST_CASE( y0x0z0 )
+BOOST_AUTO_TEST_CASE( accumulate )
 {
-    boost::array<int,5> c[] = {
-        /* Ny, Nx, dNx, Nz, dNz */
-        {   3,  4,   4,  4,   4  },
-        {   3,  8,   8,  8,   8  },
-        {   3,  7,   7,  7,   7  },
-        {   3,  8,  12, 16,  24  }
+    boost::array<int,9> c[] = {
+        /* dxcnt, dzcnt,  Lx,  Lz, Ny, Nx, dNx, Nz, dNz */
+        {      0,     0, 555, 777,  3,  4,   4,  4,   4  },
+        {      0,     0, 555, 777,  3,  8,   8,  8,   8  },
+        {      0,     0, 555, 777,  3,  7,   7,  7,   7  },
+        {      0,     0, 555, 777,  3,  8,  12, 16,  24  }
     };
 
     for (int i = 0; i < sizeof(c)/sizeof(c[0]); ++i) {
         BOOST_TEST_MESSAGE("Testing " << c[i]);
-        test_y0x0z0_helper(c[i][0], c[i][1], c[i][2], c[i][3], c[i][4]);
+        test_accumulate_helper(
+                c[i][0], c[i][1], c[i][2], c[i][3], c[i][4],
+                c[i][5], c[i][6], c[i][7], c[i][8]);
     }
 }
