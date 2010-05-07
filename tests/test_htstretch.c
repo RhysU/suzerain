5096d5d8cf9bae2b2fb40474a003e2aea6577b1b
Rhys Ulerich <rhys@ices.utexas.edu>
2010-05-07 21:31:46 +0000

    suzerain: add tests for htstretch{1,2}
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@10048 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ tests/test_htstretch.c
@@ -0,0 +1,89 @@
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.h>
+#pragma hdrstop
+#include <gsl/gsl_ieee_utils.h>
+#include <gsl/gsl_machine.h>
+#include <gsl/gsl_test.h>
+#include <suzerain/htstretch.h>
+
+#define CASE(f, x, y, z, e) \
+    gsl_test_abs(suzerain_ ## f((x),(y),(z)), (e), GSL_DBL_EPSILON, \
+        #f "(%g, %g, %g)", (x), (y), (z) );
+
+static
+void
+test_htstretch1()
+{
+    /* Check boundaries for varying delta values */
+    for (int i = 1; i < 10; i += 2) {
+        CASE(htstretch1, (double) i, 2., 0., 0.);
+        CASE(htstretch1, (double) i, 2., 2., 1.);
+    }
+
+    /* Spot check evaluation and derivatives from Mathematica N[expr, 16] */
+    CASE(htstretch1,        7., 3., 1./ 9., +1.130109424675245e-6);
+    CASE(htstretch1_ddelta, 7., 3., 1./ 9., -2.053316509625048e-6);
+    CASE(htstretch1_dL,     7., 3., 1./ 9., -4.827691460278840e-7);
+    CASE(htstretch1_dx,     7., 3., 1./ 9., +0.00001303476694275287);
+    CASE(htstretch1,        3., 2., 1./11., +0.001553191024452532);
+    CASE(htstretch1_ddelta, 3., 2., 1./11., -0.002512942959289942);
+    CASE(htstretch1_dL,     3., 2., 1./11., -0.0008866151849355177);
+    CASE(htstretch1_dx,     3., 2., 1./11., +0.01950553406858139);
+
+    /* Spot check evaluation near wall from Mathematica N[expr, 16] */
+    CASE(htstretch1,        4., 1., 1.0e-03, +5.388915112675453e-6);
+    CASE(htstretch1_ddelta, 4., 1., 1.0e-03, -9.425182395066187e-6);
+    CASE(htstretch1_dL,     4., 1., 1.0e-03, -5.410484940505296e-6);
+    CASE(htstretch1_dx,     4., 1., 1.0e-03, +0.005410484940505296);
+    CASE(htstretch1,        4., 1., 1.0e-08, +5.367402865013899e-11);
+    CASE(htstretch1_ddelta, 4., 1., 1.0e-08, -9.392957375935146e-11);
+    CASE(htstretch1_dL,     4., 1., 1.0e-08, -5.367403079566020e-11);
+    CASE(htstretch1_dx,     4., 1., 1.0e-08, +0.005367403079566020);
+}
+static
+void
+test_htstretch2()
+{
+    /* Check boundaries for varying delta values */
+    for (int i = 1; i < 10; i += 2) {
+        CASE(htstretch2, (double) i, 2., 0., 0.);
+        CASE(htstretch2, (double) i, 2., 2., 1.);
+    }
+
+    /* Spot check evaluation and derivatives from Mathematica N[expr, 16] */
+    CASE(htstretch2,        7., 3., 1./ 9., +0.0006192752099952831);
+    CASE(htstretch2_ddelta, 7., 3., 1./ 9., -0.0005055862703645726);
+    CASE(htstretch2_dL,     7., 3., 1./ 9., -0.0002643833885364380);
+    CASE(htstretch2_dx,     7., 3., 1./ 9., +0.007138351490483826);
+    CASE(htstretch2,        3., 2., 1./11., +0.01541983224264333);
+    CASE(htstretch2_ddelta, 3., 2., 1./11., -0.009494629614989877);
+    CASE(htstretch2_dL,     3., 2., 1./11., -0.008679927388636600);
+    CASE(htstretch2_dx,     3., 2., 1./11., +0.1909584025500052);
+
+    /* Spot check evaluation near wall from Mathematica N[expr, 16] */
+    CASE(htstretch2,        4., 1., 1.0e-03, +0.0001471408880498442);
+    CASE(htstretch2_ddelta, 4., 1., 1.0e-03, -0.0001102915457626845);
+    CASE(htstretch2_dL,     4., 1., 1.0e-03, -0.0001477088973243801);
+    CASE(htstretch2_dx,     4., 1., 1.0e-03, +0.1477088973243801);
+    CASE(htstretch2,        4., 1., 1.0e-08, +1.465742869555286e-9);
+    CASE(htstretch2_ddelta, 4., 1., 1.0e-08, -1.100290869880845e-9);
+    CASE(htstretch2_dL,     4., 1., 1.0e-08, -1.465742926075948e-9);
+    CASE(htstretch2_dx,     4., 1., 1.0e-08, +0.1465742926075948);
+}
+
+int
+main(int argc, char **argv)
+{
+    gsl_ieee_env_setup();
+
+    if (argc > 1) {
+        gsl_test_verbose(1);
+    }
+
+    test_htstretch1();
+    test_htstretch2();
+
+    exit(gsl_test_summary());
+}
