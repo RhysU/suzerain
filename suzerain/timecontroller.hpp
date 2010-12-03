15daf9d948da0a41aeae71e8aaba6257ea545714
Rhys Ulerich <rhys@ices.utexas.edu>
2010-12-03 20:07:38 +0000

    suzerain: timecontroller docs finished #1201
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@16026 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/timecontroller.hpp
+++ suzerain/timecontroller.hpp
@@ -36,7 +36,8 @@
 #include <suzerain/traits.hpp>
 
 /** @file
- * Provides time integration schemes.
+ * Provides higher-level control mechanisms based atop abstract
+ * time integration schemes.
  */
 
 namespace suzerain
@@ -46,8 +47,14 @@ namespace timestepper
 {
 
 /**
- * Provides an abstract base class for higher-level time advance
- * management including periodic restart and statistics writing events.
+ * Provides an abstract base class for high-level time advancement control
+ * including periodic callbacks.  Such callbacks could be used, for example, to
+ * periodically write restart files, write statistics, and output status.
+ * Subclasses must only implement the stepTime() method with the relevant
+ * time integration logic to take a single step.
+ *
+ * @tparam FPT Floating point type used to track the current time.
+ * @tparam Integer Integer type used to track the current time step.
  */
 template<
     typename FPT     = double,
@@ -57,34 +64,153 @@ class AbstractTimeController
 {
 
 public:
-    AbstractTimeController(FPT initial_t = 0,
-                           FPT min_dt = 1e-8,
-                           FPT max_dt = std::numeric_limits<FPT>::max());
 
+    /**
+     * Construct an instance with the given parameters.
+     *
+     * @param initial_t Initial simulation time.
+     * @param min_dt    Initial minimum acceptable time step.
+     *                  See min_dt() for the associated semantics.
+     * @param max_dt    Initial maximum acceptable time step.
+     *                  See max_dt() for the associated semantics.
+     */
+    AbstractTimeController(
+            FPT initial_t = 0,
+            FPT min_dt = std::numeric_limits<FPT>::epsilon(),
+            FPT max_dt = std::numeric_limits<FPT>::max());
+
+    /**
+     * Virtual destructor as appropriate for an abstract base class.
+     **/
     virtual ~AbstractTimeController();
 
-    FPT current_t() const { return current_t_; }
+    //@{
 
-    Integer current_nt() const { return current_nt_; }
-
+    /**
+     * Retrieve the minimum acceptable time step.  Physically
+     * driven time steps below this value will cause advanceTime()
+     * to abort.
+     *
+     * @return The current minimum acceptable time step.
+     */
     FPT min_dt() const { return min_dt_; }
 
+    /**
+     * Set the minimum acceptable time step.
+     *
+     * @param new_min_dt Minimum acceptable time step to set.
+     * @see min_dt() for more details.
+     */
     void min_dt(FPT new_min_dt) { min_dt_ = new_min_dt; }
 
+    //@}
+
+    //@{
+
+    /**
+     * Retrieve the maximum acceptable time step.  The controller
+     * will never take a time step in excess of this size.
+     *
+     * @return The current maximum acceptable time step.
+     */
     FPT max_dt() const { return max_dt_; }
 
+    /**
+     * Set the maximum acceptable time step.
+     *
+     * @param new_max_dt Minimum acceptable time step to set.
+     * @see max_dt() for more details.
+     */
     void max_dt(FPT new_max_dt) { max_dt_ = new_max_dt; }
 
+    //@}
+
+    /**
+     * Register a periodic callback to be used during advanceTime().  As the
+     * controller marches the simulation, \c callback will be invoked \c
+     * every_dt time steps or after \c every_t simulation time passes,
+     * whichever comes first.
+     *
+     * Callbacks must be functions or functors taking two arguments and
+     * returning a boolean value.  The first argument is of type \c FPT and
+     * will contain the current simulation time.  The second argument is of
+     * type \c Integer wand will contain the current simulation time step.  One
+     * example signature would be
+     * @code
+     *   bool my_callback(FPT t, Integer nt);
+     * @endcode
+     * The callback should return \c true if the controller should continue
+     * advancing.  If the callback returns \c false, the controller will
+     * immediately stop advancing time.
+     *
+     * @param every_dt The maximum simulation time duration
+     *                 between callbacks.
+     * @param every_nt The simulation time step count
+     *                 between callbacks.
+     * @param callback The callback to invoke.
+     *
+     * @see <tt>std::numeric_limits<T>::max()/tt> is you want to
+     *      specify either no criteria for \c every_dt or \c every_nt.
+     * @see <a href="http://www.boost.org/doc/html/ref.html">Boost.Ref</a>
+     *      if you need to provide a stateful or noncopyable functor
+     *      to \c callback.
+     */
     template<typename Callback>
     void addCallback(FPT every_dt,
                      Integer every_nt,
                      Callback callback);
 
+    /**
+     * Advance the simulation in time using stepTime() and perform required
+     * callbacks along the way.  The simulation will advance until either
+     * current_t() reaches \c final_t or current_nt() reaches \c final_nt,
+     * whichever comes first.  If any physically-determined time step
+     * is smaller than min_dt() or any registered callback returns \c false,
+     * the controller will immediately stop.
+     *
+     * @param final_t  Maximum simulation time.
+     *                 If you want to advance some relative amount \c dt,
+     *                 try supplying <tt>current_t() + dt</tt>.
+     * @param final_nt Maximum simulation time step.
+     *                 If you want to advance some relative step count \c nt,
+     *                 try supplying <tt>current_nt() + nt</tt>.
+     *
+     * @return True if the controller completed the request successfully.
+     *         False if the controller aborted for some reason.
+     */
     bool advanceTime(FPT final_t,
                      Integer final_nt = std::numeric_limits<Integer>::max());
 
+    //@{
+
+    /**
+     * Retrieve the current simulation time.
+     *
+     * @return The current simulation time.
+     */
+    FPT current_t() const { return current_t_; }
+
+    /**
+     * Retrieve the current simulation time step.
+     *
+     * @return The current simulation discrete time step.
+     */
+    Integer current_nt() const { return current_nt_; }
+
+    //@}
+
 protected:
 
+    /**
+     * Advance simulation time by a single step of at most \c max_dt.
+     * Subclasses implement this method to hook in relevant time integration
+     * logic.  Physically-driven accuracy or stability requirements may limit
+     * the actual step taken to be smaller than \c max_dt.
+     *
+     * @param max_dt Maximum step size to take.
+     *
+     * @return The actual step size taken by the time integration scheme.
+     */
     virtual FPT stepTime(FPT max_dt) const = 0;
 
 private:
@@ -172,6 +298,7 @@ bool AbstractTimeController<FPT,Integer>::advanceTime(const FPT final_t,
 
         // Take time step and advance simulation time
         const FPT actual_dt = this->stepTime(possible_dt);
+        assert(actual_dt <= possible_dt);
         current_t_  += actual_dt;
         current_nt_ += 1;
 
