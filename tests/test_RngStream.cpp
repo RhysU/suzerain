7a8e20e08298f216cf0e4a8ac043449662c67828
Rhys Ulerich <rhys@ices.utexas.edu>
2011-07-21 21:47:56 +0000

    suzerain: #1514 add L'Ecuyer RngStream tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@22497 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ tests/test_RngStream.cpp
@@ -0,0 +1,111 @@
+//  Program to test the random number streams file:    RngStream.cpp
+//  From http://www.iro.umontreal.ca/~lecuyer/myftp/streams00/c++/testRngStream.cpp
+//  Modified to use Boost Test for error reporting.
+//  Original authors retain their copyright, of course.
+
+
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.hpp>
+#pragma hdrstop
+#define BOOST_TEST_MODULE $Id$
+#include <suzerain/RngStream.hpp>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+using namespace std;
+using suzerain::RngStream;
+
+BOOST_AUTO_TEST_CASE( main_test )
+{
+   double sum;
+   int  i;
+   RngStream g1 ("g1");
+   RngStream g2 ("g2");
+   RngStream g3 ("g3");
+
+   sum = g2.RandU01 () + g3.RandU01 ();
+
+   g1.AdvanceState (5, 3);   
+   sum += g1.RandU01 ();
+
+   g1.ResetStartStream ();
+   for (i = 0;  i < 35; i++)
+      g1.AdvanceState (0,1);
+   sum += g1.RandU01 ();
+
+   g1.ResetStartStream ();
+   long sumi = 0;
+   for (i = 0;  i < 35; i++)
+      sumi += g1.RandInt (1, 10);
+   sum += sumi / 100.0;
+
+   double sum3 = 0.0;
+   for (i = 0;  i < 100;  i++) {
+      sum3 += g3.RandU01 ();
+   }
+   sum += sum3 / 10.0;
+
+   g3.ResetStartStream ();
+   for (i=1; i<=5; i++)
+      sum += g3.RandU01 ();
+
+   for (i=0; i<4; i++)
+      g3.ResetNextSubstream ();
+   for (i=0; i<5; i++)
+      sum += g3.RandU01 ();
+
+   g3.ResetStartSubstream ();
+   for (i=0; i<5; i++)
+      sum += g3.RandU01 ();
+
+   g2.ResetNextSubstream ();
+   sum3 = 0.0;
+   for (i=1; i<=100000; i++)
+      sum3 += g2.RandU01 ();
+   sum += sum3 / 10000.0;
+
+   g3.SetAntithetic (true);
+   sum3 = 0.0;
+   for (i=1; i<=100000; i++)
+      sum3 += g3.RandU01 ();
+   sum += sum3 / 10000.0;
+
+   unsigned long germe[6] = { 1, 1, 1, 1, 1, 1 };
+   RngStream::SetPackageSeed (germe);
+
+   RngStream gar[4] = { "Poisson", "Laplace", "Galois", "Cantor" };
+   for  (i = 0; i < 4; i++)
+      sum += gar[i].RandU01 ();
+
+   gar[2].AdvanceState (-127, 0);
+   sum += gar[2].RandU01 ();
+
+   gar[2].ResetNextSubstream ();
+   gar[2].IncreasedPrecis (true);
+   sum3 = 0.0;
+   for  (i = 0; i < 100000; i++)
+      sum3 += gar[2].RandU01 ();
+   sum += sum3 / 10000.0;
+
+   gar[2].SetAntithetic (true);
+   sum3 = 0.0;
+   for  (i = 0; i < 100000; i++)
+      sum3 += gar[2].RandU01 ();
+   sum += sum3 / 10000.0;
+   gar[2].SetAntithetic (false);
+
+   gar[2].IncreasedPrecis (false);
+   for  (i = 0; i < 4; i++)
+      sum += gar[i].RandU01 ();
+
+   // Original output:
+   // cout.precision(14);
+   // cout << "------------------------------------------\n";
+   // cout << "This program should print  39.697547445251\n";
+   // cout << "Actual test result =       " << sum << "\n\n";
+   // Boost.Testified with precision requirement from cout.precision above.
+   BOOST_REQUIRE_SMALL(std::abs(sum - 39.697547445251), 1e-13);
+}
