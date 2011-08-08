3fa6039e93c6c39602d5db0bad6aa34fe58f8fd2
Rhys Ulerich <rhys@ices.utexas.edu>
2011-08-08 21:18:36 +0000

    suzerain: #1514 noise to serial/parallel equivalence
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@22783 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/test_channel_parallel.sh
+++ apps/test_channel_parallel.sh
@@ -4,7 +4,7 @@ set -eu
 # Initialize test infrastructure
 source "`dirname $0`/test_channel_setup.sh"
 
-: ${ADVANCE:=--advance_nt=10}
+: ${ADVANCE:=--advance_nt=10 --fluctpercent=1}
 
 banner "Generating serial result for comparison purposes"
 (
