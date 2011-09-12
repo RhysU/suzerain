b9346e9b1285713697079460846ffa74dae13ebb
Rhys Ulerich <rhys@ices.utexas.edu>
2011-09-12 16:11:28 +0000

    suzerain: add syntax check for job scripts
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@23626 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ jobscripts/test_bash_syntax.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Ensure 'file(1)' reports what we expect for a bash script
+file "$BASH_SOURCE" | grep "Bourne-Again shell script" >/dev/null
+
+# Check that all bash scripts have valid syntax
+status=0
+for entry in *; do
+    if file "$entry" | grep "Bourne-Again shell script" >/dev/null; then
+           (bash -n "$entry" && echo "$entry: syntax ok"   ) \
+        || (status=1         && echo "$entry: syntax ERROR")
+    fi
+done
+exit $status
