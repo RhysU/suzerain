1b8da73bc5c79b9b1cb0f5b11895ecd4ce1cf4d8
Rhys Ulerich <rhys@ices.utexas.edu>
2011-09-19 17:00:17 +0000

    suzerain: #1994 progress on physical restart tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@23777 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/test_channel_physical.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -eu
+
+# Initialize test infrastructure
+source "`dirname $0`/test_channel_setup.sh"
+
+# Shorthand
+explicit="prunq ../channel_explicit"
+
+# Run each test case in this file under the following circumstances
+# (which can be overridden by providing the environment variable METACASES).
+for METACASE in ${METACASES:= 'NP=1' 'NP=2;P=--Pa=2' 'NP=2;P=--Pb=2'}; do
+NP=
+P=
+eval "$METACASE"
+
+# Prepare pmms0.h5 in serial, then in parallel, and ensure both match
+# pmms0.h5 restart file is used in the tests that follow
+banner "Preparation of physical-space version of wave-based test field"
+(
+    cd $testdir
+    runq ../channel_explicit mms0.h5 --desttemplate "pmms#.h5" --advance_nt=0 \
+                                     --restart_physical
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 --restart_physical
+    differ pmms0.h5 a0.h5
+)
+
+banner "Idempotence of restarting from physical space without time advance"
+(
+    cd $testdir
+    $explicit pmms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P --restart_physical
+    differ --delta=1e-15 --nan pmms0.h5 a0.h5
+)
+
+# BORKED?
+# banner "Conversion from physical- to wave-based restart without time advance"
+# (
+#     cd $testdir
+#     $explicit pmms0.h5 --desttemplate "a#.h5" --advance_nt=0
+#     differ --delta=3e-15 --nan mms0.h5 a0.h5
+# )
+
+# BORKED?
+# banner "Equivalence of a field both with and without a physical space restart"
+# (
+#     cd $testdir
+#     $explicit pmms0.h5 --desttemplate "a#.h5" --advance_nt=1 $P --restart_physical
+#     $explicit a0.h5    --desttemplate "b#.h5" --advance_nt=1 $P --restart_physical
+#     $explicit pmms0.h5 --desttemplate "c#.h5" --advance_nt=2 $P --restart_physical
+#     differ --delta=7e-16 --nan b0.h5 c0.h5
+# )
+
+# BORKED?
+# banner "Idempotence of restarting without time advance via physical space"
+# (
+#     cd $testdir
+#     $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P --restart_physical
+#     $explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P
+#     differ --delta=3e-15 mms0.h5 b0.h5
+# )
+
+done
--- apps/test_channel_restart.sh
+++ apps/test_channel_restart.sh
@@ -4,6 +4,9 @@ set -eu
 # Initialize test infrastructure
 source "`dirname $0`/test_channel_setup.sh"
 
+# Shorthand
+explicit="prunq ../channel_explicit"
+
 # Run each test case in this file under the following circumstances
 # (which can be overridden by providing the environment variable METACASES).
 for METACASE in ${METACASES:= 'NP=1' 'NP=2;P=--Pa=2' 'NP=2;P=--Pb=2'}; do
@@ -11,39 +14,37 @@ NP=
 P=
 eval "$METACASE"
 
-banner "Idempotence of restarting without time advancement"
+banner "Idempotence of restarting without time advance"
 (
     cd $testdir
-    prunq ../channel_explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P
     differ mms0.h5 a0.h5
 )
 
 banner "Equivalence of a field both with and without a restart"
 (
     cd $testdir
-    prunq ../channel_explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=1 $P
-    prunq ../channel_explicit a0.h5   --desttemplate "b#.h5" --advance_nt=1 $P
-    prunq ../channel_explicit mms0.h5 --desttemplate "c#.h5" --advance_nt=2 $P
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=1 $P
+    $explicit a0.h5   --desttemplate "b#.h5" --advance_nt=1 $P
+    $explicit mms0.h5 --desttemplate "c#.h5" --advance_nt=2 $P
     differ --delta=7e-16 --nan b0.h5 c0.h5
 )
 
 banner "Upsample/downsample both homogeneous directions"
 (
     cd $testdir
-    prunq ../channel_explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P \
-                                      --Nx=$((2*$Nx)) --Nz=$((3*$Nz))
-    prunq ../channel_explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P \
-                                      --Nx=$((  $Nx)) --Nz=$((  $Nz))
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P \
+                      --Nx=$((2*$Nx)) --Nz=$((3*$Nz))
+    $explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P \
+                      --Nx=$((  $Nx)) --Nz=$((  $Nz))
     differ mms0.h5 b0.h5
 )
 
 banner "Upsample/downsample inhomogeneous direction order"
 (
     cd $testdir
-    prunq ../channel_explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P \
-                                      --k=$(($k+1))
-    prunq ../channel_explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P \
-                                      --k=$(($k  ))
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P --k=$(($k+1))
+    $explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P --k=$(($k  ))
     # Chosen tolerances are wholly empirical and represent nothing deep
     differ --delta=5e-5 mms0.h5 b0.h5 /rho
     differ --delta=3e-4 mms0.h5 b0.h5 /rhou
@@ -55,10 +56,10 @@ banner "Upsample/downsample inhomogeneous direction order"
 banner "Upsample/downsample inhomogeneous direction NDOF and htdelta"
 (
     cd $testdir
-    prunq ../channel_explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P \
-                                     --Ny=$((2*$Ny)) --htdelta=$(($htdelta+1))
-    prunq ../channel_explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P \
-                                     --Ny=$((  $Ny)) --htdelta=$(($htdelta  ))
+    $explicit mms0.h5 --desttemplate "a#.h5" --advance_nt=0 $P \
+                      --Ny=$((2*$Ny)) --htdelta=$(($htdelta+1))
+    $explicit a0.h5   --desttemplate "b#.h5" --advance_nt=0 $P \
+                      --Ny=$((  $Ny)) --htdelta=$(($htdelta  ))
     # Chosen tolerances are wholly empirical and represent nothing deep
     differ --delta=6e-6 mms0.h5 b0.h5 /rho
     differ --delta=1e-4 mms0.h5 b0.h5 /rhou
