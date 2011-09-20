6432b3a10cc0c6c3822ea958e7ca05a359bb7d09
Rhys Ulerich <rhys@ices.utexas.edu>
2011-09-20 14:07:23 +0000

    suzerain: add SUZERAIN_COUNTOF macro in countof.h
    C is the obvious definition.
    C++ is from a cited Dobb's article.
    Automagic switch between the two based on language.
    Always included from suzerain/common.h
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@23810 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/countof.h
@@ -0,0 +1,72 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2011 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * countof.h: Macros to count the number of array elements at compile time
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_COUNTOF_H__
+#define __SUZERAIN_COUNTOF_H__
+
+/** @file
+ * Macros to count the number of array elements at compile time.  Provides both
+ * a type-unsafe C version and a type-safe C++ version.  The C++ version is
+ * from Ivan J. Johnson's article "Counting Array Elements at Compile Time"
+ * published 6 March 2007 in Dr. Dobb's (http://drdobbs.com/cpp/197800525).
+ */
+
+#ifndef __cplusplus
+
+/** Count the number of elements in an array at compile time */
+#define SUZERAIN_COUNTOF(x) (sizeof(x)/sizeof((x)[0]))
+
+#else /* __cplusplus */
+
+/** Type safe count the number of elements in an array at compile time */
+#define SUZERAIN_COUNTOF(x)  (                                                  \
+    0*sizeof(reinterpret_cast<const ::suzerain::BAD_ARGUMENT_TO_COUNTOF*>(x)) + \
+    0*sizeof(::suzerain::BAD_ARGUMENT_TO_COUNTOF::check_type((x), &(x))     ) + \
+    sizeof(x)/sizeof((x)[0])   )
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+namespace suzerain {
+
+class BAD_ARGUMENT_TO_COUNTOF
+{
+public:
+    class Is_pointer;
+    class Is_array {};
+    template<typename T>
+    static Is_pointer check_type(const T*, const T* const*);
+    static Is_array check_type(const void*, const void*);
+};
+
+}
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#endif /* __cplusplus */
+
+#endif /* __SUZERAIN_COUNTOF_H__ */
