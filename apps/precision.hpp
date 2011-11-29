15d24df82978fc2ff79ef3dd30bd75cfab14a6da
Rhys Ulerich <rhys@ices.utexas.edu>
2011-11-29 17:10:18 +0000

    suzerain: add degenerate 1D typedefs
    Useful for tracking row- versus column- arrays
    within channel_mean.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@25652 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/precision.hpp
+++ apps/precision.hpp
@@ -42,62 +42,76 @@ typedef std::complex<real_t>  complex_t;
 namespace Eigen {
 
     // Typedefs for real-valued matrices
+    typedef Matrix<    real_t,       1,       1> Matrix1r ;
     typedef Matrix<    real_t,       2,       2> Matrix2r ;
     typedef Matrix<    real_t,       3,       3> Matrix3r ;
     typedef Matrix<    real_t,       4,       4> Matrix4r ;
+    typedef Matrix<    real_t,       1, Dynamic> Matrix1Xr;
     typedef Matrix<    real_t,       2, Dynamic> Matrix2Xr;
     typedef Matrix<    real_t,       3, Dynamic> Matrix3Xr;
     typedef Matrix<    real_t,       4, Dynamic> Matrix4Xr;
+    typedef Matrix<    real_t, Dynamic,       1> MatrixX1r;
     typedef Matrix<    real_t, Dynamic,       2> MatrixX2r;
     typedef Matrix<    real_t, Dynamic,       3> MatrixX3r;
     typedef Matrix<    real_t, Dynamic,       4> MatrixX4r;
     typedef Matrix<    real_t, Dynamic, Dynamic> MatrixXXr;
 
     // Typedefs for complex-valued matrices
+    typedef Matrix< complex_t,       1,       1> Matrix1c ;
     typedef Matrix< complex_t,       2,       2> Matrix2c ;
     typedef Matrix< complex_t,       3,       3> Matrix3c ;
     typedef Matrix< complex_t,       4,       4> Matrix4c ;
+    typedef Matrix< complex_t,       1, Dynamic> Matrix1Xc;
     typedef Matrix< complex_t,       2, Dynamic> Matrix2Xc;
     typedef Matrix< complex_t,       3, Dynamic> Matrix3Xc;
     typedef Matrix< complex_t,       4, Dynamic> Matrix4Xc;
+    typedef Matrix< complex_t, Dynamic,       1> MatrixX1c;
     typedef Matrix< complex_t, Dynamic,       2> MatrixX2c;
     typedef Matrix< complex_t, Dynamic,       3> MatrixX3c;
     typedef Matrix< complex_t, Dynamic,       4> MatrixX4c;
     typedef Matrix< complex_t, Dynamic, Dynamic> MatrixXXc;
 
     // Typedefs for real-valued arrays
+    typedef Array<    real_t,       1,       1> Array1r ;
     typedef Array<    real_t,       2,       1> Array2r ;
     typedef Array<    real_t,       3,       1> Array3r ;
     typedef Array<    real_t,       4,       1> Array4r ;
     typedef Array<    real_t, Dynamic,       1> ArrayXr ;
+    typedef Array<    real_t,       1, Dynamic> Array1Xr;
     typedef Array<    real_t,       2, Dynamic> Array2Xr;
     typedef Array<    real_t,       3, Dynamic> Array3Xr;
     typedef Array<    real_t,       4, Dynamic> Array4Xr;
+    typedef Array<    real_t, Dynamic,       1> ArrayX1r;
     typedef Array<    real_t, Dynamic,       2> ArrayX2r;
     typedef Array<    real_t, Dynamic,       3> ArrayX3r;
     typedef Array<    real_t, Dynamic,       4> ArrayX4r;
     typedef Array<    real_t, Dynamic, Dynamic> ArrayXXr;
 
     // Typedefs for complex-valued arrays
+    typedef Array< complex_t,       1,       1> Array1c ;
     typedef Array< complex_t,       2,       2> Array2c ;
     typedef Array< complex_t,       3,       3> Array3c ;
     typedef Array< complex_t,       4,       4> Array4c ;
     typedef Array< complex_t, Dynamic,       1> ArrayXc ;
+    typedef Array< complex_t,       1, Dynamic> Array1Xc;
     typedef Array< complex_t,       2, Dynamic> Array2Xc;
     typedef Array< complex_t,       3, Dynamic> Array3Xc;
     typedef Array< complex_t,       4, Dynamic> Array4Xc;
+    typedef Array< complex_t, Dynamic,       1> ArrayX1c;
     typedef Array< complex_t, Dynamic,       2> ArrayX2c;
     typedef Array< complex_t, Dynamic,       3> ArrayX3c;
     typedef Array< complex_t, Dynamic,       4> ArrayX4c;
     typedef Array< complex_t, Dynamic, Dynamic> ArrayXXc;
 
     // Typedefs for real-valued vectors
+    typedef Matrix<    real_t,       1, 1> Vector1r;
     typedef Matrix<    real_t,       2, 1> Vector2r;
     typedef Matrix<    real_t,       3, 1> Vector3r;
     typedef Matrix<    real_t,       4, 1> Vector4r;
     typedef Matrix<    real_t, Dynamic, 1> VectorXr;
 
     // Typedefs for complex-valued vectors
+    typedef Matrix< complex_t,       1, 1> Vector1c;
     typedef Matrix< complex_t,       2, 1> Vector2c;
     typedef Matrix< complex_t,       3, 1> Vector3c;
     typedef Matrix< complex_t,       4, 1> Vector4c;
