54e95816b5fc8e5e65f682ef3e6f5eb502153e8c
Rhys Ulerich <rhys@ices.utexas.edu>
2011-12-22 20:21:09 +0000

    suzerain: add empty test for valgrind suppression
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26511 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ tests/test_empty.cpp
@@ -0,0 +1,60 @@
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.hpp>
+#pragma hdrstop
+#define BOOST_TEST_MODULE $Id$
+#include <boost/test/included/unit_test.hpp>
+#include <suzerain/mpi.hpp>
+#include <suzerain/blas_et_al.hpp>
+#include "test_tools.hpp"
+
+// Files performs no strictly useful test work.
+// Rather it exercises a small portion of Boost.Test.
+// It is intended to be used to generate valgrind suppressions.
+
+BOOST_GLOBAL_FIXTURE(BlasCleanupFixture);       // Tear down BLAS
+
+struct MPIFixture {
+    MPIFixture()  { MPI_Init(NULL, NULL); }
+    ~MPIFixture() { MPI_Finalize(); }
+};
+BOOST_GLOBAL_FIXTURE(MPIFixture);               // Setup and tear down MPI
+
+
+BOOST_AUTO_TEST_SUITE(one_suite)
+
+BOOST_AUTO_TEST_CASE( totally_suite )
+{
+    BOOST_CHECK(true);
+    BOOST_TEST_PASSPOINT();
+    BOOST_TEST_MESSAGE("Twiddling thumbs");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(two_suite)
+
+BOOST_AUTO_TEST_CASE( super_suite )
+{
+    BOOST_REQUIRE(true);
+    BOOST_TEST_PASSPOINT();
+    BOOST_TEST_MESSAGE("Still twiddling thumbs...");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(tres_suite)
+
+typedef boost::mpl::list< double,float > test_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( super_suite, T, test_types )
+{
+    BOOST_REQUIRE_EQUAL(0, T());
+    BOOST_TEST_PASSPOINT();
+    BOOST_TEST_MESSAGE("Twiddling templated thumbs...");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
