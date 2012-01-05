3d0dcd4affe114d39acf80c795cbdb483e7e2941
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-05 23:20:25 +0000

    suzerain: #2207 first draft of gbdmv logic
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26691 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/gbdmv.c
@@ -0,0 +1,321 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2011 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * gbdmv.c: provides custom, BLAS-like general band matrix-vector operations
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+// File iteration is used to generate bandwidth-specific routines.  See
+// http://www.boost.org/libs/preprocessor/doc/topics/file_iteration.html for an
+// overview.  We iterate first to generate internal, static routines and then
+// continue on to the non-iterated logic.
+
+#if !defined(BOOST_PP_IS_ITERATING) || !BOOST_PP_IS_ITERATING
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.h>
+#pragma hdrstop
+#include <suzerain/gbdmv.h>
+
+#pragma warning(disable:1418 1572 2259)
+
+// ---------------------------------------------------------
+// Iterate on this file to generate fixed bandwidth routines
+// ---------------------------------------------------------
+#define BOOST_PP_ITERATION_LIMITS (0, 9)
+#define BOOST_PP_FILENAME_1 <suzerain/gbdmv.c>
+#include BOOST_PP_ITERATE()
+
+// -----------------------------------
+// Generate general bandwidth routines
+// -----------------------------------
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  suzerain_gbdmv_internal_s
+#define GBDMV_COMPONENT float
+#define GBDMV_SCALAR    float
+#define GBDMV_KL        const int kl,
+#define GBDMV_KU        const int ku,
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  suzerain_gbdmv_internal_d
+#define GBDMV_COMPONENT double
+#define GBDMV_SCALAR    double
+#define GBDMV_KL        const int kl,
+#define GBDMV_KU        const int ku,
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  suzerain_gbdmv_internal_sc
+#define GBDMV_COMPONENT float
+#define GBDMV_SCALAR    float _Complex
+#define GBDMV_KL        const int kl,
+#define GBDMV_KU        const int ku,
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  suzerain_gbdmv_internal_dz
+#define GBDMV_COMPONENT double
+#define GBDMV_SCALAR    double _Complex
+#define GBDMV_KL        const int kl,
+#define GBDMV_KU        const int ku,
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+// ------------------------------------------------------------------
+// Provide externally callable logic dispatching to internal routines
+// ------------------------------------------------------------------
+
+int
+suzerain_gbdmv_s(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const float alpha,
+        const float *d,
+        const float *a,
+        const int lda,
+        const float *x,
+        const int incx,
+        const float beta,
+        float *y,
+        const int incy)
+{
+    // Dispatch to fixed bandwidth specialization for small bandwidth...
+    if (kl == ku) {
+        switch (kl) {
+#define ARGS trans, n, alpha, d, a, lda, x, incx, beta, y, incy
+            case 0: return suzerain_gbdmv_internal_s0(ARGS);
+            case 1: return suzerain_gbdmv_internal_s1(ARGS);
+            case 2: return suzerain_gbdmv_internal_s2(ARGS);
+            case 3: return suzerain_gbdmv_internal_s3(ARGS);
+            case 4: return suzerain_gbdmv_internal_s4(ARGS);
+            case 5: return suzerain_gbdmv_internal_s5(ARGS);
+            case 6: return suzerain_gbdmv_internal_s6(ARGS);
+            case 7: return suzerain_gbdmv_internal_s7(ARGS);
+            case 8: return suzerain_gbdmv_internal_s8(ARGS);
+            case 9: return suzerain_gbdmv_internal_s9(ARGS);
+#undef ARGS
+        }
+    }
+
+    // ...otherwise employ a general bandwidth implementation
+    return suzerain_gbdmv_internal_s(trans, n, kl, ku,
+                                     alpha, d, a, lda, x, incx,
+                                     beta,             y, incy);
+}
+
+int
+suzerain_gbdmv_d(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const double alpha,
+        const double *d,
+        const double *a,
+        const int lda,
+        const double *x,
+        const int incx,
+        const double beta,
+        double *y,
+        const int incy)
+{
+    // Dispatch to fixed bandwidth specialization for small bandwidth...
+    if (kl == ku) {
+        switch (kl) {
+#define ARGS trans, n, alpha, d, a, lda, x, incx, beta, y, incy
+            case 0: return suzerain_gbdmv_internal_d0(ARGS);
+            case 1: return suzerain_gbdmv_internal_d1(ARGS);
+            case 2: return suzerain_gbdmv_internal_d2(ARGS);
+            case 3: return suzerain_gbdmv_internal_d3(ARGS);
+            case 4: return suzerain_gbdmv_internal_d4(ARGS);
+            case 5: return suzerain_gbdmv_internal_d5(ARGS);
+            case 6: return suzerain_gbdmv_internal_d6(ARGS);
+            case 7: return suzerain_gbdmv_internal_d7(ARGS);
+            case 8: return suzerain_gbdmv_internal_d8(ARGS);
+            case 9: return suzerain_gbdmv_internal_d9(ARGS);
+#undef ARGS
+        }
+    }
+
+    // ...otherwise employ a general bandwidth implementation
+    return suzerain_gbdmv_internal_d(trans, n, kl, ku,
+                                     alpha, d, a, lda, x, incx,
+                                     beta,             y, incy);
+}
+
+int
+suzerain_gbdmv_sc(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const float alpha[2],
+        const float *d,
+        const float *a,
+        const int lda,
+        const float (*x)[2],
+        const int incx,
+        const float beta[2],
+        float (*y)[2],
+        const int incy)
+{
+    float _Complex alpha_c, beta_c;
+    memcpy(&alpha_c, alpha, sizeof(float _Complex));
+    memcpy(&beta_c,  beta,  sizeof(float _Complex));
+
+    // Dispatch to fixed bandwidth specialization for small bandwidth...
+    if (kl == ku) {
+        switch (kl) {
+#define ARGS trans, n, alpha_c, (void *) d, (void *) a, lda, \
+             (void *) x, incx, beta_c, (void *) y, incy
+            case 0: return suzerain_gbdmv_internal_sc0(ARGS);
+            case 1: return suzerain_gbdmv_internal_sc1(ARGS);
+            case 2: return suzerain_gbdmv_internal_sc2(ARGS);
+            case 3: return suzerain_gbdmv_internal_sc3(ARGS);
+            case 4: return suzerain_gbdmv_internal_sc4(ARGS);
+            case 5: return suzerain_gbdmv_internal_sc5(ARGS);
+            case 6: return suzerain_gbdmv_internal_sc6(ARGS);
+            case 7: return suzerain_gbdmv_internal_sc7(ARGS);
+            case 8: return suzerain_gbdmv_internal_sc8(ARGS);
+            case 9: return suzerain_gbdmv_internal_sc9(ARGS);
+#undef ARGS
+        }
+    }
+
+    // ...otherwise employ a general bandwidth implementation
+    return suzerain_gbdmv_internal_sc(
+            trans, n, kl, ku,
+            alpha_c, (void *) d, (void *) a, lda, (void *) x, incx,
+            beta_c,                               (void *) y, incy);
+}
+
+int
+suzerain_gbdmv_dz(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const double alpha[2],
+        const double *d,
+        const double *a,
+        const int lda,
+        const double (*x)[2],
+        const int incx,
+        const double beta[2],
+        double (*y)[2],
+        const int incy)
+{
+    double _Complex alpha_c, beta_c;
+    memcpy(&alpha_c, alpha, sizeof(double _Complex));
+    memcpy(&beta_c,  beta,  sizeof(double _Complex));
+
+    // Dispatch to fixed bandwidth specialization for small bandwidth...
+    if (kl == ku) {
+        switch (kl) {
+#define ARGS trans, n, alpha_c, (void *) d, (void *) a, lda, \
+             (void *) x, incx, beta_c, (void *) y, incy
+            case 0: return suzerain_gbdmv_internal_dz0(ARGS);
+            case 1: return suzerain_gbdmv_internal_dz1(ARGS);
+            case 2: return suzerain_gbdmv_internal_dz2(ARGS);
+            case 3: return suzerain_gbdmv_internal_dz3(ARGS);
+            case 4: return suzerain_gbdmv_internal_dz4(ARGS);
+            case 5: return suzerain_gbdmv_internal_dz5(ARGS);
+            case 6: return suzerain_gbdmv_internal_dz6(ARGS);
+            case 7: return suzerain_gbdmv_internal_dz7(ARGS);
+            case 8: return suzerain_gbdmv_internal_dz8(ARGS);
+            case 9: return suzerain_gbdmv_internal_dz9(ARGS);
+#undef ARGS
+        }
+    }
+
+    // ...otherwise employ a general bandwidth implementation
+    return suzerain_gbdmv_internal_dz(
+            trans, n, kl, ku,
+            alpha_c, (void *) d, (void *) a, lda, (void *) x, incx,
+            beta_c,                               (void *) y, incy);
+}
+
+#else
+
+// ------------------------------------------------------------
+// Generate fixed bandwidth routines using BOOST_PP_ITERATION()
+// ------------------------------------------------------------
+#define kl BOOST_PP_ITERATION()
+#define ku BOOST_PP_ITERATION()
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  BOOST_PP_CAT(suzerain_gbdmv_internal_s, \
+                                     BOOST_PP_ITERATION())
+#define GBDMV_COMPONENT float
+#define GBDMV_SCALAR    float
+#define GBDMV_KL
+#define GBDMV_KU
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  BOOST_PP_CAT(suzerain_gbdmv_internal_d, \
+                                     BOOST_PP_ITERATION())
+#define GBDMV_COMPONENT double
+#define GBDMV_SCALAR    double
+#define GBDMV_KL
+#define GBDMV_KU
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  BOOST_PP_CAT(suzerain_gbdmv_internal_sc, \
+                                     BOOST_PP_ITERATION())
+#define GBDMV_COMPONENT float
+#define GBDMV_SCALAR    float _Complex
+#define GBDMV_KL
+#define GBDMV_KU
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#define GBDMV_STATIC    static
+#define GBDMV_FUNCTION  BOOST_PP_CAT(suzerain_gbdmv_internal_dz, \
+                                     BOOST_PP_ITERATION())
+#define GBDMV_COMPONENT double
+#define GBDMV_SCALAR    double _Complex
+#define GBDMV_KL
+#define GBDMV_KU
+#define GBDMV_LDA       const int lda,
+#include "gbdmv.def"
+
+#undef kl
+#undef ku
+
+#endif
---
+++ suzerain/gbdmv.h
@@ -0,0 +1,189 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2011 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * gbdmv.h: provides custom, BLAS-like general band matrix-vector operations
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_GBDMV_H__
+#define __SUZERAIN_GBDMV_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** \file
+ * Provides custom, BLAS-like diagonal matrix times general band matrix-vector
+ * operations.  Includes mixed real/complex operations and fixed bandwidth
+ * kernels.
+ */
+
+/*!
+ * \brief Compute \f$ y \leftarrow{} \alpha{} D A x + \beta{} y \f$.
+ *
+ * Transposes of \f$ A \f$ can be taken using the \c trans parameter.
+ *
+ * \param trans One of 'N', 'T', or 'C' for no transpose, a transpose,
+ *      or a conjugate transpose, respectively.
+ * \param n Dimension of all matrices and vectors.
+ * \param kl Number of subdiagonals in band storage of \c a.
+ * \param ku Number of superdiagonals in band storage of \c a.
+ * \param alpha Multiplicative scalar \f$ \alpha \f$.
+ * \param d Contiguous storage for diagonal matrix \f$ D \f$.
+ * \param a General band storage for matrix \f$ A \f$.
+ * \param lda Leading dimension of \c a.
+ * \param x Vector to be multiplied.
+ * \param incx Stride of vector \c x.
+ * \param beta Multiplicative scalar \f$ \beta \f$.
+ * \param y Vector to be added to product and to contain result.
+ * \param incy Stride of vector \c y.
+ *
+ * \return Zero on success and a BLAS-like error code otherwise.
+ * \see A BLAS reference for more details, especially for general
+ *      band storage matrix requirements.
+ */
+int
+suzerain_gbdmv_s(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const float alpha,
+        const float *d,
+        const float *a,
+        const int lda,
+        const float *x,
+        const int incx,
+        const float beta,
+        float *y,
+        const int incy);
+
+/*! \copydoc suzerain_gbdmv_s */
+int
+suzerain_gbdmv_d(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const double alpha,
+        const double *d,
+        const double *a,
+        const int lda,
+        const double *x,
+        const int incx,
+        const double beta,
+        double *y,
+        const int incy);
+
+/*!
+ * \brief Compute \f$ y \leftarrow{} \alpha{} D A x + \beta{} y \f$ for complex
+ * \f$\alpha{}\f$, \f$x\f$, \f$\beta\f$, and \f$y\f$ but real-valued \f$D\f$
+ * and \f$A\f$.  Real-valued strides are in units of <tt>float</tt> while
+ * complex-valued strides are in units of <tt>float[2]</tt>.
+ *
+ * Transposes of \f$ A \f$ can be taken using the \c trans parameter.
+ *
+ * \param trans One of 'N', 'T', or 'C' for no transpose, a transpose,
+ *      or a conjugate transpose, respectively.
+ * \param n Dimension of all matrices and vectors.
+ * \param kl Number of subdiagonals in band storage of \c a.
+ * \param ku Number of superdiagonals in band storage of \c a.
+ * \param alpha Multiplicative scalar \f$ \alpha \f$.
+ * \param d Contiguous storage for diagonal matrix \f$ D \f$.
+ * \param a General band storage for matrix \f$ A \f$.
+ * \param lda Leading dimension of \c a in units of <tt>float</tt>.
+ * \param x Vector to be multiplied.
+ * \param incx Stride of vector \c x in units of <tt>float[2]</tt>.
+ * \param beta Multiplicative scalar \f$ \beta \f$.
+ * \param y Vector to be added to product and to contain result.
+ * \param incy Stride of vector \c y in units of <tt>float[2]</tt>.
+ *
+ * \return Zero on success and a BLAS-like error code otherwise.
+ * \see A BLAS reference for for general band storage matrix requirements.
+ */
+int
+suzerain_gbdmv_sc(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const float alpha[2],
+        const float *d,
+        const float *a,
+        const int lda,
+        const float (*x)[2],
+        const int incx,
+        const float beta[2],
+        float (*y)[2],
+        const int incy);
+
+/*!
+ * \brief Compute \f$ y \leftarrow{} \alpha{} A x + \beta{} y \f$ for complex
+ * \f$\alpha{}\f$, \f$x\f$, \f$\beta\f$, and \f$y\f$ but real-valued \f$D\f$
+ * and \f$A\f$.  Real-valued strides are in units of <tt>double</tt> while
+ * complex-valued strides are in units of <tt>double[2]</tt>.
+ *
+ * Transposes of \f$ A \f$ can be taken using the \c trans parameter.
+ *
+ * \param trans One of 'N', 'T', or 'C' for no transpose, a transpose,
+ *      or a conjugate transpose, respectively.
+ * \param n Dimension of all matrices and vectors.
+ * \param kl Number of subdiagonals in band storage of \c a.
+ * \param ku Number of superdiagonals in band storage of \c a.
+ * \param alpha Multiplicative scalar \f$ \alpha \f$.
+ * \param d Contiguous storage for diagonal matrix \f$ D \f$.
+ * \param a General band storage for matrix \f$ A \f$.
+ * \param lda Leading dimension of \c a in units of <tt>double</tt>.
+ * \param x Vector to be multiplied.
+ * \param incx Stride of vector \c x in units of <tt>double[2]</tt>.
+ * \param beta Multiplicative scalar \f$ \beta \f$.
+ * \param y Vector to be added to product and to contain result.
+ * \param incy Stride of vector \c y in units of <tt>double[2]</tt>.
+ *
+ * \return Zero on success and a BLAS-like error code otherwise.
+ * \see A BLAS reference for for general band storage matrix requirements.
+ */
+int
+suzerain_gbdmv_dz(
+        const char trans,
+        const int n,
+        const int kl,
+        const int ku,
+        const double alpha[2],
+        const double *d,
+        const double *a,
+        const int lda,
+        const double (*x)[2],
+        const int incx,
+        const double beta[2],
+        double (*y)[2],
+        const int incy);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __SUZERAIN_GBDMV_H__ */
