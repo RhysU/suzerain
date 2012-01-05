e8f864cc6da6fb8a1d4b29690b86c8bd091c426e
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-05 17:55:05 +0000

    suzerain: #2123 fixed bandwidth tests
    Upping coverage on the kl = ku = something test cases.
    Also tweaks to get better error messages by naming the autogenerated
    test cases.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26676 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_gbmv.cpp
+++ tests/test_gbmv.cpp
@@ -12,7 +12,10 @@
 
 BOOST_GLOBAL_FIXTURE(BlasCleanupFixture);
 
+using boost::unit_test::framework::master_test_suite;
+using boost::unit_test::make_test_case;
 using std::numeric_limits;
+using std::size_t;
 
 // Test suzerain_gbmv_?  against suzerain_blas_*gbmv_external
 // Test suzerain_gbmb_?? against suzerain_blasext_?gbmzv_external
@@ -86,8 +89,6 @@ std::basic_ostream<charT,traits>& operator<<(
 
 static void test_gbmv_s(const gbmv_tc_type& t)
 {
-    BOOST_TEST_MESSAGE("\tscenario " << t);
-
     const float close_enough = numeric_limits<float>::epsilon()*t.m*t.n*15;
     const float inv_rand_max = float(1) / RAND_MAX;
     const int lena = t.lda * t.n;
@@ -124,8 +125,6 @@ static void test_gbmv_s(const gbmv_tc_type& t)
 
 static void test_gbmv_d(const gbmv_tc_type& t)
 {
-    BOOST_TEST_MESSAGE("\tscenario " << t);
-
     const double close_enough = numeric_limits<double>::epsilon()*t.m*t.n*15;
     const double inv_rand_max = double(1) / RAND_MAX;
     const int lena = t.lda * t.n;
@@ -158,9 +157,7 @@ static void test_gbmv_d(const gbmv_tc_type& t)
 
 static void test_gbmv_sc(const gbmzv_tc_type& t)
 {
-    BOOST_TEST_MESSAGE("\tscenario " << t);
-
-    const float close_enough = numeric_limits<float>::epsilon()*t.m*t.n*15;
+    const float close_enough = numeric_limits<float>::epsilon()*t.m*t.n*20;
     const float inv_rand_max = float(1) / RAND_MAX;
     const int lena = t.lda * t.n;
     const int lenx = 2 * abs(t.incx) * (toupper(t.trans) == 'N' ? t.n : t.m);
@@ -197,9 +194,7 @@ static void test_gbmv_sc(const gbmzv_tc_type& t)
 
 static void test_gbmv_dz(const gbmzv_tc_type& t)
 {
-    BOOST_TEST_MESSAGE("\tscenario " << t);
-
-    const double close_enough = numeric_limits<double>::epsilon()*t.m*t.n*15;
+    const double close_enough = numeric_limits<double>::epsilon()*t.m*t.n*25;
     const double inv_rand_max = double(1) / RAND_MAX;
     const int lena = t.lda * t.n;
     const int lenx = 2 * abs(t.incx) * (toupper(t.trans) == 'N' ? t.n : t.m);
@@ -235,7 +230,11 @@ init_unit_test_suite( int argc, char* argv[] )
 {
     (void) argc; // Unused
     (void) argv; // Unused
-    boost::unit_test::framework::master_test_suite().p_name.value = __FILE__;
+    master_test_suite().p_name.value = __FILE__;
+
+    // -------------------------------------------------------
+    // Register test cases designed for generalized bandwidths
+    // -------------------------------------------------------
 
     const gbmv_tc_type gbmv_tc[] = {
         // trans,  m,  n, kl, ku, alpha, lda, incx, beta, incy
@@ -369,56 +368,213 @@ init_unit_test_suite( int argc, char* argv[] )
         ,{   'T',  4,  5,  3,  4,   5.0,   9,    3, 11.0,   -2}
         ,{   'T', 19, 17,  3,  4,   0.0,   9,    3,  1.0,    1} // Quick
     };
-    const std::size_t ncases = sizeof(gbmv_tc)/sizeof(gbmv_tc[0]);
+    const size_t gcases = sizeof(gbmv_tc)/sizeof(gbmv_tc[0]);
 
     // Register test_gbmv_s cases
-    boost::unit_test::framework::master_test_suite().add(
-            BOOST_PARAM_TEST_CASE(&test_gbmv_s, gbmv_tc, gbmv_tc + ncases));
-
-    // Register test_gbmv_d cases
-    boost::unit_test::framework::master_test_suite().add(
-            BOOST_PARAM_TEST_CASE(&test_gbmv_d, gbmv_tc, gbmv_tc + ncases));
-
-    // Register test_gbmv_sc
-    for (std::size_t i = 0; i < ncases; ++i) {
-        gbmzv_tc_type t(gbmv_tc[i]);
-
-        // Real-valued alpha, beta
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_sc, &t, &t + 1));
-
-        // Imaginary-valued alpha, beta
-        std::swap(t.alpha[0], t.alpha[1]);
-        std::swap(t.beta[0],  t.beta[1]);
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_sc, &t, &t + 1));
-
-        // Truly complex alpha, beta
-        t.alpha[0] += 1;
-        t.beta[0]  -= 1;
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_sc, &t, &t + 1));
+    for (size_t i = 0; i < gcases; ++i) {
+        std::ostringstream name;
+        name << BOOST_TEST_STRINGIZE(test_gbmv_s) << ' ' << gbmv_tc[i];
+        master_test_suite().add(make_test_case(
+                &test_gbmv_s, name.str(), gbmv_tc + i, gbmv_tc + i + 1));
     }
 
-    // Register test_gbmv_dz
-    for (std::size_t i = 0; i < ncases; ++i) {
-        gbmzv_tc_type t(gbmv_tc[i]);
+    // Register test_gbmv_d cases
+    for (size_t i = 0; i < gcases; ++i) {
+        std::ostringstream name;
+        name << BOOST_TEST_STRINGIZE(test_gbmv_d) << ' ' << gbmv_tc[i];
+        master_test_suite().add(make_test_case(
+                &test_gbmv_d, name.str(), gbmv_tc + i, gbmv_tc + i + 1));
+    }
 
-        // Real-valued alpha, beta
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_dz, &t, &t + 1));
+    // Register test_gbmv_sc cases
+    for (size_t i = 0; i < gcases; ++i) {
 
-        // Imaginary-valued alpha, beta
-        std::swap(t.alpha[0], t.alpha[1]);
-        std::swap(t.beta[0],  t.beta[1]);
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_dz, &t, &t + 1));
+        gbmzv_tc_type c(gbmv_tc[i]);
 
-        // Truly complex alpha, beta
-        t.alpha[0] += 1;
-        t.beta[0]  -= 1;
-        boost::unit_test::framework::master_test_suite().add(
-                BOOST_PARAM_TEST_CASE(&test_gbmv_dz, &t, &t + 1));
+        { // Real-valued alpha, beta
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " real " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_sc, name.str(), &c, &c + 1));
+        }
+
+        { // Imaginary-valued alpha, beta
+            std::swap(c.alpha[0], c.alpha[1]);
+            std::swap(c.beta[0],  c.beta[1]);
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " imag " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_sc, name.str(), &c, &c + 1));
+        }
+
+        { // Truly complex alpha, beta
+            c.alpha[0] += 1.5;
+            c.beta[0]  -= 1.5;
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " complex " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_sc, name.str(), &c, &c + 1));
+        }
+    }
+
+    // Register test_gbmv_dz cases
+    for (size_t i = 0; i < gcases; ++i) {
+
+        gbmzv_tc_type c(gbmv_tc[i]);
+
+        { // Real-valued alpha, beta
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " real " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_dz, name.str(), &c, &c + 1));
+        }
+
+        { // Imaginary-valued alpha, beta
+            std::swap(c.alpha[0], c.alpha[1]);
+            std::swap(c.beta[0],  c.beta[1]);
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " imag " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_dz, name.str(), &c, &c + 1));
+        }
+
+        { // Truly complex alpha, beta
+            c.alpha[0] += 1.5;
+            c.beta[0]  -= 1.5;
+            std::ostringstream name;
+            name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " complex " << c;
+            master_test_suite().add(make_test_case(
+                    &test_gbmv_dz, name.str(), &c, &c + 1));
+        }
+    }
+
+    // -------------------------------------------------------
+    // Register test cases designed for fixed bandwidths
+    // -------------------------------------------------------
+
+    const gbmv_tc_type fixed_tc[] = {
+        // kl, ku to be set while lda is a delta
+        // trans,  m,  n, kl, ku, alpha, lda, incx, beta, incy
+         {   'N', 17, 19,  0,  0,  -5.0,   1,    1,  0.0,    1} // Regular
+        ,{   'N', 17, 19,  0,  0,   5.0,   1,   -1, 11.0,    1}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   0,    1,  0.0,    1}
+        ,{   'N', 17, 19,  0,  0,   5.0,   0,    1, 11.0,   -1}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   1,    1,  0.0,    2}
+        ,{   'N', 17, 19,  0,  0,   5.0,   1,   -1, 11.0,    2}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   0,    1,  0.0,    2}
+        ,{   'N', 17, 19,  0,  0,   5.0,   0,    1, 11.0,   -2}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   1,    3,  0.0,    1}
+        ,{   'N', 17, 19,  0,  0,   5.0,   1,   -3, 11.0,    1}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   0,    3,  0.0,    1}
+        ,{   'N', 17, 19,  0,  0,   5.0,   0,    3, 11.0,   -1}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   1,    3,  0.0,    2}
+        ,{   'N', 17, 19,  0,  0,   5.0,   1,   -3, 11.0,    2}
+        ,{   'N', 17, 19,  0,  0,  -5.0,   0,    3,  0.0,    2}
+        ,{   'N', 17, 19,  0,  0,   5.0,   0,    3, 11.0,   -2}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   1,    1,  0.0,    1}
+        ,{   'T', 17, 19,  0,  0,   5.0,   1,   -1, 11.0,    1}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   0,    1,  0.0,    1}
+        ,{   'T', 17, 19,  0,  0,   5.0,   0,    1, 11.0,   -1}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   1,    1,  0.0,    2}
+        ,{   'T', 17, 19,  0,  0,   5.0,   1,   -1, 11.0,    2}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   0,    1,  0.0,    2}
+        ,{   'T', 17, 19,  0,  0,   5.0,   0,    1, 11.0,   -2}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   1,    3,  0.0,    1}
+        ,{   'T', 17, 19,  0,  0,   5.0,   1,   -3, 11.0,    1}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   0,    3,  0.0,    1}
+        ,{   'T', 17, 19,  0,  0,   5.0,   0,    3, 11.0,   -1}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   1,    3,  0.0,    2}
+        ,{   'T', 17, 19,  0,  0,   5.0,   1,   -3, 11.0,    2}
+        ,{   'T', 17, 19,  0,  0,  -5.0,   0,    3,  0.0,    2}
+        ,{   'T', 17, 19,  0,  0,   5.0,   0,    3, 11.0,   -2}
+        ,{   'T', 19, 17,  0,  0,   0.0,   2,    3,  1.0,    1} // Quick
+    };
+
+    // Loop over fixed kl = ku = k bandwidths
+    const size_t max_fixed_bandwidth = 9;
+    for (size_t k = 0; k < max_fixed_bandwidth; ++k) {
+
+        // Loop over fixed_tc and register both real and complex tests
+        for (size_t i = 0; i < sizeof(fixed_tc)/sizeof(fixed_tc[0]); ++i) {
+
+            // Prepare real-valued test details for bandwidth k
+            gbmv_tc_type r(fixed_tc[i]);
+            r.kl   = r.ku = k;
+            r.lda += (r.kl + r.ku + 1);
+
+            { // Register test_gbmv_s case
+                std::ostringstream name;
+                name << BOOST_TEST_STRINGIZE(test_gbmv_s) << ' ' << gbmv_tc[i];
+                master_test_suite().add(make_test_case(
+                        &test_gbmv_s, name.str(), &r, &r + 1));
+            }
+
+            { // Register test_gbmv_d case
+                std::ostringstream name;
+                name << BOOST_TEST_STRINGIZE(test_gbmv_d) << ' ' << gbmv_tc[i];
+                master_test_suite().add(make_test_case(
+                        &test_gbmv_d, name.str(), &r, &r + 1));
+            }
+
+            { // Register test_gbmv_sc cases
+                gbmzv_tc_type c(r);
+
+                { // Real-valued alpha, beta
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " real " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_sc, name.str(), &c, &c + 1));
+                }
+
+                { // Imaginary-valued alpha, beta
+                    std::swap(c.alpha[0], c.alpha[1]);
+                    std::swap(c.beta[0],  c.beta[1]);
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " imag " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_sc, name.str(), &c, &c + 1));
+                }
+
+                { // Truly complex alpha, beta
+                    c.alpha[0] += 1.5;
+                    c.beta[0]  -= 1.5;
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_sc) << " complex " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_sc, name.str(), &c, &c + 1));
+                }
+            }
+
+            { // Register test_gbmv_dz cases
+                gbmzv_tc_type c(r);
+
+                { // Real-valued alpha, beta
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " real " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_dz, name.str(), &c, &c + 1));
+                }
+
+                { // Imaginary-valued alpha, beta
+                    std::swap(c.alpha[0], c.alpha[1]);
+                    std::swap(c.beta[0],  c.beta[1]);
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " imag " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_dz, name.str(), &c, &c + 1));
+                }
+
+                { // Truly complex alpha, beta
+                    c.alpha[0] += 1.5;
+                    c.beta[0]  -= 1.5;
+                    std::ostringstream name;
+                    name << BOOST_TEST_STRINGIZE(test_gbmv_dz) << " complex " << c;
+                    master_test_suite().add(make_test_case(
+                            &test_gbmv_dz, name.str(), &c, &c + 1));
+                }
+            }
+
+        }
     }
 
     return 0;
