6a517e16aab3130d1d6afaff9a2dd678eaed1876
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-06 23:54:39 +0000

    suzerain: #2208 unroll to kl = ku = 15
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26777 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/gbddmv.c
+++ suzerain/gbddmv.c
@@ -47,7 +47,7 @@
 // ---------------------------------------------------------
 // Iterate on this file to generate fixed bandwidth routines
 // ---------------------------------------------------------
-#define BOOST_PP_ITERATION_LIMITS (0, 9)
+#define BOOST_PP_ITERATION_LIMITS (0, 15)
 #define BOOST_PP_FILENAME_1 <suzerain/gbddmv.c>
 #include BOOST_PP_ITERATE()
 
@@ -117,16 +117,22 @@ suzerain_gbddmv_s(
     if (kl == ku) {
         switch (kl) {
 #define ARGS trans, n, alpha0, d0, alpha1, d1, a, lda, x, incx, beta, y, incy
-            case 0: return suzerain_gbddmv_internal_s0(ARGS);
-            case 1: return suzerain_gbddmv_internal_s1(ARGS);
-            case 2: return suzerain_gbddmv_internal_s2(ARGS);
-            case 3: return suzerain_gbddmv_internal_s3(ARGS);
-            case 4: return suzerain_gbddmv_internal_s4(ARGS);
-            case 5: return suzerain_gbddmv_internal_s5(ARGS);
-            case 6: return suzerain_gbddmv_internal_s6(ARGS);
-            case 7: return suzerain_gbddmv_internal_s7(ARGS);
-            case 8: return suzerain_gbddmv_internal_s8(ARGS);
-            case 9: return suzerain_gbddmv_internal_s9(ARGS);
+            case  0: return suzerain_gbddmv_internal_s0(ARGS);
+            case  1: return suzerain_gbddmv_internal_s1(ARGS);
+            case  2: return suzerain_gbddmv_internal_s2(ARGS);
+            case  3: return suzerain_gbddmv_internal_s3(ARGS);
+            case  4: return suzerain_gbddmv_internal_s4(ARGS);
+            case  5: return suzerain_gbddmv_internal_s5(ARGS);
+            case  6: return suzerain_gbddmv_internal_s6(ARGS);
+            case  7: return suzerain_gbddmv_internal_s7(ARGS);
+            case  8: return suzerain_gbddmv_internal_s8(ARGS);
+            case  9: return suzerain_gbddmv_internal_s9(ARGS);
+            case 10: return suzerain_gbddmv_internal_s10(ARGS);
+            case 11: return suzerain_gbddmv_internal_s11(ARGS);
+            case 12: return suzerain_gbddmv_internal_s12(ARGS);
+            case 13: return suzerain_gbddmv_internal_s13(ARGS);
+            case 14: return suzerain_gbddmv_internal_s14(ARGS);
+            case 15: return suzerain_gbddmv_internal_s15(ARGS);
 #undef ARGS
         }
     }
@@ -160,16 +166,22 @@ suzerain_gbddmv_d(
     if (kl == ku) {
         switch (kl) {
 #define ARGS trans, n, alpha0, d0, alpha1, d1, a, lda, x, incx, beta, y, incy
-            case 0: return suzerain_gbddmv_internal_d0(ARGS);
-            case 1: return suzerain_gbddmv_internal_d1(ARGS);
-            case 2: return suzerain_gbddmv_internal_d2(ARGS);
-            case 3: return suzerain_gbddmv_internal_d3(ARGS);
-            case 4: return suzerain_gbddmv_internal_d4(ARGS);
-            case 5: return suzerain_gbddmv_internal_d5(ARGS);
-            case 6: return suzerain_gbddmv_internal_d6(ARGS);
-            case 7: return suzerain_gbddmv_internal_d7(ARGS);
-            case 8: return suzerain_gbddmv_internal_d8(ARGS);
-            case 9: return suzerain_gbddmv_internal_d9(ARGS);
+            case  0: return suzerain_gbddmv_internal_d0(ARGS);
+            case  1: return suzerain_gbddmv_internal_d1(ARGS);
+            case  2: return suzerain_gbddmv_internal_d2(ARGS);
+            case  3: return suzerain_gbddmv_internal_d3(ARGS);
+            case  4: return suzerain_gbddmv_internal_d4(ARGS);
+            case  5: return suzerain_gbddmv_internal_d5(ARGS);
+            case  6: return suzerain_gbddmv_internal_d6(ARGS);
+            case  7: return suzerain_gbddmv_internal_d7(ARGS);
+            case  8: return suzerain_gbddmv_internal_d8(ARGS);
+            case  9: return suzerain_gbddmv_internal_d9(ARGS);
+            case 10: return suzerain_gbddmv_internal_d10(ARGS);
+            case 11: return suzerain_gbddmv_internal_d11(ARGS);
+            case 12: return suzerain_gbddmv_internal_d12(ARGS);
+            case 13: return suzerain_gbddmv_internal_d13(ARGS);
+            case 14: return suzerain_gbddmv_internal_d14(ARGS);
+            case 15: return suzerain_gbddmv_internal_d15(ARGS);
 #undef ARGS
         }
     }
@@ -209,16 +221,22 @@ suzerain_gbddmv_sc(
         switch (kl) {
 #define ARGS trans, n, alpha0_c, (void *) d0, alpha1_c, (void *) d1,    \
             (void *) a, lda, (void *) x, incx, beta_c, (void *) y, incy
-            case 0: return suzerain_gbddmv_internal_sc0(ARGS);
-            case 1: return suzerain_gbddmv_internal_sc1(ARGS);
-            case 2: return suzerain_gbddmv_internal_sc2(ARGS);
-            case 3: return suzerain_gbddmv_internal_sc3(ARGS);
-            case 4: return suzerain_gbddmv_internal_sc4(ARGS);
-            case 5: return suzerain_gbddmv_internal_sc5(ARGS);
-            case 6: return suzerain_gbddmv_internal_sc6(ARGS);
-            case 7: return suzerain_gbddmv_internal_sc7(ARGS);
-            case 8: return suzerain_gbddmv_internal_sc8(ARGS);
-            case 9: return suzerain_gbddmv_internal_sc9(ARGS);
+            case  0: return suzerain_gbddmv_internal_sc0(ARGS);
+            case  1: return suzerain_gbddmv_internal_sc1(ARGS);
+            case  2: return suzerain_gbddmv_internal_sc2(ARGS);
+            case  3: return suzerain_gbddmv_internal_sc3(ARGS);
+            case  4: return suzerain_gbddmv_internal_sc4(ARGS);
+            case  5: return suzerain_gbddmv_internal_sc5(ARGS);
+            case  6: return suzerain_gbddmv_internal_sc6(ARGS);
+            case  7: return suzerain_gbddmv_internal_sc7(ARGS);
+            case  8: return suzerain_gbddmv_internal_sc8(ARGS);
+            case  9: return suzerain_gbddmv_internal_sc9(ARGS);
+            case 10: return suzerain_gbddmv_internal_sc10(ARGS);
+            case 11: return suzerain_gbddmv_internal_sc11(ARGS);
+            case 12: return suzerain_gbddmv_internal_sc12(ARGS);
+            case 13: return suzerain_gbddmv_internal_sc13(ARGS);
+            case 14: return suzerain_gbddmv_internal_sc14(ARGS);
+            case 15: return suzerain_gbddmv_internal_sc15(ARGS);
 #undef ARGS
         }
     }
@@ -259,16 +277,22 @@ suzerain_gbddmv_dz(
         switch (kl) {
 #define ARGS trans, n, alpha0_c, (void *) d0, alpha1_c, (void *) d1,    \
             (void *) a, lda, (void *) x, incx, beta_c, (void *) y, incy
-            case 0: return suzerain_gbddmv_internal_dz0(ARGS);
-            case 1: return suzerain_gbddmv_internal_dz1(ARGS);
-            case 2: return suzerain_gbddmv_internal_dz2(ARGS);
-            case 3: return suzerain_gbddmv_internal_dz3(ARGS);
-            case 4: return suzerain_gbddmv_internal_dz4(ARGS);
-            case 5: return suzerain_gbddmv_internal_dz5(ARGS);
-            case 6: return suzerain_gbddmv_internal_dz6(ARGS);
-            case 7: return suzerain_gbddmv_internal_dz7(ARGS);
-            case 8: return suzerain_gbddmv_internal_dz8(ARGS);
-            case 9: return suzerain_gbddmv_internal_dz9(ARGS);
+            case  0: return suzerain_gbddmv_internal_dz0(ARGS);
+            case  1: return suzerain_gbddmv_internal_dz1(ARGS);
+            case  2: return suzerain_gbddmv_internal_dz2(ARGS);
+            case  3: return suzerain_gbddmv_internal_dz3(ARGS);
+            case  4: return suzerain_gbddmv_internal_dz4(ARGS);
+            case  5: return suzerain_gbddmv_internal_dz5(ARGS);
+            case  6: return suzerain_gbddmv_internal_dz6(ARGS);
+            case  7: return suzerain_gbddmv_internal_dz7(ARGS);
+            case  8: return suzerain_gbddmv_internal_dz8(ARGS);
+            case  9: return suzerain_gbddmv_internal_dz9(ARGS);
+            case 10: return suzerain_gbddmv_internal_dz10(ARGS);
+            case 11: return suzerain_gbddmv_internal_dz11(ARGS);
+            case 12: return suzerain_gbddmv_internal_dz12(ARGS);
+            case 13: return suzerain_gbddmv_internal_dz13(ARGS);
+            case 14: return suzerain_gbddmv_internal_dz14(ARGS);
+            case 15: return suzerain_gbddmv_internal_dz15(ARGS);
 #undef ARGS
         }
     }
