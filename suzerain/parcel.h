82bb867f7ff80789dd4aebd9e08ae634374368ef
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-12 20:03:22 +0000

    suzerain: add parcel.h
    See http://stackoverflow.com/questions/8839566/ for motivation.
    Intent is to eventually sue this within the B-spline workspace routines.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26929 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/parcel.h
@@ -0,0 +1,168 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2012 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * parcel.h: utilities for parcelling out monolithically malloc-ed memory
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+#include <inttypes.h>
+#include <stddef.h>
+#include <boost/preprocessor.hpp>
+
+#ifndef __SUZERAIN_PARCEL_H__
+#define __SUZERAIN_PARCEL_H__
+
+#ifdef __cplusplus
+# error "SUZERAIN_PARCEL implementation cannot be used in C++ code"
+#endif
+
+// TODO Employ malloc guarantees of alignment of __alignof__(double)
+// Doing so on the first decl will allow it to have no padding when
+// minalign < __alignof__(double).
+
+/**
+ * Perform a single malloc that will be parceled out into multiple logical
+ * regions in a type- and alignment-safe manner.  Macro arguments are
+ * <pre>
+ *   ptr      - pointer name (check for NULL and free appropriately)
+ *   malloc   - malloc(3)-like function used for allocation
+ *   decl_seq - a Boost.Preprocessor seq of "decl"s.
+ * </pre>
+ *
+ * A "decl" is a tuple (varname, datatype, varcount, varqual, minalign)
+ * where
+ * <pre>
+ *   varname  - pointer variable name (e.g. my_double_ptr)
+ *   datatype - variable type (e.g. 'double')
+ *   varcount - number of elements of type datatype to allocate (e.g. 1, 5)
+ *   varqual  - qualifiers to add to pointer variable (e.g. const, restrict)
+ *   minalign - minimum allowable alignment of data in bytes
+ * </pre>
+ *
+ * One sample invocation is
+ * <pre>
+ *   SUZERAIN_PARCEL(p, malloc, ((c, char,   3, const,     0))
+ *                              ((d, double, 5, restrict, 16))
+ *                              ((i, int,    1,         ,  0)));
+ * </pre>
+ * where the extra parenthesis are crucial to make a sequence of decls.  This
+ * invocation allocates a single buffer of sufficient size to hold a char[3], a
+ * double[5], and a single int.  The buffer starts at p and is of size p_parcel
+ * bytes.  c is a const pointer to a naturally aligned location for the
+ * char[3], d points to a 16-byte aligned, restrict-ed location for the
+ * double[5], and i points to a naturally aligned location for a single int.
+ * The three pointers have increasing addresses.  The buffer spans [p, p + sz).
+ * malloc(3) was used for the allocation but any other unary allocation
+ * function may be supplied.  The entire work buffer should be deallocated
+ * using free(p).
+ *
+ * The packing is suboptimal and the memory footprint slightly larger compared
+ * to using a struct with a single flexible array member.  That is, either
+ * <pre>
+ *   struct { char c[3], int i, double d[]; } s;
+ * </pre>
+ * or
+ * <pre>
+ *   struct { double d[5], int i, char c[]; } s;
+ * </pre>
+ * is more efficient from a memory footprint perspective.  This is because we
+ * must assume a worst-case allocation and cannot pack members.  The upside of
+ * SUZERAIN_PARCEL is that multiple arrays may be allocated via one malloc(3)
+ * call in a way that satisfies their alignment needs.
+ *
+ * Ideas lifted verbatim from http://stackoverflow.com/questions/227897/.
+ * Attempting to find a cleaner solution via
+ * http://stackoverflow.com/questions/8839566/.  These macros generate
+ * code which the compiler should trivially optimize into the ground.
+ */
+#define SUZERAIN_PARCEL(ptr, malloc, decl_seq)                                     \
+    BOOST_PP_SEQ_FOR_EACH(SUZERAIN_PARCEL_DATASIZE,,decl_seq)                      \
+    BOOST_PP_SEQ_FOR_EACH(SUZERAIN_PARCEL_ALIGNMENT,,decl_seq)                     \
+    BOOST_PP_SEQ_FOR_EACH(SUZERAIN_PARCEL_PAD,,decl_seq)                           \
+    BOOST_PP_SEQ_FOR_EACH(SUZERAIN_PARCEL_MASK,,decl_seq)                          \
+    const size_t BOOST_PP_CAT(ptr,_parcel)                                         \
+        = BOOST_PP_SEQ_FOR_EACH(SUZERAIN_PARCEL_SIZESUM,,decl_seq);                \
+    void * const ptr = malloc(BOOST_PP_CAT(ptr,_parcel))                           \
+    BOOST_PP_TUPLE_ELEM(3, 2, BOOST_PP_SEQ_FOLD_LEFT(SUZERAIN_PARCEL_OP,           \
+        ((char*) ptr                                                               \
+         BOOST_PP_COMMA() 0                                                        \
+         BOOST_PP_COMMA()), decl_seq))
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#define SUZERAIN_PARCEL_APPLY(macro, tuple_args) macro tuple_args
+
+#define SUZERAIN_PARCEL_DATASIZE(r,data,elem)                                      \
+    SUZERAIN_PARCEL_APPLY(SUZERAIN_PARCEL_DATASIZE_,elem)
+#define SUZERAIN_PARCEL_DATASIZE_(varname, datatype, varcount, varqual, minalign)  \
+    const size_t BOOST_PP_CAT(_PARCEL_datasize_,varname)                           \
+        = varcount*sizeof(datatype);
+
+#define SUZERAIN_PARCEL_ALIGNMENT(r,data,elem)                                     \
+    SUZERAIN_PARCEL_APPLY(SUZERAIN_PARCEL_ALIGNMENT_,elem)
+#define SUZERAIN_PARCEL_ALIGNMENT_(varname, datatype, varcount, varqual, minalign) \
+    const size_t BOOST_PP_CAT(_PARCEL_alignment_,varname)                          \
+        = __alignof__(datatype) > minalign ? __alignof__(datatype) : minalign;     \
+    assert(!(BOOST_PP_CAT(_PARCEL_alignment_,varname)                              \
+           & (BOOST_PP_CAT(_PARCEL_alignment_,varname) - 1)));
+
+#define SUZERAIN_PARCEL_PAD(r,data,elem)                                           \
+    SUZERAIN_PARCEL_APPLY(SUZERAIN_PARCEL_PAD_,elem)
+#define SUZERAIN_PARCEL_PAD_(varname, datatype, varcount, varqual, minalign)       \
+    const size_t BOOST_PP_CAT(_PARCEL_pad_,varname)                                \
+        = BOOST_PP_CAT(_PARCEL_alignment_,varname) - 1;
+
+#define SUZERAIN_PARCEL_MASK(r,data,elem)                                          \
+    SUZERAIN_PARCEL_APPLY(SUZERAIN_PARCEL_MASK_,elem)
+#define SUZERAIN_PARCEL_MASK_(varname, datatype, varcount, varqual, minalign)      \
+    const uintptr_t BOOST_PP_CAT(_PARCEL_mask_,varname)                            \
+        = ~(uintptr_t)(BOOST_PP_CAT(_PARCEL_alignment_,varname) - 1);
+
+#define SUZERAIN_PARCEL_SIZESUM(r,data,elem)                                       \
+    SUZERAIN_PARCEL_APPLY(SUZERAIN_PARCEL_SIZESUM_,elem)
+#define SUZERAIN_PARCEL_SIZESUM_(varname, datatype, varcount, varqual, minalign)   \
+    + (  BOOST_PP_CAT(_PARCEL_datasize_,varname)                                   \
+       + BOOST_PP_CAT(_PARCEL_pad_,varname))
+
+#define SUZERAIN_PARCEL_OP(s, state, elem) (                                       \
+        BOOST_PP_TUPLE_ELEM(5, 0, elem) BOOST_PP_COMMA()                           \
+        BOOST_PP_TUPLE_ELEM(5, 2, elem) BOOST_PP_COMMA()                           \
+        BOOST_PP_TUPLE_ELEM(3, 2, state) SUZERAIN_PARCEL_OP_(                      \
+            BOOST_PP_TUPLE_ELEM(5, 0, elem),                                       \
+            BOOST_PP_TUPLE_ELEM(5, 1, elem),                                       \
+            BOOST_PP_TUPLE_ELEM(5, 2, elem),                                       \
+            BOOST_PP_TUPLE_ELEM(5, 3, elem),                                       \
+            BOOST_PP_TUPLE_ELEM(3, 0, state),                                      \
+            BOOST_PP_TUPLE_ELEM(3, 1, state)                                       \
+        )                                                                          \
+    )
+#define SUZERAIN_PARCEL_OP_(varname, datatype, varcount, varqual, prev, prevcount) \
+    ; datatype * varqual varname = (void *)                                        \
+    ((((uintptr_t)(prev + prevcount)+BOOST_PP_CAT(_PARCEL_pad_,varname))           \
+      & BOOST_PP_CAT(_PARCEL_mask_,varname)))
+
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+#endif // __SUZERAIN_PARCEL_H__
---
+++ tests/test_parcel.c
@@ -0,0 +1,80 @@
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/parcel.h>
+#include <suzerain/common.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_test.h>
+
+// Random number generation for all methods managed in main()
+static gsl_rng * rng = NULL;
+
+static
+void
+test_one()
+{
+    for (int j = 0; j < 50; ++j) {
+
+        // Generate random size and alignment requirements
+        const size_t sc = 1 + gsl_rng_uniform_int(rng, 10U);
+        const size_t sd = 5 + gsl_rng_uniform_int(rng, 21U);
+        const size_t si = 2 + gsl_rng_uniform_int(rng, 10U);
+        const size_t sf = 7 + gsl_rng_uniform_int(rng, 31U);
+        const size_t ad = 1 << (3 + gsl_rng_uniform_int(rng,  2U));
+        const size_t af = 1 << (2 + gsl_rng_uniform_int(rng,  2U));
+
+        // Malloc and parcel out memory according to those needs
+        SUZERAIN_PARCEL(p, malloc, ((c, char,   sc, const,     0))
+                                   ((d, double, sd, restrict, ad))
+                                   ((i, int,    si,         ,  0))
+                                   ((f, float,  sd, restrict, af)));
+
+        // Ensure the alignments came back as requested
+        const unsigned long int ac = __alignof__(char);
+        const unsigned long int ai = __alignof__(int);
+        gsl_test(!p                          , "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu:        alloc p  OK", j, sc, sd, si, sf    );
+        gsl_test(p_parcel < sc + sd + si + sf, "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu:        p_parcel OK", j, sc, sd, si, sf    );
+        gsl_test(!((uintptr_t)c & ~(ac - 1)) , "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu,ac=%zu: align c  OK", j, sc, sd, si, sf, ac);
+        gsl_test(!((uintptr_t)d & ~(ad - 1)) , "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu,ad=%zu: align d  OK", j, sc, sd, si, sf, ad);
+        gsl_test(!((uintptr_t)i & ~(ai - 1)) , "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu,ai=%zu: align d  OK", j, sc, sd, si, sf, ai);
+        gsl_test(!((uintptr_t)f & ~(af - 1)) , "j=%d,sc=%zu,sd=%zu,si=%zu,sf=%zu,af=%zu: align f  OK", j, sc, sd, si, sf, af);
+
+        // Ensure the pointers came back with increasing addresses
+        gsl_test((uintptr_t)p > (uintptr_t)c, "j=%d,sc=%zu,sd=%zu,si=%zu:        p <= c   OK", j, sc, sd, si, ac);
+        gsl_test((uintptr_t)c > (uintptr_t)d, "j=%d,sc=%zu,sd=%zu,si=%zu:        c <= d   OK", j, sc, sd, si, ac);
+        gsl_test((uintptr_t)d > (uintptr_t)i, "j=%d,sc=%zu,sd=%zu,si=%zu:        d <= i   OK", j, sc, sd, si, ac);
+        gsl_test((uintptr_t)i > (uintptr_t)f, "j=%d,sc=%zu,sd=%zu,si=%zu:        i <= f   OK", j, sc, sd, si, ac);
+
+        // Write data through the pointers to check for segfaults
+        for (size_t k = 0; k < sc; ++k) c[k] = 'y';
+        for (size_t k = 0; k < sd; ++k) d[k] = 123.456;
+        for (size_t k = 0; k < si; ++k) i[k] = 7;
+        for (size_t k = 0; k < sd; ++k) f[k] = 789;
+
+        // Free all the memory in one swoop
+        free(p);
+
+        // Ensure a second usage in the same scope is without name conflicts
+        SUZERAIN_PARCEL(qp, malloc, ((qc, char,   (sc-1), const restrict, 0))
+                                    ((qd, double, (sd-2),               , 0))
+                                    ((qi, int,    (si-1),               , 0)));
+        (void) qi;
+        free(qp);
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    SUZERAIN_UNUSED(argc);
+    SUZERAIN_UNUSED(argv);
+
+    rng = gsl_rng_alloc(gsl_rng_taus);
+    gsl_rng_set(rng, 0);
+
+    test_one();
+
+    gsl_rng_free(rng);
+
+    exit(gsl_test_summary());
+}
