871197671d4b6ee811f626a6b49a6b8f91108a15
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-16 23:40:53 +0000

    suzerain: #2211 checkpoint minor progress
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27001 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/rholut_imexop.h
+++ suzerain/rholut_imexop.h
@@ -31,6 +31,8 @@
 #ifndef __SUZERAIN_RHOLUT_IMEXOP_H__
 #define __SUZERAIN_RHOLUT_IMEXOP_H__
 
+#include <suzerain/bsplineop.h>
+
 /** @file
  * Provides implicit operator apply and solve routines for a single wall-normal
  * pencil of state information.  Meant to be used in conjunction with compute
@@ -40,6 +42,54 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+void suzerain_rholut_imexop_apply(const double         km,
+                                  const double         kn,
+                                  const double         Re,
+                                  const double         Pr,
+                                  const double         Ma,
+                                  const double         alpha,
+                                  const double         gamma,
+                                  const double * const nu,
+                                  const int            inc_nu,
+                                  const double * const ux,
+                                  const int            inc_ux,
+                                  const double * const uy,
+                                  const int            inc_uy,
+                                  const double * const uz,
+                                  const int            inc_uz,
+                                  const double * const nuux,
+                                  const int            inc_nuux,
+                                  const double * const nuuy,
+                                  const int            inc_nuuy,
+                                  const double * const nuuz,
+                                  const int            inc_nuuz,
+                                  const double * const m_gradrho,
+                                  const int            inc_m_gradrho,
+                                  const double * const ex_gradrho,
+                                  const int            inc_ex_gradrho,
+                                  const double * const ey_gradrho,
+                                  const int            inc_ey_gradrho,
+                                  const double * const ez_gradrho,
+                                  const int            inc_ez_gradrho,
+                                  const double * const e_divm,
+                                  const int            inc_e_divm,
+                                  const double * const e_nablarho,
+                                  const int            inc_e_nablarho,
+                                  const double phi,
+                                  const double (*in_rho )[2],
+                                  const double (*in_rhou)[2],
+                                  const double (*in_rhov)[2],
+                                  const double (*in_rhow)[2],
+                                  const double (*in_rhoe)[2],
+                                  double (*out_rho )[2],
+                                  double (*out_rhou)[2],
+                                  double (*out_rhov)[2],
+                                  double (*out_rhow)[2],
+                                  double (*out_rhoe)[2],
+                                  const suzerain_bsplineop_workspace * w);
+
+void suzerain_rholut_imexop_build();
 
 
 
