c59336eaedbc777c9551386e2a24193af6dc31be
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-16 22:49:07 +0000

    suzerain: #2211 new rholut_imexop header
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@26997 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/rholut_imexop.h
@@ -0,0 +1,50 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2012 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of Suzerain.
+ *
+ * Suzerain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Suzerain is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * rholut_imexop.h: hybrid implicit/explicit operator apply and solve
+ *
+ * $Id$
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __SUZERAIN_RHOLUT_IMEXOP_H__
+#define __SUZERAIN_RHOLUT_IMEXOP_H__
+
+/** @file
+ * Provides implicit operator apply and solve routines for a single wall-normal
+ * pencil of state information.  Meant to be used in conjunction with compute
+ * kernels found in rholut.hpp.  Coded in C99 to facilitate profiling.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __SUZERAIN_RHOLUT_IMEXOP_H__ */
