b3cabb0a608eadaa98e6431565bf30a38870dfe0
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-24 22:35:04 +0000

    suzerain: bsmbsm dox cleanup
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27255 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/bsmbsm.h
+++ suzerain/bsmbsm.h
@@ -38,16 +38,17 @@
  *
  * Matrix \f$A\f$ is a BSMBSM when
  * \f[A = \begin{pmatrix}
- *     B_{0\,0}  & \cdots & B_{0\,s-1}   \\
+ *     B_{0\,0}  & \cdots & B_{0\,S-1}   \\
  *     \vdots    & \ddots & \vdots       \\
- *     B_{s-1,0} & \cdots & B_{s-1\,s-1}
+ *     B_{S-1,0} & \cdots & B_{S-1\,S-1}
  * \end{pmatrix}\f]
- * where every \f$B_{i\,j}\f$ is an \f$n\times{}\f$ banded submatrix containing
- * \c kl subdiagonals and \c ku superdiagonals.  The structure of a BSMBSM is
- * defined completely by the parameters <tt>s</tt>, <tt>n</tt>, <tt>kl</tt>,
- * <tt>ku</tt>.  The number of rows and columns is <tt>N = s*n</tt>.  We
- * set the convention that lowercase identifiers indicate submatrix details
- * while uppercase identifiers indicate global matrix details.
+ * where every \f$B_{i\,j}\f$ is an <tt>n</tt>x<tt>n</tt> banded submatrix
+ * containing \c kl subdiagonals and \c ku superdiagonals.  We set the
+ * convention that lowercase identifiers indicate submatrix details while
+ * uppercase identifiers indicate global matrix details.  The structure of a
+ * BSMBSM is defined completely by the parameters <tt>S</tt>, <tt>n</tt>,
+ * <tt>kl</tt>, and <tt>ku</tt>.  The number of rows and columns is <tt>N :=
+ * S*n</tt>.
  *
  * Building \f$A\f$ from individually contiguous, banded submatrices
  * \f$B_{i\,j}\f$ within \f$A\f$ is both convenient and efficient.  For
@@ -56,20 +57,20 @@
  * contiguous, banded submatrices is grossly inefficient for solving linear
  * equations.
  *
- * The zero-indexed permutation vector \f$q\!(i) =
- * n\times\left(i\bmod{}s\right) + \lfloor{}i/s\rfloor{}\f$ may always be used
- * to convert a BSMBSM into a globally banded <tt>N</tt>x<tt>N</tt> matrix with
+ * The zero-indexed permutation vector \f$q(i) \coloneqq
+ * \left(i\bmod{}S\right)n + \lfloor{}i/S\rfloor{}\f$ may always be used to
+ * convert a BSMBSM into a globally banded <tt>N</tt>x<tt>N</tt> matrix with
  * minimum bandwidth.  More concretely, the permutation matrix \f$P\f$ uniquely
- * defined by vector \f$q\f$ causes \f$P A P^{\mbox{T}}\f$ to have <tt>KL =
- * s*(kl+1)-1</tt> subdiagonals and <tt>KU = s*(ku+1)-1</tt> superdiagonals
- * summing to overall bandwidth <tt>KL + 1 + KU = s*(kl + ku + 2)-1</tt>.  Note
- * that the reverse permutation vector has a simple closed form \f$q^{-1}\!(i)
- * = s\times\left(i\bmod{}n\right) + \lfloor{}i/n\rfloor{}\f$.
+ * defined by vector \f$q\f$ causes \f$P A P^{\mbox{T}}\f$ to have <tt>KL :=
+ * S*(kl+1)-1</tt> subdiagonals and <tt>KU := S*(ku+1)-1</tt> superdiagonals
+ * summing to overall bandwidth <tt>KL + 1 + KU = S*(kl + ku + 2)-1</tt>.  Note
+ * that the reverse permutation vector has a simple closed form \f$q^{-1}(i)
+ * \coloneqq \left(i\bmod{}n\right)S + \lfloor{}i/n\rfloor{}\f$.
  *
  * With \f$A\f$ in hand, the banded renumbering \f$P A P^{\mbox{T}}\f$ may be
  * formed using \f$\left.P A P^{\mbox{T}}\right|_{i\,j} =
- * \left.A\right|_{q^{-1}\!(i)\,q^{-1}\!(j)}\f$.  This renumbering may be LU
- * factorized in order <tt>s*n*(s*(kl + ku + 2)-1)^2</tt> floating point
+ * \left.A\right|_{q^{-1}(i)\,q^{-1}(j)}\f$.  This renumbering may be LU
+ * factorized in order <tt>S*n*(S*(kl + ku + 2)-1)^2</tt> floating point
  * operations to find \f$LU = P A P^{\mbox{T}}\f$.  The linear equation
  * \f$AX=B\f$, which is equivalent to \f$LUPX=PB\f$, has the solution \f$X =
  * A^{-1}B = P^{\mbox{T}}\left(LU\right)^{-1}PB\f$.  Here inversion has been
@@ -81,8 +82,30 @@ extern "C" {
 #endif
 
 
-/** Encapsulates storage of a complex-valued BSMBSM matrix. */
+/**
+ * Encapsulates storage of a complex-valued BSMBSM matrix.
+ * @see bsmbsm.h for details on BSMBSMs.
+ */
 typedef struct suzerain_zbsmbsm {
+
+    int n;
+
+    int kl;
+
+    int ku;
+
+    int ld;
+
+    int S;
+
+    int N;
+
+    int KL;
+
+    int KU;
+
+    int LD;
+
 } suzerain_zbsmbsm;
 
 #ifdef __cplusplus
