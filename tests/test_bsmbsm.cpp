fad3882c49112866f8efb03dac8aba17a0abff12
Rhys Ulerich <rhys@ices.utexas.edu>
2012-01-25 19:02:21 +0000

    suzerain: #2211 scaffold test_bsmbsm
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27273 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ tests/test_bsmbsm.cpp
@@ -0,0 +1,21 @@
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/common.hpp>
+#pragma hdrstop
+#define BOOST_TEST_MODULE $Id$
+#include <suzerain/bsmbsm.h>
+#include <boost/test/included/unit_test.hpp>
+
+#include "test_tools.hpp"
+
+BOOST_GLOBAL_FIXTURE(BlasCleanupFixture);
+
+BOOST_AUTO_TEST_SUITE( NOP )
+
+BOOST_AUTO_TEST_CASE( q )
+{
+    // TODO
+}
+
+BOOST_AUTO_TEST_SUITE_END()
