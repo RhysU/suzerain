2747d6fdd186b7986291168ee28e2456b824ef66
Rhys Ulerich <rhys@ices.utexas.edu>
2012-02-01 23:33:34 +0000

    suzerain: #2211 checkpoint test progress
    Factorization now succeeding because it appears I'm
    no longer clobbering memory.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27536 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_rholut_imexop.cpp
+++ tests/test_rholut_imexop.cpp
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE( operator_consistency )
     // Initialize discrete B-spline operators
     using suzerain::bspline;
     using suzerain::bsplineop;
-    const real_t breakpts[] = { 0.0, 0.5, 1.5, 2.0, 2.5, 2.75, 3.0 };
+    const real_t breakpts[] = { 0.0, 0.5, 1.5, 2.25, 2.5, 3.0, 3.35 };
     suzerain::bspline b(8, suzerain::bspline::from_breakpoints(),
                         SUZERAIN_COUNTOF(breakpts), breakpts);
     suzerain::bsplineop op(b, 2, SUZERAIN_BSPLINEOP_COLLOCATION_GREVILLE);
@@ -76,21 +76,59 @@ BOOST_AUTO_TEST_CASE( operator_consistency )
         suzerain_bsmbsm_zaPxpby('N', S, n, 1.0, &B2[j*N], 1, 0.0, &B1[j*N], 1);
     }
 
-    // Form PAP^T
-    suzerain_bsmbsm A;
-    boost::scoped_array<complex_t> buf(
-        new complex_t[n*(op.max_kl() + 1 + op.max_ku())]);
-    boost::scoped_array<complex_t> papt(
-        new complex_t[n*(S*(op.max_kl() + op.max_ku() + 2) - 1)]);
+    // Form PAP^T (with size computations to check documentation is right)
+    const int bufsize  = n*(op.max_kl() + 1 + op.max_ku());
+    const int paptsize = (S*n)*(S*(op.max_kl() + op.max_ku() + 2) - 1);
+    const int lusize   = (S*n)*(S*(2*op.max_kl() + op.max_ku() + 3) - 2);
+    suzerain_bsmbsm A = suzerain_bsmbsm_construct(
+            S, n, op.max_kl(), op.max_ku());
+    BOOST_REQUIRE_EQUAL(bufsize,  A.ld * A.n);
+    BOOST_REQUIRE_EQUAL(paptsize, A.LD * A.N);
+    BOOST_REQUIRE_EQUAL(lusize,   (A.LD + A.KL) * A.N);
+    boost::scoped_array<complex_t> buf(new complex_t[bufsize]);
+    boost::scoped_array<complex_t> papt(new complex_t[paptsize]);
     suzerain_rholut_imexop_packc(phi, km, kn, &s, &r, &ld, op.get(),
                                  0, 1, 2, 3, 4, buf.get(), &A, papt.get());
 
-    // "Invert"
-    // TODO
+    // Factor LU = PAP^T and solve (LU)^{-1} B2 = B1
+    boost::scoped_array<complex_t> lu(new complex_t[(A.LD + A.KL)*A.N]);
+    boost::scoped_array<int>       ipiv(new int[A.N]);
+    char equed;
+    boost::scoped_array<real_t>    scale_r(new real_t[A.N]);
+    boost::scoped_array<real_t>    scale_c(new real_t[A.N]);
+    double rcond;
+    boost::scoped_array<real_t>    ferr(new real_t[N]);
+    boost::scoped_array<real_t>    berr(new real_t[N]);
+    boost::scoped_array<complex_t> work(new complex_t[2*A.N]);
+    boost::scoped_array<real_t>    rwork(new real_t[A.N]);
 
-    // Compute B2 = P^T*B1
+    BOOST_REQUIRE_EQUAL(0, suzerain_lapack_zgbsvx(/* fact  */ 'E',
+                                                  /* trans */ 'N',
+                                                  /* n     */ A.N,
+                                                  /* kl    */ A.KL,
+                                                  /* ku    */ A.KU,
+                                                  /* nrhs  */ N,
+                                                  /* ab    */ papt.get(),
+                                                  /* ldab  */ A.LD,
+                                                  /* afb   */ lu.get(),
+                                                  /* ldafb */ A.LD + A.KL,
+                                                  /* ipiv  */ ipiv.get(),
+                                                  /* equed */ &equed,
+                                                  /* r     */ scale_r.get(),
+                                                  /* c     */ scale_c.get(),
+                                                  /* b     */ B2.get(),
+                                                  /* ldb   */ N,
+                                                  /* x     */ B1.get(),
+                                                  /* ldx   */ N,
+                                                  /* rcond */ &rcond,
+                                                  /* ferr  */ ferr.get(),
+                                                  /* berr  */ berr.get(),
+                                                  /* work  */ work.get(),
+                                                  /* rwork */ rwork.get()));
+
+    // Compute B1 = P^T*B2
     for (int j = 0; j < N; ++j) {
-        suzerain_bsmbsm_zaPxpby('T', S, n, 1.0, &B1[j*N], 1, 0.0, &B2[j*N], 1);
+        suzerain_bsmbsm_zaPxpby('T', S, n, 1.0, &B2[j*N], 1, 0.0, &B1[j*N], 1);
     }
 
     // TODO Check result
