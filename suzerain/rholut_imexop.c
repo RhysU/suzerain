a3a8978e8faa694dfd9a64e2fca95d5a4a58b60b
Rhys Ulerich <rhys@ices.utexas.edu>
2012-02-06 18:36:10 +0000

    suzerain: #2212 (rhow,rhow) submatrix
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27709 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/rholut_imexop.c
+++ suzerain/rholut_imexop.c
@@ -193,6 +193,17 @@ suzerain_rholut_imexop_apply(
                 w->D[D2], w->ld, in_rho, inc, 1.0, out_rhow, inc);
         }
 
+        /* in_rhow */ {
+            suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
+                -gm1*ikn,              REF(uz),
+                -invRe*(km2+ap43*kn2), REF(nu),
+                w->D[M],  w->ld, in_rhow, inc, 1.0, out_rhow, inc);
+
+            suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
+                invRe,                 REF(nu),
+                w->D[D2], w->ld, in_rhow, inc, 1.0, out_rhow, inc);
+        }
+
         if (in_rhoe) suzerain_bsplineop_accumulate_complex(M, nrhs,
                 -gm1*invMa2*ikn, in_rhoe, inc, n, 1.0, out_rhow, inc, n, w);
 
