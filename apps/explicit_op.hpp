036af264f8d70b9beaf2d03d187900ac922ac5e1
Rhys Ulerich <rhys@ices.utexas.edu>
2012-02-07 19:49:09 +0000

    suzerain: #2214 refs in OperatorCommonBlock
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27756 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/explicit_op.hpp
+++ apps/explicit_op.hpp
@@ -18,6 +18,7 @@
 #include <suzerain/multi_array.hpp>
 #include <suzerain/operator_base.hpp>
 #include <suzerain/pencil_grid.hpp>
+#include <suzerain/rholut_imexop.h>
 #include <suzerain/scenario_definition.hpp>
 #include <suzerain/state_fwd.hpp>
 
@@ -34,6 +35,19 @@ namespace channel {
  * statistics sampling purposes.
  *
  * The quantities are as follows:
+ * \li \c ref_nu         Linearization reference \f$C^{\nu}               \f$
+ * \li \c ref_ux         Linearization reference \f$C^{u_x}               \f$
+ * \li \c ref_uy         Linearization reference \f$C^{u_y}               \f$
+ * \li \c ref_uz         Linearization reference \f$C^{u_z}               \f$
+ * \li \c ref_nuux       Linearization reference \f$C^{\nu u_x}           \f$
+ * \li \c ref_nuuy       Linearization reference \f$C^{\nu u_y}           \f$
+ * \li \c ref_nuuz       Linearization reference \f$C^{\nu u_z}           \f$
+ * \li \c ref_m_gradrho  Linearization reference \f$C^{m}_{\nabla\rho}    \f$
+ * \li \c ref_ex_gradrho Linearization reference \f$C^{e_x}_{\nabla\rho}  \f$
+ * \li \c ref_ey_gradrho Linearization reference \f$C^{e_y}_{\nabla\rho}  \f$
+ * \li \c ref_ez_gradrho Linearization reference \f$C^{e_z}_{\nabla\rho}  \f$
+ * \li \c ref_e_divm     Linearization reference \f$C^{e}_{\nabla\cdot{}m}\f$
+ * \li \c ref_e_deltarho Linearization reference \f$C^{e}_{\Delta\rho}    \f$
  * \li \c u  The nonlinear operator computes the instantaneous spatial (x, z)
  *     mean streamwise velocity profile as collocation point values.
  *     The linear operator then uses the information to compute
@@ -50,8 +64,16 @@ namespace channel {
  *
  * "Time-step-specific temporal means" are time averages taken across a single
  * time step of quantities which vary on each substep.  As the substeps are all
- * of equal length, a simple running mean reset on substep zero and then
+ * of equal length, a simple running mean is reset on substep zero and then
  * accumulated.
+ *
+ * Contiguous \c storage has been arranged so that an Allreduce operations
+ * should be performed on <tt>[storage.data(), u().data() + u().size())</tt> on
+ * substep zero (to include reference quantities) and on <tt>[u.data(),
+ * u().size())</tt> on later substeps.
+ *
+ * @see rholut_imexop.h for details on linearization and the associated
+ * reference quantities.
  */
 class OperatorCommonBlock
 {
@@ -64,7 +86,7 @@ public:
 private:
 
     /** Type of the contiguous storage used to house all scalars */
-    typedef Eigen::Array<real_t, Eigen::Dynamic, 4> storage_type;
+    typedef Eigen::Array<real_t, Eigen::Dynamic, 17> storage_type;
 
 public:
 
@@ -72,16 +94,61 @@ public:
     storage_type storage;
 
     // Declare a named, mutable "view" into \c storage for each quantity
-    storage_type::ColXpr u()       { return storage.col(0); }
-    storage_type::ColXpr f()       { return storage.col(1); }
-    storage_type::ColXpr f_dot_u() { return storage.col(2); }
-    storage_type::ColXpr qb()      { return storage.col(3); }
+    storage_type::ColXpr ref_nu()         { return storage.col( 0); }
+    storage_type::ColXpr ref_ux()         { return storage.col( 1); }
+    storage_type::ColXpr ref_uy()         { return storage.col( 2); }
+    storage_type::ColXpr ref_uz()         { return storage.col( 3); }
+    storage_type::ColXpr ref_nuux()       { return storage.col( 4); }
+    storage_type::ColXpr ref_nuuy()       { return storage.col( 5); }
+    storage_type::ColXpr ref_nuuz()       { return storage.col( 6); }
+    storage_type::ColXpr ref_m_gradrho()  { return storage.col( 7); }
+    storage_type::ColXpr ref_ex_gradrho() { return storage.col( 8); }
+    storage_type::ColXpr ref_ey_gradrho() { return storage.col( 9); }
+    storage_type::ColXpr ref_ez_gradrho() { return storage.col(10); }
+    storage_type::ColXpr ref_e_divm()     { return storage.col(11); }
+    storage_type::ColXpr ref_e_deltarho() { return storage.col(12); }
+    storage_type::ColXpr u()              { return storage.col(13); }
+    storage_type::ColXpr f()              { return storage.col(14); }
+    storage_type::ColXpr f_dot_u()        { return storage.col(15); }
+    storage_type::ColXpr qb()             { return storage.col(16); }
 
     // Declare a named, mutable "view" into \c storage for each quantity
-    storage_type::ConstColXpr u()       const { return storage.col(0); }
-    storage_type::ConstColXpr f()       const { return storage.col(1); }
-    storage_type::ConstColXpr f_dot_u() const { return storage.col(2); }
-    storage_type::ConstColXpr qb()      const { return storage.col(3); }
+    storage_type::ConstColXpr ref_nu()         const { return storage.col( 0); }
+    storage_type::ConstColXpr ref_ux()         const { return storage.col( 1); }
+    storage_type::ConstColXpr ref_uy()         const { return storage.col( 2); }
+    storage_type::ConstColXpr ref_uz()         const { return storage.col( 3); }
+    storage_type::ConstColXpr ref_nuux()       const { return storage.col( 4); }
+    storage_type::ConstColXpr ref_nuuy()       const { return storage.col( 5); }
+    storage_type::ConstColXpr ref_nuuz()       const { return storage.col( 6); }
+    storage_type::ConstColXpr ref_m_gradrho()  const { return storage.col( 7); }
+    storage_type::ConstColXpr ref_ex_gradrho() const { return storage.col( 8); }
+    storage_type::ConstColXpr ref_ey_gradrho() const { return storage.col( 9); }
+    storage_type::ConstColXpr ref_ez_gradrho() const { return storage.col(10); }
+    storage_type::ConstColXpr ref_e_divm()     const { return storage.col(11); }
+    storage_type::ConstColXpr ref_e_deltarho() const { return storage.col(12); }
+    storage_type::ConstColXpr u()              const { return storage.col(13); }
+    storage_type::ConstColXpr f()              const { return storage.col(14); }
+    storage_type::ConstColXpr f_dot_u()        const { return storage.col(15); }
+    storage_type::ConstColXpr qb()             const { return storage.col(16); }
+
+    /** Prepare data for use by implicit operator API in rholut_imexop.h. */
+    void imexop_ref(suzerain_rholut_imexop_ref   &ref,
+                    suzerain_rholut_imexop_refld &ld)
+    {
+        ld.nu         = 1; ref.nu         = ref_nu().data();
+        ld.ux         = 1; ref.ux         = ref_ux().data();
+        ld.uy         = 1; ref.uy         = ref_uy().data();
+        ld.uz         = 1; ref.uz         = ref_uz().data();
+        ld.nuux       = 1; ref.nuux       = ref_nuux().data();
+        ld.nuuy       = 1; ref.nuuy       = ref_nuuy().data();
+        ld.nuuz       = 1; ref.nuuz       = ref_nuuz().data();
+        ld.m_gradrho  = 1; ref.m_gradrho  = ref_m_gradrho().data();
+        ld.ex_gradrho = 1; ref.ex_gradrho = ref_ex_gradrho().data();
+        ld.ey_gradrho = 1; ref.ey_gradrho = ref_ey_gradrho().data();
+        ld.ez_gradrho = 1; ref.ez_gradrho = ref_ez_gradrho().data();
+        ld.e_divm     = 1; ref.e_divm     = ref_e_divm().data();
+        ld.e_deltarho = 1; ref.e_deltarho = ref_e_deltarho().data();
+    }
 
 private:
 
