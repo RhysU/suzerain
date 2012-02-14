57213b5662b76db8e30315cb17d122d09730d37e
Rhys Ulerich <rhys@ices.utexas.edu>
2012-02-14 02:43:06 +0000

    suzerain: #2212 avoid issues when phi == 0
    I had not algorithmically considered the case when phi == 0.
    Rework some of the scaling to better allow that situation.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@27941 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/rholut_imexop.c
+++ suzerain/rholut_imexop.c
@@ -69,266 +69,271 @@ suzerain_rholut_imexop_accumulate(
     const double invMa2      = 1 / Ma2;
     const double ginvRePr    = s->gamma / (s->Re * s->Pr);
 
-    // Accumulate the requested portions of the M + \varphi L operator.
-    // The zscal beta/phi and M gbmv phi coefficients scale output by beta.
-    // Notice that the phi in (M + phi*L) is achieved using final M gbmv.
+    // Accumulate the requested portions of the M + \varphi L operator.  Scale
+    // output by beta, accumulate non-mass contributions, and finally
+    // accumulate the mass contributions.  Mass contributions come last as they
+    // are expected to have magnitudes much larger than phi-- this helps to
+    // ensure better rounding of \varphi L contributions.
 
     // Shorthand for the common pattern of providing a r->foo, ld->foo pair.
 #   define REF(quantity) r->quantity, ld->quantity
 
     if (in_rho ) {  // Accumulate density terms into out_rho
-        suzerain_blas_zscal(n, beta/phi, out_rho, inc);
+        suzerain_blas_zscal(n, beta, out_rho, inc);
 
         if (in_rhou) suzerain_bsplineop_accumulate_complex(M, nrhs,
-                -ikm, in_rhou, inc, n, 1.0, out_rho, inc, n, w);
+                -phi*ikm, in_rhou, inc, n, 1.0, out_rho, inc, n, w);
 
         if (in_rhov) suzerain_bsplineop_accumulate_complex(D1,nrhs,
-                -1.0, in_rhov, inc, n, 1.0, out_rho, inc, n, w);
+                -phi*1.0, in_rhov, inc, n, 1.0, out_rho, inc, n, w);
 
         if (in_rhow) suzerain_bsplineop_accumulate_complex(M, nrhs,
-                -ikn, in_rhow, inc, n, 1.0, out_rho, inc, n, w);
+                -phi*ikn, in_rhow, inc, n, 1.0, out_rho, inc, n, w);
 
         if (in_rhoe) {/* NOP */};
 
         suzerain_bsplineop_accumulate_complex(
-                M, nrhs, 1.0, in_rho,  inc, n, phi, out_rho, inc, n, w);
+                M, nrhs, 1.0, in_rho,  inc, n, 1.0, out_rho, inc, n, w);
     }
 
     if (in_rhou) {  // Accumulate X momentum terms into out_rhou
-        suzerain_blas_zscal(n, beta/phi, out_rhou, inc);
+        suzerain_blas_zscal(n, beta, out_rhou, inc);
 
         if (in_rho) {
             suzerain_blasext_zgbdddmv_d('N', n, w->kl[M], w->ku[M],
-                -0.5*gm1*ikm,          REF(m_gradrho),
-                invRe*(ap43*km2+kn2),  REF(nuux),
-                -ap13*invRe*(ikm+ikn), REF(nuuz),
+                -phi*0.5*gm1*ikm,          REF(m_gradrho),
+                phi*invRe*(ap43*km2+kn2),  REF(nuux),
+                -phi*ap13*invRe*(ikm+ikn), REF(nuuz),
                 w->D[M],  w->ld, in_rho, inc, 1.0, out_rhou, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                -ap13*invRe*ikm,       REF(nuuy),
+                -phi*ap13*invRe*ikm,       REF(nuuy),
                 w->D[D1], w->ld, in_rho, inc, 1.0, out_rhou, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -invRe,                REF(nuux),
+                -phi*invRe,                REF(nuux),
                 w->D[D2], w->ld, in_rho, inc, 1.0, out_rhou, inc);
         }
 
         /* in_rhou */ {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikm,              REF(ux),
-                -invRe*(ap43*km2+kn2), REF(nu),
+                -phi*gm1*ikm,              REF(ux),
+                -phi*invRe*(ap43*km2+kn2), REF(nu),
                 w->D[M],  w->ld, in_rhou, inc, 1.0, out_rhou, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                invRe,                 REF(nu),
+                phi*invRe,                 REF(nu),
                 w->D[D2], w->ld, in_rhou, inc, 1.0, out_rhou, inc);
         }
 
         if (in_rhov) {
             suzerain_blasext_zgbdmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikm,       REF(uy),
+                -phi*gm1*ikm,       REF(uy),
                 w->D[M],  w->ld, in_rhov, inc, 1.0, out_rhou, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                ap13*invRe*ikm, REF(nu),
+                phi*ap13*invRe*ikm, REF(nu),
                 w->D[D1], w->ld, in_rhov, inc, 1.0, out_rhou, inc);
         }
 
         if (in_rhow) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikm,             REF(uz),
-                ap13*invRe*(ikm+ikn), REF(nu),
+                -phi*gm1*ikm,             REF(uz),
+                phi*ap13*invRe*(ikm+ikn), REF(nu),
                 w->D[M],  w->ld, in_rhow, inc, 1.0, out_rhou, inc);
         }
 
         if (in_rhoe) suzerain_bsplineop_accumulate_complex(M, nrhs,
-                -gm1*invMa2*ikm, in_rhoe, inc, n, 1.0, out_rhou, inc, n, w);
+                -phi*gm1*invMa2*ikm, in_rhoe, inc, n,
+                1.0, out_rhou, inc, n, w);
 
         suzerain_bsplineop_accumulate_complex(
-                M, nrhs, 1.0, in_rhou, inc, n, phi, out_rhou, inc, n, w);
+                M, nrhs, 1.0, in_rhou, inc, n, 1.0, out_rhou, inc, n, w);
     }
 
     if (in_rhov) {  // Accumulate Y momentum terms into out_rhov
-        suzerain_blas_zscal(n, beta/phi, out_rhov, inc);
+        suzerain_blas_zscal(n, beta, out_rhov, inc);
 
         if (in_rho) {
             suzerain_blasext_zgbdmv_d('N', n, w->kl[M], w->ku[M],
-                invRe*(km2+kn2), REF(nuuy),
+                phi*invRe*(km2+kn2), REF(nuuy),
                 w->D[M],  w->ld, in_rho, inc, 1.0, out_rhov, inc);
 
             suzerain_blasext_zgbdddmv_d('N', n, w->kl[D1], w->ku[D1],
-                -0.5*gm1,        REF(m_gradrho),
-                -ap13*invRe*ikm, REF(nuux),
-                -ap13*invRe*ikn, REF(nuuz),
+                -phi*0.5*gm1,        REF(m_gradrho),
+                -phi*ap13*invRe*ikm, REF(nuux),
+                -phi*ap13*invRe*ikn, REF(nuuz),
                 w->D[D1], w->ld, in_rho, inc, 1.0, out_rhov, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -ap43*invRe,     REF(nuuy),
+                -phi*ap43*invRe,     REF(nuuy),
                 w->D[D2], w->ld, in_rho, inc, 1.0, out_rhov, inc);
         }
 
         if (in_rhou) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[D1], w->ku[D1],
-                gm1,            REF(ux),
-                ap13*invRe*ikm, REF(nu),
+                phi*gm1,            REF(ux),
+                phi*ap13*invRe*ikm, REF(nu),
                 w->D[D1],  w->ld, in_rhou, inc, 1.0, out_rhov, inc);
         }
 
         /* in_rhov */ {
             suzerain_blasext_zgbdmv_d('N', n, w->kl[M], w->ku[M],
-                -invRe*(km2+kn2), REF(nu),
+                -phi*invRe*(km2+kn2), REF(nu),
                 w->D[M],  w->ld, in_rhov, inc, 1.0, out_rhov, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                gm1,             REF(uy),
+                phi*gm1,             REF(uy),
                 w->D[D1], w->ld, in_rhov, inc, 1.0, out_rhov, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                ap43*invRe,      REF(nu),
+                phi*ap43*invRe,      REF(nu),
                 w->D[D2], w->ld, in_rhov, inc, 1.0, out_rhov, inc);
         }
 
         if (in_rhow) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[D1], w->ku[D1],
-                gm1,            REF(uz),
-                ap13*invRe*ikn, REF(nu),
+                phi*gm1,            REF(uz),
+                phi*ap13*invRe*ikn, REF(nu),
                 w->D[D1],  w->ld, in_rhow, inc, 1.0, out_rhov, inc);
         }
 
         if (in_rhoe) suzerain_bsplineop_accumulate_complex(D1, nrhs,
-                -gm1*invMa2, in_rhoe, inc, n, 1.0, out_rhov, inc, n, w);
+                -phi*gm1*invMa2, in_rhoe, inc, n,
+                1.0, out_rhov, inc, n, w);
 
         suzerain_bsplineop_accumulate_complex(
-                M, nrhs, 1.0, in_rhov, inc, n, phi, out_rhov, inc, n, w);
+                M, nrhs, 1.0, in_rhov, inc, n, 1.0, out_rhov, inc, n, w);
     }
 
     if (in_rhow) {  // Accumulate Z momentum terms into out_rhow
-        suzerain_blas_zscal(n, beta/phi, out_rhow, inc);
+        suzerain_blas_zscal(n, beta, out_rhow, inc);
 
         if (in_rho) {
             suzerain_blasext_zgbdddmv_d('N', n, w->kl[M], w->ku[M],
-                -0.5*gm1*ikn,          REF(m_gradrho),
-                invRe*(km2+ap43*kn2),  REF(nuuz),
-                -ap13*invRe*(ikm+ikn), REF(nuux),
+                -phi*0.5*gm1*ikn,          REF(m_gradrho),
+                phi*invRe*(km2+ap43*kn2),  REF(nuuz),
+                -phi*ap13*invRe*(ikm+ikn), REF(nuux),
                 w->D[M],  w->ld, in_rho, inc, 1.0, out_rhow, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                -ap13*invRe*ikn,       REF(nuuy),
+                -phi*ap13*invRe*ikn,       REF(nuuy),
                 w->D[D1], w->ld, in_rho, inc, 1.0, out_rhow, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -invRe,                REF(nuuz),
+                -phi*invRe,                REF(nuuz),
                 w->D[D2], w->ld, in_rho, inc, 1.0, out_rhow, inc);
         }
 
         if (in_rhou) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikn,             REF(ux),
-                ap13*invRe*(ikm+ikn), REF(nu),
+                -phi*gm1*ikn,             REF(ux),
+                phi*ap13*invRe*(ikm+ikn), REF(nu),
                 w->D[M],  w->ld, in_rhou, inc, 1.0, out_rhow, inc);
         }
 
         if (in_rhov) {
             suzerain_blasext_zgbdmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikn,       REF(uy),
+                -phi*gm1*ikn,       REF(uy),
                 w->D[M],  w->ld, in_rhov, inc, 1.0, out_rhow, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                ap13*invRe*ikn, REF(nu),
+                phi*ap13*invRe*ikn, REF(nu),
                 w->D[D1], w->ld, in_rhov, inc, 1.0, out_rhow, inc);
         }
 
         /* in_rhow */ {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -gm1*ikn,              REF(uz),
-                -invRe*(km2+ap43*kn2), REF(nu),
+                -phi*gm1*ikn,              REF(uz),
+                -phi*invRe*(km2+ap43*kn2), REF(nu),
                 w->D[M],  w->ld, in_rhow, inc, 1.0, out_rhow, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                invRe,                 REF(nu),
+                phi*invRe,                 REF(nu),
                 w->D[D2], w->ld, in_rhow, inc, 1.0, out_rhow, inc);
         }
 
         if (in_rhoe) suzerain_bsplineop_accumulate_complex(M, nrhs,
-                -gm1*invMa2*ikn, in_rhoe, inc, n, 1.0, out_rhow, inc, n, w);
+                -phi*gm1*invMa2*ikn, in_rhoe, inc, n,
+                1.0, out_rhow, inc, n, w);
 
         suzerain_bsplineop_accumulate_complex(
-                M, nrhs, 1.0, in_rhow, inc, n, phi, out_rhow, inc, n, w);
+                M, nrhs, 1.0, in_rhow, inc, n, 1.0, out_rhow, inc, n, w);
     }
 
     if (in_rhoe) {  // Accumulate total energy terms into out_rhoe
-        suzerain_blas_zscal(n, beta/phi, out_rhoe, inc);
+        suzerain_blas_zscal(n, beta, out_rhoe, inc);
 
         if (in_rho) {
             suzerain_blasext_zgbdddmv_d('N', n, w->kl[M], w->ku[M],
-                -ikm,                    REF(ex_gradrho),
-                -ikn,                    REF(ez_gradrho),
-                -ginvRePr/gm1*(km2+kn2), REF(e_deltarho),
+                -phi*ikm,                    REF(ex_gradrho),
+                -phi*ikn,                    REF(ez_gradrho),
+                -phi*ginvRePr/gm1*(km2+kn2), REF(e_deltarho),
                 w->D[M],  w->ld, in_rho, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                -1.0,                    REF(ey_gradrho),
+                -phi,                        REF(ey_gradrho),
                 w->D[D1], w->ld, in_rho, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                ginvRePr/gm1,            REF(e_deltarho),
+                phi*ginvRePr/gm1,            REF(e_deltarho),
                 w->D[D2], w->ld, in_rho, inc, 1.0, out_rhoe, inc);
         }
 
         if (in_rhou) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -ikm,                    REF(e_divm),
-                -ginvRePr*Ma2*(km2+kn2), REF(nuux),
+                -phi*ikm,                    REF(e_divm),
+                -phi*ginvRePr*Ma2*(km2+kn2), REF(nuux),
                 w->D[M],  w->ld, in_rhou, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -ginvRePr*Ma2,            REF(nuux),
+                -phi*ginvRePr*Ma2,            REF(nuux),
                 w->D[D2], w->ld, in_rhou, inc, 1.0, out_rhoe, inc);
         }
 
         if (in_rhov) {
             suzerain_blasext_zgbdmv_d('N', n, w->kl[M], w->ku[M],
-                ginvRePr*Ma2*(km2+kn2), REF(nuuy),
+                phi*ginvRePr*Ma2*(km2+kn2), REF(nuuy),
                 w->D[M],  w->ld, in_rhov, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                -1.0,                   REF(e_divm),
+                -phi,                       REF(e_divm),
                 w->D[D1], w->ld, in_rhov, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -ginvRePr*Ma2,          REF(nuuy),
+                -phi*ginvRePr*Ma2,          REF(nuuy),
                 w->D[D2], w->ld, in_rhov, inc, 1.0, out_rhoe, inc);
         }
 
         if (in_rhow) {
             suzerain_blasext_zgbddmv_d('N', n, w->kl[M], w->ku[M],
-                -ikn,                    REF(e_divm),
-                -ginvRePr*Ma2*(km2+kn2), REF(nuuz),
+                -phi*ikn,                    REF(e_divm),
+                -phi*ginvRePr*Ma2*(km2+kn2), REF(nuuz),
                 w->D[M],  w->ld, in_rhow, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                -ginvRePr*Ma2,            REF(nuuz),
+                -phi*ginvRePr*Ma2,            REF(nuuz),
                 w->D[D2], w->ld, in_rhow, inc, 1.0, out_rhoe, inc);
         }
 
         /* in_rhoe */ {
             suzerain_blasext_zgbdddmv_d('N', n, w->kl[M], w->ku[M],
-                -s->gamma*ikm,       REF(ux),
-                -s->gamma*ikn,       REF(uz),
-                -ginvRePr*(km2+kn2), REF(nu),
+                -phi*s->gamma*ikm,       REF(ux),
+                -phi*s->gamma*ikn,       REF(uz),
+                -phi*ginvRePr*(km2+kn2), REF(nu),
                 w->D[M],  w->ld, in_rhoe, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D1], w->ku[D1],
-                -s->gamma,           REF(uy),
+                -phi*s->gamma,           REF(uy),
                 w->D[D1], w->ld, in_rhoe, inc, 1.0, out_rhoe, inc);
 
             suzerain_blasext_zgbdmv_d('N', n, w->kl[D2], w->ku[D2],
-                ginvRePr,            REF(nu),
+                phi*ginvRePr,            REF(nu),
                 w->D[D2], w->ld, in_rhoe, inc, 1.0, out_rhoe, inc);
         }
 
         suzerain_bsplineop_accumulate_complex(
-                M, nrhs, 1.0, in_rhoe, inc, n, phi, out_rhoe, inc, n, w);
+                M, nrhs, 1.0, in_rhoe, inc, n, 1.0, out_rhoe, inc, n, w);
     }
 
 #   undef REF
