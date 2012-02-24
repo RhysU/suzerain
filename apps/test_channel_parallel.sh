00b5c24ff8b0ef80b8469c167dcf1e478ee29c8b
Rhys Ulerich <rhys@ices.utexas.edu>
2012-02-24 22:21:13 +0000

    suzerain: #2515 automake --implicit tests
    Adds the hybrid operators into the 'make check' suite.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@28317 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/test_channel_parallel.sh
+++ apps/test_channel_parallel.sh
@@ -6,10 +6,13 @@ source "`dirname $0`/test_channel_setup.sh"
 
 : ${ADVANCE:=--advance_nt=10 --status_nt=5 --fluct_percent=10 --fluct_seed=45678}
 
-banner "Generating serial result for comparison purposes"
+# Test both purely explicit and hybrid implicit/explicit operators
+for OP in --explicit --implicit; do
+
+banner "Generating serial result for comparison purposes ($OP)"
 (
     cd $testdir
-    run ../channel mms0.h5 --restart_destination "serial#.h5" $ADVANCE
+    run ../channel $OP mms0.h5 --restart_destination "serial#.h5" $ADVANCE
 )
 
 # Run each test case in this file under the following circumstances
@@ -19,15 +22,17 @@ NP=
 P=
 eval "$METACASE"
 
-banner "Equivalence of serial and parallel execution"
+banner "Equivalence of serial and parallel execution ($OP)"
 (
     cd $testdir
-    prun ../channel mms0.h5 --restart_destination "a#.h5" $ADVANCE $P
+    prun ../channel $OP mms0.h5 --restart_destination "a#.h5" $ADVANCE $P
     # Stricter tolerance performed first for non-/bar_foo quantities
     differ_exclude $exclude_datasets_bar --delta=2e-14 --nan serial0.h5 a0.h5
     for dset in $datasets_bar; do
         differ --delta=5e-12 --relative=5e-12 serial0.h5 a0.h5 $dset
     done
 )
+
+done
 
 done
