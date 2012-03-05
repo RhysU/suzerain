95923c45c73b3cf9b203b1df8fc73a8637c53f4b
Rhys Ulerich <rhys@ices.utexas.edu>
2012-03-05 22:12:46 +0000

    suzerain: #2298 correct headers
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@28470 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/gbddddmv.h
+++ suzerain/gbddddmv.h
@@ -8,7 +8,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  *--------------------------------------------------------------------------
- * gbdddddmv.h: provides BLAS-like general band matrix-vector operations
+ * gbddddmv.h: provides BLAS-like general band matrix-vector operations
  * $Id$
  */
 
