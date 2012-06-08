30071e6deb3b16e4f2f01e31a49495d5658751a6
Rhys Ulerich <rhys@ices.utexas.edu>
2012-06-08 00:11:35 +0000

    suzerain: fix MPI check macros
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@30734 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/error.h
+++ suzerain/error.h
@@ -270,8 +270,8 @@ FILE * suzerain_set_stream(FILE * new_stream);
     do { \
         const int _chk_stat = (stmt); \
         if (_chk_stat != MPI_SUCCESS) { \
-            char _chk_reason[255]; \
-            char *_chk_mpistring = NULL; \
+            char _chk_reason[384]; \
+            char *_chk_mpistring[MPI_MAX_ERROR_STRING]; \
             int _chk_len; \
             const int _chk_string_stat \
                 = MPI_Error_string(_chk_stat,_chk_mpistring,&_chk_len); \
