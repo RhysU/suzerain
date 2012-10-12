6b0a1b389f9d5c5171aea20034df29dfdb86f00a
Rhys Ulerich <rhys@ices.utexas.edu>
2012-10-12 21:48:33 +0000

    suzerain: hygiene FIXME added
    No idea how this is working as it stands...
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@33809 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_bsplineop.cpp
+++ tests/test_bsplineop.cpp
@@ -22,6 +22,8 @@
  *--------------------------------------------------------------------------
  */
 
+// FIXME Missing BOOST_AUTO_TEST_SUITE( test_suite_name ) invocations ?
+
 #ifdef HAVE_CONFIG_H
 #include <suzerain/config.h>
 #endif
