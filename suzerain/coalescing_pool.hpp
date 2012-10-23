66dc701d29b6f1b72362f2355c08e173e2638747
Rhys Ulerich <rhys@ices.utexas.edu>
2012-10-23 05:42:10 +0000

    suzerain: #2527 upstream coalescing_pool changes
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@34099 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/coalescing_pool.hpp
+++ suzerain/coalescing_pool.hpp
@@ -21,6 +21,7 @@
 #include <stdexcept>
 #include <string>
 #include <utility>
+#include <boost/intrusive/link_mode.hpp>
 #include <boost/intrusive/set_hook.hpp>
 #include <boost/intrusive/set.hpp>
 #include <boost/next_prior.hpp>
@@ -39,7 +40,17 @@ template <typename T>
 struct coalescing_pool_policy
 {
     /** Should all newly allocated arenas be zeroed? */
-    static const bool zero_new_arenas = true;
+    static const bool zero_new_arenas
+            = true;
+
+    /**
+     * What Boost Intrusive linking mode should be used for free list entries?
+     * Valid options are \c normal_link, \c safe_link, or \c auto_unlink.
+     *
+     * @see Boost Intrusive for more details.
+     */
+    static const ::boost::intrusive::link_mode_type link_mode_type
+            = boost::intrusive::safe_link;
 };
 
 /**
@@ -466,7 +477,7 @@ private:
     class fl_entry
         : public blocks,
           public ::boost::intrusive::set_base_hook<
-                ::boost::intrusive::link_mode< ::boost::intrusive::normal_link>
+                ::boost::intrusive::link_mode<Policy::link_mode_type>
           >
     {
     public:
@@ -685,7 +696,7 @@ void coalescing_pool<T,ArenaAllocator,Policy>::release(
 
     const typename arenas_type::iterator end = arenas.end();
     typename arenas_type::iterator i = ::std::lower_bound(
-            arenas.begin(), end, ::std::distance(b.b_, b.e_), compare_extents());
+            arenas.begin(), end, b.nelems(), compare_extents());
     for (; i != end; ++i) {
         if (i->contains(b)) {
 
