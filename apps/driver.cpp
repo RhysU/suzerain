0306c0627301906bc20776ee2799d07fd2874df3
Rhys Ulerich <rhys@ices.utexas.edu>
2012-10-31 23:28:00 +0000

    suzerain: #2480 checkpointing progress
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@34278 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/driver.cpp
+++ apps/driver.cpp
@@ -29,9 +29,73 @@
 
 #include "driver.hpp"
 
+#ifdef HAVE_UNDERLING
+#include <fftw3.h>
+#include <fftw3-mpi.h>
+#include <underling/underling.h>
+#include <underling/error.h>
+#endif
+
+#include <suzerain/mpi_datatype.hpp>
+#include <suzerain/mpi.hpp>
+
+#include "logging.hpp"
+#include "support.hpp"
+
 namespace suzerain {
 
 namespace support {
+
+Driver::Driver()
+    : grid(),
+      fftwdef( /* rigor_fft   */ fftw::measure,
+               /* rigor_mpi   */ fftw::estimate),
+      restart( /* metadata    */ "metadata.h5.XXXXXX",
+               /* uncommitted */ "uncommitted.h5.XXXXXX",
+               /* destination */ "restart#.h5",
+               /* retain      */ 1,
+               /* dt          */ 0,
+               /* nt          */ 0),
+      statsdef(/* destination */ "sample#.h5"),
+      timedef( /* advance_dt  */ 0,
+               /* advance_nt  */ 0,
+               /* advance_wt  */ 0,
+               /* status_dt   */ 0,
+               /* status_nt   */ 0,
+               /* min_dt      */ 1e-8,
+               /* max_dt      */ 1),
+      sigdef(),
+      b(),
+      bop(),
+      gop(),
+      dgrid(),
+      state_linear(),
+      state_nonlinear(),
+      esioh(NULL)
+{
+}
+
+Driver::~Driver()
+{
+
+    // Remove the metadata file.
+    // Preserve restart.uncommitted as it may help post mortem debugging.
+    if (suzerain::mpi::comm_rank(MPI_COMM_WORLD) == 0) {
+        if (0 == unlink(restart.metadata.c_str())) {
+            DEBUG("Cleaned up temporary file " << restart.metadata);
+        } else {
+            WARN("Error cleaning up temporary file " << restart.metadata);
+        }
+    }
+
+    // Finalize any ESIO handle in use
+    if (esioh)
+        esio_handle_finalize(esioh);
+
+#ifdef HAVE_UNDERLING
+    underling_cleanup();
+#endif
+}
 
 } // end namespace support
 
