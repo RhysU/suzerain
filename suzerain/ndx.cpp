4fbf68fcbd17393c7a7ed9ce92f584b53a2f8b08
Rhys Ulerich <rhys@ices.utexas.edu>
2012-11-01 20:24:00 +0000

    suzerain: #2480 prep ndx.{hpp,cpp} for refactor
    Need a common place to house 3D Navier--Stokes storage conventions.
    This will be the place.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@34286 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/ndx.cpp
@@ -0,0 +1,26 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2012 Rhys Ulerich
+// Copyright (C) 2012 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+// ndx.cpp: Symbolic indices, names, and descriptions for Navier--Stokes
+// $Id$
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+#include <suzerain/ndx.hpp>
+
+namespace suzerain {
+
+namespace ndx {
+
+} // namespace ndx
+
+} // namespace suzerain
---
+++ suzerain/ndx.hpp
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2012 Rhys Ulerich
+// Copyright (C) 2012 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+// ndx.hpp: Symbolic indices, names, and descriptions for Navier--Stokes
+// $Id$
+
+#ifndef SUZERAIN_NDX_HPP
+#define SUZERAIN_NDX_HPP
+
+#include <suzerain/common.hpp>
+
+namespace suzerain {
+
+namespace ndx {
+
+} // namespace ndx
+
+} // namespace suzerain
+
+#endif // SUZERAIN_NDX_HPP
