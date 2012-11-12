ceac18fa7a9948f096355a626f12b6b42c1943d4
Rhys Ulerich <rhys@ices.utexas.edu>
2012-11-12 14:57:23 +0000

    suzerain: copy rename_hdf5.py utility
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@34522 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ postproc/remove_hdf5.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""Usage: rename_hdf5.py [-n] PATTERN REPL HDF5FILE...
+Rename datasets matching using Python's re.sub(PATTERN, REPL) within HDF5FILE.
+
+Options:
+    -n Don't actually change anything, just print what actions would be taken.
+"""
+import sys
+import getopt
+import h5py
+import re
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def rename_hdf5(pattern, repl, hdf5file, dryrun=False):
+    print "Processing", hdf5file
+
+    if dryrun:
+        f = h5py.File(hdf5file, 'r')
+    else:
+        f = h5py.File(hdf5file, 'r+')
+
+    for oldname in f.iterkeys():
+        newname = re.sub(pattern, repl, oldname)
+        if newname == oldname:
+            continue
+
+        print "Renaming", oldname, "to", newname
+        if not dryrun:
+            f[newname] = f[oldname]
+            del f[oldname]
+
+    f.close()
+
+def main(argv=None):
+
+    # Permit interactive use
+    if argv is None:
+        argv = sys.argv
+
+    # Parse and check incoming command line arguments
+    dryrun = False
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hn", ["help"])
+        except getopt.error, msg:
+             raise Usage(msg)
+        for o, a in opts:
+            if o == "-n":
+                dryrun = True
+            elif o in ("-h", "--help"):
+                print __doc__
+                return 0
+        if len(args) < 3:
+            print >>sys.stderr, "Too few arguments.  See --help."
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        return 2
+
+    # Process each file in turn
+    (pattern, repl), hdf5files = args[:2], args[2:]
+    for hdf5file in hdf5files:
+        rename_hdf5(pattern, repl, hdf5file, dryrun)
+
+if __name__ == "__main__":
+    sys.exit(main())
