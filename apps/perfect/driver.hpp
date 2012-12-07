a3984adce68c561c8dc31f0825cdebc5c84d9ec6
Rhys Ulerich <rhys@ices.utexas.edu>
2012-12-07 21:45:10 +0000

    suzerain: #2480 draft perfect::driver API
    This should be a roadmap for what needs implementing towards getting
    a driver class for the perfect gas application.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@35140 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/perfect/driver.hpp
@@ -0,0 +1,134 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2008, 2009, 2010, 2011, 2012 Rhys Ulerich
+// Copyright (C) 2012 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+// driver.hpp: A driver for nondimensional perfect gas simulations
+// $Id$
+
+#ifndef SUZERAIN_PERFECT_DRIVER_HPP
+#define SUZERAIN_PERFECT_DRIVER_HPP
+
+#include <suzerain/common.hpp>
+#include <suzerain/support/driver_base.hpp>
+
+#include "scenario_definition.hpp"
+
+namespace suzerain {
+
+namespace perfect {
+
+// Forward
+class mean_quantities;
+
+/**
+ * An driver class for managing a nondimensional perfect gas application using
+ * details from \ref suzerain::rholut.  Instantiate from within \c main().
+ */
+class driver : public support::driver_base
+{
+    /** Provides simple access to the superclass type */
+    typedef support::driver_base super;
+
+public:
+
+    /** @copydoc driver_base::driver_base */
+    driver(const std::string &application_synopsis,
+           const std::string &description = "",
+           const std::string &revstr = "");
+
+    /** @copydoc driver_base::initialize */
+    virtual std::vector<std::string> initialize(int argc, char **argv);
+
+    /** Nondimensional scenario parameters used by physics routines. */
+    shared_ptr<scenario_definition> scenario;
+
+    /**
+     * Maintains instantaneously sampled wall-normal mean quantities.  Member
+     * \c mean.t is used to track the last time statistics were computed and is
+     * used as a flag to avoid expensive recomputation.
+     */
+    mean_quantities mean;
+
+    /**
+     * Beyond the inherited behavior, this method initializes #mean.
+     * @copydetails application_base::load_grid_and_operators
+     */
+    virtual void load_grid_and_operators(
+            const esio_handle esioh);
+
+    /**
+     * Collectively compute statistics from #state_linear saving them into
+     * #mean and destroying #state_nonlinear in the process.  Any currently
+     * cached results in #mean will be reused <em>without recomputation</em>
+     * whenever <tt>this->controller && means.t = controller->current_t()</tt>.
+     */
+    virtual void compute_statistics();
+
+protected:
+
+    /**
+     * Beyond the inherited behavior, this method saves #scenario.
+     * @copydetails driver_base::save_metadata_hook
+     */
+    virtual void save_metadata_hook(
+            const esio_handle esioh);
+
+    /**
+     * Beyond the inherited behavior, this method invokes \ref
+     * compute_statistics and saves #mean.
+     * @copydetails driver_base::save_statistics_hook
+     */
+    virtual bool save_statistics_hook(
+            const esio_handle esioh);
+
+    /**
+     * Beyond the inherited behavior, this method loads #scenario.
+     * @copydetails driver_base::save_metadata_hook
+     */
+    virtual void load_metadata_hook(
+            const esio_handle esioh);
+
+    /**
+     * Beyond the inherited behavior, this method loads #mean.
+     * @copydetails application_base::load_restart
+     */
+    virtual void load_statistics_hook(
+            const esio_handle esioh);
+
+    /**
+     * The default interval is a fractional flow through time per #grid
+     * and #bulk_rho_u.
+     */
+    virtual void default_status_interval(time_type& t, step_type&);
+
+    /** @copydoc default_status_interval */
+    virtual void default_restart_interval(time_type& t, step_type&);
+
+    /** @copydoc default_status_interval */
+    virtual void default_statistics_interval(time_type& t, step_type&);
+
+};
+
+} // end namespace perfect
+
+} // end namespace suzerain
+
+#endif // SUZERAIN_PERFECT_DRIVER_HPP
