62de62c8cc6af448b983f5c4d5386acc0bc7a2b0
Rhys Ulerich <rhys@ices.utexas.edu>
2013-01-15 15:14:40 +0000

    suzerain: #2480 trim down main_advance
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@36188 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/main_advance.cpp
+++ apps/perfect/main_advance.cpp
@@ -22,21 +22,19 @@
 //--------------------------------------------------------------------------
 
 /** @file
- * Application executing \ref suzerain::perfect::driver_init::run.
+ * Application executing \ref suzerain::perfect::driver_advance::run.
  */
 
 #ifdef HAVE_CONFIG_H
 #include <suzerain/config.h>
 #endif
 
-#include <boost/math/special_functions/gamma.hpp>
 #include <esio/esio.h>
 
 #include <suzerain/common.hpp>
-#include <suzerain/ndx.hpp>
-#include <suzerain/physical_view.hpp>
 #include <suzerain/support/logging.hpp>
-#include <suzerain/validation.hpp>
+#include <suzerain/support/noise_definition.hpp>
+#include <suzerain/zgbsv_specification.hpp>
 
 #include "driver.hpp"
 
@@ -45,11 +43,11 @@
 namespace suzerain { namespace perfect {
 
 /** Application for initializing new restart files. */
-struct driver_init : public driver
+struct driver_advance : public driver
 {
-    driver_init(const std::string& revstr)
-        : driver("Perfect gas compressible channel initialization",
-                 "[RESTART-FILE]",
+    driver_advance(const std::string& revstr)
+        : driver("Compressible, perfect gas simulation time advancement",
+                 "RESTART-FILE",
                  "",
                  revstr)
     {}
@@ -60,186 +58,75 @@ struct driver_init : public driver
 
 } /* namespace perfect */ } /* namespace suzerain */
 
-// Provided by main_init_svnrev.{c,h} so revstr updates are merely relinking
+// Provided by main_advance_svnrev.{c,h} so revstr updates are merely relinking
 extern "C" const char revstr[];
 
 /** Instantiate and invoke the application */
 int main(int argc, char **argv)
 {
-    suzerain::perfect::driver_init app(revstr);
+    suzerain::perfect::driver_advance app(revstr);
     return app.run(argc, argv);
 }
 
 int
-suzerain::perfect::driver_init::run(int argc, char **argv)
+suzerain::perfect::driver_advance::run(int argc, char **argv)
 {
-    using boost::math::constants::pi;
-    using boost::math::tgamma;
-    using std::pow;
-    using std::sin;
+    using std::string;
+    using std::vector;
 
-    // Establish default grid and domain extents
-    grid->L.x()   = 4 * pi<real_t>();
-    grid->L.y()   = 2;
-    grid->L.z()   = 4 * pi<real_t>() / 3;
-    grid->Nx(1);
-    grid->DAFx(1.5);
-    grid->Ny(32);
-    grid->k       = 8;
-    grid->htdelta = 3;
-    grid->Nz(1);
-    grid->DAFz(1.5);
+    // Storage for binary-specific options
+    const support::noise_definition noisedef;
+    bool use_explicit  = false;
+    bool use_implicit  = false;
+    bool use_yang11    = false;
+    bool use_smr91     = false;
+    string solver_spec(static_cast<string>(suzerain::zgbsv_specification()));
 
-    // Establish default scenario parameters
-    scenario->Re         = 100;
-    scenario->Ma         = 1.5;
-    scenario->Pr         = 0.7;
-    scenario->bulk_rho   = 1;
-    scenario->bulk_rho_u = 1;
-    scenario->alpha      = 0;
-    scenario->beta       = real_t(2) / 3;
-    scenario->gamma      = 1.4;
-
-    // Establish default time step aggressiveness
-    timedef = make_shared<support::time_definition>(/* per Venugopal */ 0.72);
-
-    // Establish default MMS parameters and plug into program options
-    msoln = make_shared<manufactured_solution>(
-            manufactured_solution::default_caption
-            + " (active only when --mms supplied)");
-    options.add_definition(msoln->isothermal_channel());
-
-    // Establish binary-specific options
-    std::pointer_to_binary_function<real_t,const char*,void>
-        ensure_real_tnonnegative(validation::ensure_nonnegative<real_t>);
-    real_t mms    = -1;
-    real_t npower =  1;
+    // Register binary-specific options
+    options.add_definition(const_cast<support::noise_definition&>(noisedef));
     options.add_options()
-        ("clobber", "Overwrite an existing restart file?")
-        ("npower",
-            boost::program_options::value(&npower)->default_value(npower),
-            "Power n in (0, 1] used to control the flatness of the"
-            " \"parabolic\" streamwise velocity profile (y*(L-y))^n.")
-        ("mms",
-            boost::program_options::value(&mms)
-            ->notifier(std::bind2nd(ensure_real_tnonnegative, "mms")),
-            "If given, prepare a manufactured solution at the specified time.")
+        ("explicit", "Use purely explicit operators")
+        ("implicit", "Use hybrid implicit/explicit operators")
+        ("smr91",    "Advance time per Spalart, Moser, and Rogers 1991")
+        ("yang11",   "Advance time per Shan Yang's 2011 thesis")
+        ("solver",   boost::program_options::value<string>(&solver_spec)
+                         ->default_value(solver_spec),
+                     "Use the specified algorithm for any implicit solves")
     ;
 
     // Initialize application and then process binary-specific options
     // (henceforth suzerain::support::logging macros becomes usable)
-    std::vector<std::string> positional = initialize(argc, argv);
+    vector<string> positional = initialize(argc, argv);
+
+    // Select type of timestepping operators to use (default implicit)
+    options.conflicting_options("implicit", "explicit");
+    if (options.variables().count("explicit")) {
+        use_explicit = true;
+    } else {
+        use_implicit = true;
+    }
+
+    // Select type of timestepping schemes to use (default smr91)
+    options.conflicting_options("smr91", "yang11");
+    if (options.variables().count("yang11")) {
+        use_yang11 = true;
+    } else {
+        use_smr91 = true;
+    }
+
     if (positional.size() != 1) {
         FATAL0("Exactly one restart file name must be specified");
         return EXIT_FAILURE;
     }
     const std::string restart_file = positional[0];
-    const bool clobber = options.variables().count("clobber");
-    if (npower <= 0 || npower > 1) {
-        FATAL0("npower in (0,1] required");
-        return EXIT_FAILURE;
-    }
-    if (grid->k < 4) {
-        FATAL0("k >= 4 required for two non-trivial wall-normal derivatives");
-        return EXIT_FAILURE;
-    }
+
+    // FIXME STARTHERE
 
     DEBUG0("Establishing runtime parallel infrastructure and resources");
     establish_ieee_mode();
     load_grid_and_operators(NULL);
     establish_decomposition();
     establish_state_storage(fields.size(), fields.size());
-
-    DEBUG0("Initializing state_linear to contain the data in wave space");
-    if (mms >= 0) {
-
-        INFO0("Manufactured solution will be initialized at t = " << mms);
-        INFO0("Disabling bulk_rho and bulk_rho_u constraints"
-              " due to manufactured solution use");
-        scenario->bulk_rho   = std::numeric_limits<real_t>::quiet_NaN();
-        scenario->bulk_rho_u = std::numeric_limits<real_t>::quiet_NaN();
-
-        accumulate_manufactured_solution(
-                1, *msoln, 0, *state_nonlinear, *grid, *dgrid, *cop, *b, mms);
-        state_linear->assign(*state_nonlinear);
-
-    } else if (!dgrid->has_zero_zero_modes()) {
-
-        DEBUG("Saving no non-zero information on non-zero-zero rank");
-        msoln.reset();
-        fill(*state_linear, 0);
-
-    } else {
-
-        INFO("Parabolic profile will be initialized with npower = " << npower);
-        msoln.reset();
-        fill(*state_linear, 0);
-
-        DEBUG("Initialization uses constant rho, v, w, and T");
-        const real_t rho = scenario->bulk_rho;
-        const real_t v   = 0;
-        const real_t w   = 0;
-        const real_t T   = 1;
-
-        DEBUG("Finding normalization so u = (y*(L-y))^npower integrates to 1");
-        real_t normalization;
-        if (npower == 1) {
-            // Mathematica: (Integrate[(x (L-x)),{x,0,L}]/L)^(-1)
-            normalization = 6 / pow(grid->L.y(), 2);
-        } else {
-            // Mathematica: (Integrate[(x (L - x))^n, {x, 0, L}]/L)^(-1)
-            //      -  (Gamma[-n] Gamma[3/2+n])
-            //       / (2^(-1-2 n) L^(1+2 n) \[Pi]^(3/2) Csc[n \[Pi]])
-            const real_t num1   = sin(npower * pi<real_t>());
-            const real_t num2   = tgamma(-npower);
-            const real_t num3   = tgamma(real_t(3)/2 + npower);
-            const real_t denom1 = pow(           2, -1-2*npower);
-            const real_t denom2 = pow( grid->L.y(),  1+2*npower);
-            const real_t denom3 = pow(pi<real_t>(),  real_t(3)/2);
-            normalization = - (num1 * num2 * num3 * grid->L.y())
-                          /   (denom1 * denom2 * denom3);
-        }
-
-        DEBUG("Preparing the wall-normal streamwise velocity profile");
-        ArrayXr u(grid->N.y());
-        for (int j = 0; j < u.size(); ++j) {
-            const real_t y_j = b->collocation_point(j);
-            u[j] = scenario->bulk_rho_u
-                 * normalization
-                 * pow(y_j * (grid->L.y() - y_j), npower);
-        }
-
-        DEBUG("Preparing specific internal energy using the equation of state");
-        ArrayXr E = T / (scenario->gamma*(scenario->gamma - 1))
-                  + (scenario->Ma*scenario->Ma/2) * (u*u + v*v + w*w);
-
-        DEBUG("Converting the u and E profiles to B-spline coefficients");
-        // (By partition of unity property rho, v, and w are so already)
-        suzerain::bsplineop_lu masslu(*cop);
-        masslu.opform_mass(*cop);
-        masslu.solve(1, u.data(), 1, u.size());
-        masslu.solve(1, E.data(), 1, E.size());
-
-        DEBUG("Copying the coefficients directly into the zero-zero modes");
-        Map<VectorXc>((*state_linear)[ndx::e  ].origin(), grid->N.y())
-                = (rho * E).cast<complex_t>();
-        Map<VectorXc>((*state_linear)[ndx::mx ].origin(), grid->N.y())
-                = (rho * u).cast<complex_t>();
-        Map<VectorXc>((*state_linear)[ndx::my ].origin(), grid->N.y())
-                .setConstant(rho * v);
-        Map<VectorXc>((*state_linear)[ndx::mz ].origin(), grid->N.y())
-                .setConstant(rho * w);
-        Map<VectorXc>((*state_linear)[ndx::rho].origin(), grid->N.y())
-                .setConstant(rho    );
-
-    }
-
-    DEBUG0("Saving the newly initialized state to disk");
-    if (msoln) {
-        save_restart(mms, restart_file, clobber);
-    } else {
-        save_restart(0,   restart_file, clobber);
-    }
 
     return EXIT_SUCCESS;
 }
