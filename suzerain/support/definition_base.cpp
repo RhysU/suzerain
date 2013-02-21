0a1b791d5da7a61091b823f6448ccb2e07bd892c
Rhys Ulerich <rhys@ices.utexas.edu>
2013-02-21 17:48:40 +0000

    suzerain: #2735 stub definition_base.cpp
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@37207 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/support/definition_base.cpp
@@ -0,0 +1,31 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+// Copyright (C) 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc definition_base.hpp
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+
+#include <suzerain/support/definition_base.hpp>
+
+#include <suzerain/support/logging.hpp>
+
+namespace suzerain {
+
+namespace support {
+
+} // namespace support
+
+} // namespace suzerain
