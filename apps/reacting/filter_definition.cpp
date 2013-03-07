8f09ffb63f786d90ca293415fcb88cf4efe1c207
Victor Topalian <topalian@ices.utexas.edu>
2013-03-07 18:28:54 +0000

    suzerain: add initial filter_definition
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@37650 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/reacting/filter_definition.cpp
@@ -0,0 +1,173 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2010, 2011, 2012, 2013 Rhys Ulerich
+// Copyright (C) 2012, 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc filter_definition.hpp
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+
+#include "filter_definition.hpp"
+
+#include <suzerain/common.hpp>
+#include <suzerain/exprparse.hpp>
+#include <suzerain/support/logging.hpp>
+#include <suzerain/validation.hpp>
+
+/** @file 
+ * Provides classes handling problem defintion for filter
+ * flow, e.g., bulk density and momentum.
+ */
+
+namespace suzerain {
+
+static void parse_positive(const std::string& s, real_t *t, const char *n)
+{
+    const real_t v = exprparse<real_t>(s, n);
+    validation::ensure_positive(v, n);
+    *t = v;
+}
+
+static void parse_nonnegative(const std::string& s, real_t *t, const char *n)
+{
+    const real_t v = exprparse<real_t>(s, n);
+    validation::ensure_nonnegative(v, n);
+    *t = v;
+}
+
+namespace reacting {
+
+filter_definition::filter_definition()
+    : bulk_rho  (std::numeric_limits<real_t>::quiet_NaN())
+    , bulk_rho_u(std::numeric_limits<real_t>::quiet_NaN())
+{
+}
+
+filter_definition::filter_definition(
+        const real_t bulk_rho,
+        const real_t bulk_rho_u)
+    : bulk_rho  (bulk_rho  )
+    , bulk_rho_u(bulk_rho_u)
+{
+}
+
+filter_definition::~filter_definition()
+{
+    // NOP
+}
+
+// Strings used in options_description and populate/override/save/load.
+static const char name_bulk_rho[]   = "bulk_rho";
+static const char name_bulk_rho_u[] = "bulk_rho_u";
+
+// Descriptions used in options_description and populate/override/save/load.
+static const char desc_bulk_rho[]   = "Bulk density target";
+static const char desc_bulk_rho_u[] = "Bulk momentum target";
+
+boost::program_options::options_description
+filter_definition::options_description()
+{
+    using boost::bind;
+    using boost::lexical_cast;
+    using boost::program_options::options_description;
+    using boost::program_options::typed_value;
+    using boost::program_options::value;
+    using std::auto_ptr;
+    using std::string;
+
+    options_description retval("filter flow scenario parameters");
+
+    // Complicated add_options() calls done to allow changing the default value
+    // displayed when the default is NaN.  NaN is used as a NOP value by client
+    // code.  Validation routines used below all silently allow NaNs.
+
+    auto_ptr<typed_value<string> > p;
+
+    // bulk_rho
+    p.reset(value<string>());
+    p->notifier(bind(&parse_nonnegative, _1, &bulk_rho, name_bulk_rho));
+    if (!(boost::math::isnan)(bulk_rho)) {
+        p->default_value(lexical_cast<string>(bulk_rho));
+    }
+    retval.add_options()(name_bulk_rho, p.release(), desc_bulk_rho);
+
+    // bulk_rho_u
+    p.reset(value<string>());
+    p->notifier(bind(&parse_nonnegative, _1, &bulk_rho_u, name_bulk_rho_u));
+    if (!(boost::math::isnan)(bulk_rho_u)) {
+        p->default_value(lexical_cast<string>(bulk_rho_u));
+    }
+    retval.add_options()(name_bulk_rho_u, p.release(), desc_bulk_rho_u);
+
+    return retval;
+}
+
+void
+filter_definition::populate(
+        const filter_definition& that,
+        const bool verbose)
+{
+#define CALL_MAYBE_POPULATE(mem)                                             \
+    maybe_populate(name_ ## mem, desc_ ## mem, this->mem, that.mem, verbose)
+    CALL_MAYBE_POPULATE(bulk_rho);
+    CALL_MAYBE_POPULATE(bulk_rho_u);
+#undef CALL_MAYBE_POPULATE
+}
+
+void
+filter_definition::override(
+        const filter_definition& that,
+        const bool verbose)
+{
+#define CALL_MAYBE_OVERRIDE(mem)                                            \
+    maybe_override(name_ ## mem, desc_ ## mem, this->mem, that.mem, verbose)
+    CALL_MAYBE_OVERRIDE(bulk_rho);
+    CALL_MAYBE_OVERRIDE(bulk_rho_u);
+#undef CALL_MAYBE_OVERRIDE
+}
+
+void
+filter_definition::save(
+        const esio_handle h) const
+{
+    DEBUG0("Storing filter_definition parameters");
+
+    // Only root writes data
+    int procid;
+    esio_handle_comm_rank(h, &procid);
+    esio_line_establish(h, 1, 0, (procid == 0 ? 1 : 0));
+
+    esio_line_write(h, name_bulk_rho,   &this->bulk_rho,   0, desc_bulk_rho);
+    esio_line_write(h, name_bulk_rho_u, &this->bulk_rho_u, 0, desc_bulk_rho_u);
+}
+
+void
+filter_definition::load(
+        const esio_handle h,
+        const bool verbose)
+{
+    DEBUG0("Loading filter_definition parameters");
+
+    // All ranks load
+    esio_line_establish(h, 1, 0, 1);
+
+    filter_definition t;
+    esio_line_read(h, name_bulk_rho,   &t.bulk_rho,   0);
+    esio_line_read(h, name_bulk_rho_u, &t.bulk_rho_u, 0);
+    this->populate(t, verbose);  // Prefer this to incoming
+}
+
+} // namespace reacting
+
+} // namespace suzerain
---
+++ apps/reacting/filter_definition.hpp
@@ -0,0 +1,125 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2010, 2011, 2012, 2013 Rhys Ulerich
+// Copyright (C) 2012, 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_FILTER_DEFINITION_HPP
+#define SUZERAIN_FILTER_DEFINITION_HPP
+
+/** @file
+ * Classes handling reacting channel flow problem scenario parameters.
+ */
+
+#include <esio/esio.h>
+
+#include <suzerain/common.hpp>
+#include <suzerain/support/definition_base.hpp>
+
+/** @file 
+ * Provides classes handling problem defintion for channel
+ * flow, e.g., bulk density and momentum.
+ */
+
+namespace suzerain {
+
+namespace reacting {
+
+/**
+ * Holds parameters defining channel flow case.
+ */
+class filter_definition : public support::definition_base
+{
+public:
+
+    /**
+     * Construct an instance with all parameters set to NaN.
+     * Clients can use NaN as a not-yet-specified or use-the-default value.
+     */
+    filter_definition();
+
+    /**
+     * Construct an instance with the given parameter values.
+     *
+     * @param bulk_rho   Bulk density target.
+     * @param bulk_rho_u Bulk streamwise momentum target.
+     */
+    filter_definition(const real_t bulk_rho,
+                        const real_t bulk_rho_u);
+
+
+    /** Virtual destructor to permit use as a base class */
+    virtual ~filter_definition();
+
+    /**
+     * Populate any NaN members in \c this with values from \c that.
+     * Subclasses should override this method adding any desired functionality
+     * either before or after invoking the superclass version.
+     *
+     * @param that    Instance from which information is taken.
+     * @param verbose Should logging be emitted when a value is retained?
+     */
+    virtual void populate(
+            const filter_definition& that,
+            const bool verbose = false);
+
+    /**
+     * Override members in \c this with non-NaN values from \c that.
+     * Subclasses should override this method adding any desired functionality
+     * either before or after invoking the superclass version.
+     *
+     * @param that    Instance from which information is taken.
+     * @param verbose Should logging be emitted when an override occurs?
+     */
+    virtual void override(
+            const filter_definition& that,
+            const bool verbose = false);
+
+    /**
+     * Save scenario into an ESIO-based file.
+     * Subclasses should override this method adding any desired functionality
+     * either before or after invoking the superclass version.
+     *
+     * @param h Open, writable handle in which details will be saved.
+     */
+    virtual void save(
+            const esio_handle h) const;
+
+    /**
+     * Populate scenario from an ESIO-based file.
+     * Subclasses should override this method adding any desired functionality
+     * either before or after invoking the superclass version.
+     *
+     * @param h       Open, readable handle from which details will be loaded.
+     * @param verbose Should logging be emitted when a value is retained?
+     */
+    virtual void load(
+            const esio_handle h,
+            const bool verbose = true);
+
+    /** @copydoc support::definition_base::options_description() */
+    boost::program_options::options_description options_description();
+
+    /**
+     * The bulk density used as a target for integral constraints.
+     */
+    real_t bulk_rho;
+
+    /**
+     * The bulk streamwise momentum used as a target for integral constraints.
+     */
+    real_t bulk_rho_u;
+
+};
+
+} // namespace reacting
+
+} // namespace suzerain
+
+#endif // SUZERAIN_FILTER_DEFINITION_HPP
