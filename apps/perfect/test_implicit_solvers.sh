db92b18f89afbdc95c92e1d11981c0eee897c32d
Rhys Ulerich <rhys@ices.utexas.edu>
2013-03-13 22:19:55 +0000

    suzerain: add solver consistency checks
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@37781 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/perfect/test_implicit_solvers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -eu
+
+# Initialize test infrastructure
+source "`dirname $0`/test_setup.sh"
+
+: ${ADVANCE:=--advance_nt=1 --statistics_dt=0 --statistics_nt=0}
+: ${OPER:=--implicit} # E.g. '--explicit' or unset to use default
+
+# We want to share wisdom across test cases as much as possible to tamp down
+# rounding-related discrepancies due to FFT kernel differences (ticket #2515)
+WIZ="--plan_wisdom=$(mktemp "--tmpdir=$testdir" wisdom.XXXXXX)"
+
+banner "Generating default implicit solve result for comparison purposes"
+(
+    cd $testdir
+    ../perfect_advance $OPER $ADVANCE $WIZ mms0.h5 \
+                       --restart_destination "a#.h5"
+)
+
+# Run each test case in this file under the following circumstances
+# (which can be overridden by providing the environment variable SPECIFICATION).
+for SPECIFICATION in                                     \
+    zgbsv                                                \
+    zgbsvx,equil=false                                   \
+    zgbsvx,equil=true                                    \
+    zcgbsvx,reuse=false,aiter=1,siter=-1,diter=5,tolsc=0 \
+    zcgbsvx,reuse=true,aiter=1,siter=-1,diter=5,tolsc=0
+do
+
+banner "Similarity of solver specification ${SPECIFICATION:+ ($SPECIFICATION)}"
+(
+    cd $testdir
+    ../perfect_advance $OPER $ADVANCE $WIZ --solver=$SPECIFICATION mms0.h5 \
+                       --restart_destination "b#.h5"
+
+    # Ensure simulation time "/t" matches before bothering with anything else
+    differ --use-system-epsilon --nan a0.h5 b0.h5 /t
+
+    # Only worry about differences in state, not other derived quantities
+    differ --delta=5e-15 --nan a0.h5 b0.h5 /rho
+    differ --delta=5e-15 --nan a0.h5 b0.h5 /rho_u
+    differ --delta=5e-15 --nan a0.h5 b0.h5 /rho_v
+    differ --delta=5e-15 --nan a0.h5 b0.h5 /rho_w
+    differ --delta=5e-15 --nan a0.h5 b0.h5 /rho_E
+)
+
+done
