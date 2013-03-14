8e9981577027082b2b16822e8db302b6c437c891
Rhys Ulerich <rhys@ices.utexas.edu>
2013-03-14 22:41:13 +0000

    suzerain: stub files for zgbsv_bsmbsm_solver class
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@37856 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/zgbsv_bsmbsm_solver.cpp
@@ -0,0 +1,27 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+// Copyright (C) 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc zgbsv_bsmbsm_solver.hpp
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+
+#include <suzerain/zgbsv_bsmbsm_solver.hpp>
+
+namespace suzerain {
+
+// TODO Implement
+
+} // end namespace suzerain
---
+++ suzerain/zgbsv_bsmbsm_solver.hpp
@@ -0,0 +1,31 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+// Copyright (C) 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_ZGBSV_BSMBSM_SOLVER_HPP
+#define SUZERAIN_ZGBSV_BSMBSM_SOLVER_HPP
+
+/** @file
+ * Encapsulates solving BSMBSM problems per a \ref zgbsv_specification.
+ * @see The definition of a BSMBSM problem in \ref bsmbsm.h.
+ */
+
+#include <suzerain/common.hpp>
+#include <suzerain/bsmbsm.h>
+#include <suzerain/zgbsv_specification.hpp>
+
+namespace suzerain {
+
+// TODO Implement
+
+} // namespace suzerain
+
+#endif // SUZERAIN_ZGBSV_BSMBSM_SOLVER_HPP
