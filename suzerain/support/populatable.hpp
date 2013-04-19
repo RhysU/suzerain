d968f221c16d158cd42829f388b471e912161179
Rhys Ulerich <rhys@ices.utexas.edu>
2013-04-19 22:05:28 +0000

    suzerain: scaffolding for override/populate
    No actual functionality just yet.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@38797 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/support/populatable.hpp
@@ -0,0 +1,48 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2010, 2011, 2012, 2013 Rhys Ulerich
+// Copyright (C) 2012, 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_SUPPORT_POPULATABLE_HPP
+#define SUZERAIN_SUPPORT_POPULATABLE_HPP
+
+/** @file
+ * Provides \ref populatable.
+ */
+
+namespace suzerain {
+
+namespace support {
+
+/**
+ * Abstract interface indicating details may be populated from another instance.
+ */
+class populatable
+{
+public:
+
+};
+
+} // namespace support
+
+} // namespace suzerain
+
+#endif // SUZERAIN_SUPPORT_POPULATABLE_HPP
