5273c94c60780953708804e8fab20167ac2d1464
Rhys Ulerich <rhys@ices.utexas.edu>
2013-04-19 21:32:36 +0000

    suzerain: saveable/loadable draft intf
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@38795 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/support/loadable.hpp
@@ -0,0 +1,60 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2010, 2011, 2012, 2013 Rhys Ulerich
+// Copyright (C) 2012, 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_SUPPORT_LOADABLE_HPP
+#define SUZERAIN_SUPPORT_LOADABLE_HPP
+
+/** @file
+ * Provides \ref loadable.
+ */
+
+#include <esio/esio.h>
+
+namespace suzerain {
+
+namespace support {
+
+/** Abstract interface indicating state may be loaded from an ESIO handle. */
+class loadable
+{
+public:
+
+    /**
+     * Load state from an ESIO-based file.  Descendants of instances should
+     * override this method adding any desired functionality either before or
+     * after invoking the superclass version.
+     *
+     * @param h       Open, readable handle from which details will be loaded.
+     * @param verbose Should logging be emitted when a value is retained?
+     */
+    virtual void load(
+            const esio_handle h,
+            const bool verbose = true) = 0;
+
+};
+
+} // namespace support
+
+} // namespace suzerain
+
+#endif // SUZERAIN_SUPPORT_LOADABLE_HPP
