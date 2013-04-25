2b8562a856a2c08cf6beb441a407e709b70847d9
Rhys Ulerich <rhys@ices.utexas.edu>
2013-04-25 16:42:18 +0000

    suzerain: checkpoint S, inv_S progress
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@38903 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/nonreflecting_treatment.cpp
+++ apps/perfect/nonreflecting_treatment.cpp
@@ -64,9 +64,28 @@ std::vector<real_t> nonreflecting_treatment::apply_operator(
     // State enters method as coefficients in X, Y, and Z directions
 
     // Retrieve upper boundary reference state from the common block
-    // FIXME STARTHERE
-    // const real_t rho = common.
-    // const real_t u
+    const real_t rho = common.ref_rho().tail<1>()[0];
+    const real_t u   = common.ref_ux ().tail<1>()[0];
+    const real_t v   = common.ref_uy ().tail<1>()[0];
+    const real_t w   = common.ref_uz ().tail<1>()[0];
+    const real_t a   = common.ref_a  ().tail<1>()[0];
+
+    // Prepare oft-used derived quantities
+    const real_t inv_rho = 1 / rho;
+    const real_t u2      = u * u;
+    const real_t v2      = v * v;
+    const real_t w2      = w * w;
+    const real_t a2      = a * a;
+
+    // Prepare oft-used scenario-related constants
+    const real_t gamma      = scenario.gamma;
+    const real_t inv_gamma  = 1 / gamma;
+    const real_t gamma1     = gamma - 1;
+    const real_t inv_gamma1 = 1 / gamma1;
+    const real_t Ma         = scenario.Ma;
+    const real_t inv_Ma     = 1 / Ma;
+    const real_t Ma2        = Ma * Ma;
+    const real_t inv_Ma2    = 1 / Ma2;
 
     // Build the appropriate 5x5 projection matrices:
     //   1) The documentation lists these as rho, rho_u, rho_v, rho_w, rho_E.
@@ -74,31 +93,54 @@ std::vector<real_t> nonreflecting_treatment::apply_operator(
     //   3) Form piece parts for readability and then pre-compute products.
     Matrix5r RY = Matrix5r::Zero();
     {
-        RY(ndx::rho, ndx::rho) = 1;
+        RY(ndx::e  , ndx::e  ) = 1;
         RY(ndx::mx , ndx::my ) = 1;
         RY(ndx::my , ndx::mz ) = 1;
         RY(ndx::mz , ndx::mx ) = 1;
-        RY(ndx::e  , ndx::e  ) = 1;
+        RY(ndx::rho, ndx::rho) = 1;
     }
 
     Matrix5r inv_RY = Matrix5r::Zero();
     {
-        inv_RY(ndx::rho, ndx::rho) = 1;
+        inv_RY(ndx::e  , ndx::e  ) = 1;
         inv_RY(ndx::mx , ndx::mz ) = 1;
         inv_RY(ndx::my , ndx::mx ) = 1;
         inv_RY(ndx::mz , ndx::my ) = 1;
-        inv_RY(ndx::e  , ndx::e  ) = 1;
+        inv_RY(ndx::rho, ndx::rho) = 1;
     }
 
     Matrix5r S = Matrix5r::Zero();
     {
-        S(ndx::rho, ndx::rho) = 1;
-        // FIXME STARTHERE
-
+        S(ndx::e  , ndx::e )  =   gamma1 * inv_Ma2;
+        S(ndx::e  , ndx::mx)  = - gamma1 * u;
+        S(ndx::e  , ndx::my)  = - gamma1 * v;
+        S(ndx::e  , ndx::mz)  = - gamma1 * w;
+        S(ndx::e  , ndx::rho) =   a2 * inv_gamma * inv_Ma2;
+        S(ndx::mx , ndx::mx)  =   inv_rho;
+        S(ndx::mx , ndx::rho) = - u * inv_rho;
+        S(ndx::my , ndx::my)  =   inv_rho;
+        S(ndx::my , ndx::rho) = - v * inv_rho;
+        S(ndx::mz , ndx::mz)  =   inv_rho;
+        S(ndx::mz , ndx::rho) = - w * inv_rho;
+        S(ndx::rho, ndx::rho) =   1;
     }
 
     Matrix5r inv_S  = Matrix5r::Zero();
-
+    {
+        S(ndx::e  , ndx::e )  =   Ma2 * inv_gamma1;
+        S(ndx::e  , ndx::mx)  =   Ma2 * rho * u;
+        S(ndx::e  , ndx::my)  =   Ma2 * rho * v;
+        S(ndx::e  , ndx::mz)  =   Ma2 * rho * w;
+        S(ndx::e  , ndx::rho) =   Ma2 * (u2 + v2 + w2)
+                              +   a2 * inv_gamma * inv_gamma1;
+        S(ndx::mx , ndx::mx)  =   rho;
+        S(ndx::mx , ndx::rho) =   u;
+        S(ndx::my , ndx::my)  =   rho;
+        S(ndx::my , ndx::rho) =   v;
+        S(ndx::mz , ndx::mz)  =   rho;
+        S(ndx::mz , ndx::rho) =   w;
+        S(ndx::rho, ndx::rho) =   1;
+    }
 
     Matrix5r VL = Matrix5r::Zero(), inv_VL = Matrix5r::Zero();
     Matrix5r BG = Matrix5r::Zero();
