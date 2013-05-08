0c5e7c50c66085102005bea668fc87ff6c20eb2e
Rhys Ulerich <rhys@ices.utexas.edu>
2013-05-08 03:04:42 +0000

    suzerain: #2399 use bitfields for brevity
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@39187 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/isothermal_mass_operator.cpp
+++ suzerain/isothermal_mass_operator.cpp
@@ -59,26 +59,26 @@ class isothermal_enforcer
     : public boost::noncopyable // To defend against expense in for_each
 {
 
-    /** Bitmasks used to precompute and cache which conditions to #flags. */
-    enum {
-        ENFORCE_LOWER_E  = 1 << 0,
-        ENFORCE_LOWER_U  = 1 << 1,
-        ENFORCE_LOWER_V  = 1 << 2,
-        ENFORCE_LOWER_W  = 1 << 3,
-        ENFORCE_LOWER_CS = 1 << 4,
-        ENFORCE_UPPER_E  = 1 << 5,
-        ENFORCE_UPPER_U  = 1 << 6,
-        ENFORCE_UPPER_V  = 1 << 7,
-        ENFORCE_UPPER_W  = 1 << 8,
-        ENFORCE_UPPER_CS = 1 << 9,
-    };
-
     const std::size_t               Ny;       ///< # of points across y
     const std::ptrdiff_t            incf;     ///< Stride between fields
     const isothermal_specification& spec;     ///< Boundary specification
     const real_t                    lower_E;  ///< Specific total energy
     const real_t                    upper_E;  ///< Specific total energy
-    const unsigned int              flags;    ///< Which BCs are enforced?
+
+    /**
+     * Bit fields marking exactly which BCs should be enforced.
+     * @{*/
+    const int enforce_lower_e  : 1;
+    const int enforce_upper_e  : 1;
+    const int enforce_lower_u  : 1;
+    const int enforce_upper_u  : 1;
+    const int enforce_lower_v  : 1;
+    const int enforce_upper_v  : 1;
+    const int enforce_lower_w  : 1;
+    const int enforce_upper_w  : 1;
+    const int enforce_lower_cs : 1;
+    const int enforce_upper_cs : 1;
+    /**@}*/
 
 public:
 
@@ -93,18 +93,16 @@ public:
         , spec(spec)
         , lower_E(lower_E)
         , upper_E(upper_E)
-        , flags(
-            ENFORCE_LOWER_E  * !(boost::math::isnan)(lower_E     )
-          | ENFORCE_LOWER_U  * !(boost::math::isnan)(spec.lower_u)
-          | ENFORCE_LOWER_V  * !(boost::math::isnan)(spec.lower_v)
-          | ENFORCE_LOWER_W  * !(boost::math::isnan)(spec.lower_w)
-          | ENFORCE_LOWER_CS
-          | ENFORCE_UPPER_E  * !(boost::math::isnan)(upper_E     )
-          | ENFORCE_UPPER_U  * !(boost::math::isnan)(spec.upper_u)
-          | ENFORCE_UPPER_V  * !(boost::math::isnan)(spec.upper_v)
-          | ENFORCE_UPPER_W  * !(boost::math::isnan)(spec.upper_w)
-          | ENFORCE_UPPER_CS
-          )
+        , enforce_lower_e (!(boost::math::isnan)(lower_E     ))
+        , enforce_upper_e (!(boost::math::isnan)(upper_E     ))
+        , enforce_lower_u (!(boost::math::isnan)(spec.lower_u))
+        , enforce_upper_u (!(boost::math::isnan)(spec.upper_u))
+        , enforce_lower_v (!(boost::math::isnan)(spec.lower_v))
+        , enforce_upper_v (!(boost::math::isnan)(spec.upper_v))
+        , enforce_lower_w (!(boost::math::isnan)(spec.lower_w))
+        , enforce_upper_w (!(boost::math::isnan)(spec.upper_w))
+        , enforce_lower_cs(true)
+        , enforce_upper_cs(true)
     {}
 
     /**
@@ -120,37 +118,37 @@ public:
 #define UPPER(var) ((&upper_rho)[((var) - ndx::rho)*incf])
 
         // Set total energy to be density times specific total energy
-        if (flags & ENFORCE_LOWER_E) LOWER(ndx::e) = lower_rho * lower_E;
-        if (flags & ENFORCE_UPPER_E) UPPER(ndx::e) = upper_rho * upper_E;
+        if (enforce_lower_e) LOWER(ndx::e) = lower_rho * lower_E;
+        if (enforce_upper_e) UPPER(ndx::e) = upper_rho * upper_E;
 
         // Set streamwise momentum to be density times prescribed velocity
-        if (flags & ENFORCE_LOWER_U) LOWER(ndx::mx) = lower_rho * spec.lower_u;
-        if (flags & ENFORCE_UPPER_U) UPPER(ndx::mx) = upper_rho * spec.upper_u;
+        if (enforce_lower_u) LOWER(ndx::mx) = lower_rho * spec.lower_u;
+        if (enforce_upper_u) UPPER(ndx::mx) = upper_rho * spec.upper_u;
 
         // Set wall-normal momentum to be density times prescribed velocity
-        if (flags & ENFORCE_LOWER_V) LOWER(ndx::my) = lower_rho * spec.lower_v;
-        if (flags & ENFORCE_UPPER_V) UPPER(ndx::my) = upper_rho * spec.upper_v;
+        if (enforce_lower_v) LOWER(ndx::my) = lower_rho * spec.lower_v;
+        if (enforce_upper_v) UPPER(ndx::my) = upper_rho * spec.upper_v;
 
         // Set spanwise momentum to be density times prescribed velocity
-        if (flags & ENFORCE_LOWER_W) LOWER(ndx::mz) = lower_rho * spec.lower_w;
-        if (flags & ENFORCE_UPPER_W) UPPER(ndx::mz) = upper_rho * spec.upper_w;
+        if (enforce_lower_w) LOWER(ndx::mz) = lower_rho * spec.lower_w;
+        if (enforce_upper_w) UPPER(ndx::mz) = upper_rho * spec.upper_w;
 
         // Do nothing to the density equation
 
         // Set species partial densities to be density times mixture fraction.
-        if (flags & (ENFORCE_LOWER_CS | ENFORCE_UPPER_CS)) {
+        if (enforce_lower_cs & enforce_upper_cs) {
             assert(spec.lower_cs.size() == spec.upper_cs.size());
             const std::size_t num_species = spec.lower_cs.size();
             for (std::size_t s = 1; s < num_species; ++s) {
                 LOWER(ndx::rho_(s)) = lower_rho * spec.lower_cs[s];
                 UPPER(ndx::rho_(s)) = upper_rho * spec.upper_cs[s];
             }
-        } else if (flags & ENFORCE_LOWER_CS) {
+        } else if (enforce_lower_cs) {
             const std::size_t num_species = spec.lower_cs.size();
             for (std::size_t s = 1; s < num_species; ++s) {
                 LOWER(ndx::rho_(s)) = lower_rho * spec.lower_cs[s];
             }
-        } else if (flags & ENFORCE_UPPER_CS) {
+        } else if (enforce_upper_cs) {
             const std::size_t num_species = spec.upper_cs.size();
             for (std::size_t s = 1; s < num_species; ++s) {
                 UPPER(ndx::rho_(s)) = upper_rho * spec.upper_cs[s];
