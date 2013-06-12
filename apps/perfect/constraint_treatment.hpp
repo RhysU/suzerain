RhysU/suzerain

commit 69b6c73d220f650319f75917ca01e6a382f9a86d

suzerain: #2399 use isothermal_spec for references
Removes operator_common_block from nonreflecting_treatment.{hpp,cpp}.
More in line with what Victor has been doing.  Requires some paired
choices within main_advance.cpp which are not yet done.

git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@39977 0bd85f0b-c151-43b9-a1e9-27231104551f

apps/perfect/main_advance.cpp
@@ -222,8 +222,7 @@ suzerain::perfect::driver_advance::run(int argc, char **argv)
             INFO0(who, "Preparing nonreflecting upper boundary treatment");
             shared_ptr<nonreflecting_treatment> nonreflecting(
                     new nonreflecting_treatment(
-                        *scenario, *isothermal,
-                        *grid, *dgrid, *cop, *b, common_block));
+                        *scenario, *isothermal, *grid, *dgrid, *cop, *b));
             nonreflecting->N = N;
             N = nonreflecting;
         }

apps/perfect/nonreflecting_treatment.cpp
@@ -30,11 +30,9 @@
 #include <suzerain/bspline.hpp>
 #include <suzerain/grid_specification.hpp>
 #include <suzerain/inorder.hpp>
-#include <suzerain/isothermal_specification.hpp>
 #include <suzerain/ndx.hpp>
 #include <suzerain/pencil_grid.hpp>
 
-#include "common_block.hpp"
 #include "scenario_definition.hpp"
 
 #pragma float_control(precise, on)
@@ -61,12 +59,10 @@ nonreflecting_treatment::nonreflecting_treatment(
         const grid_specification &grid,
         const pencil_grid &dgrid,
         const bsplineop &cop,
-        bspline &b,
-        const operator_common_block &common)
+        bspline &b)
     : operator_base(grid, dgrid, cop, b)
     , scenario(scenario)
     , isothermal(isothermal)
-    , common(common)
     , who("nonreflecting_treatment")
 {
 #ifndef NDEBUG
@@ -126,12 +122,7 @@ nonreflecting_treatment::apply_operator(
     // The hideous const_cast is required due to timestepping API.
     if (substep_index == 0) {
         const_cast<nonreflecting_treatment*>(this)
-                ->compute_giles_matrices(common.ref_rho().tail<1>()[0],
-                                         common.ref_ux ().tail<1>()[0],
-                                         common.ref_uy ().tail<1>()[0],
-                                         common.ref_uz ().tail<1>()[0],
-                                         common.ref_a  ().tail<1>()[0],
-                                         1 /* y = L_y has positive normal */);
+                ->compute_giles_matrices_upper();
     }
 
     // Wavenumber traversal modeled after those found in suzerain/diffwave.c

apps/perfect/nonreflecting_treatment.hpp
@@ -29,6 +29,7 @@
  */
 
 #include <suzerain/common.hpp>
+#include <suzerain/isothermal_specification.hpp>
 #include <suzerain/operator_base.hpp>
 #include <suzerain/state_fwd.hpp>
 #include <suzerain/timestepper.hpp>
@@ -39,13 +40,11 @@ namespace suzerain {
 class bspline;
 class bsplineop;
 class grid_specification;
-class isothermal_specification;
 class pencil_grid;
 
 namespace perfect {
 
 // Forward declarations
-class operator_common_block;
 class scenario_definition;
 
 /**
@@ -72,8 +71,7 @@ public:
             const grid_specification& grid,
             const pencil_grid& dgrid,
             const bsplineop& cop,
-            bspline& b,
-            const operator_common_block& common);
+            bspline& b);
 
     /**
      * Applies Giles conditions delegating most processing to #N.
@@ -121,46 +119,14 @@ public:
 
 protected:
 
-    /**
-     * Compute Medida's Giles-like nonreflecting boundary condition matrices
-     * given reference state information and the \ref scenario_definition
-     * provided at construction time.  Inflow vs outflow and subsonic vs
-     * supersonic conditions are determined using \c ref_v and \c normal_sign.
-     * Updates all of
-     * <ol>
-     *   <li>#VL_S_RY</li>
-     *   <li>#PG_BG_VL_S_RY_by_chi</li>
-     *   <li>#PG_CG_VL_S_RY_by_chi</li>
-     *   <li>#ImPG_CG_VL_S_RY</li>
-     *   <li>#invVL_S_RY</li>
-     * </ol>
-     * during invocation.
-     *
-     * @param ref_rho     Nondimensional reference density \f$rho\f$.
-     * @param ref_u       Nondimensional reference streamwise velocity \f$u\f$.
-     * @param ref_v       Nondimensional reference velocity \f$v\f$.
-     * @param ref_w       Nondimensional reference spanwise velocity \f$w\f$.
-     * @param ref_a       Nondimensional reference sound speed \f$a\f$.
-     * @param normal_sign Sign of a boundary-normal vector.
-     *                    For the boundary at \f$y=0\f$ this should be negative.
-     *                    For the boundary at \f$y=L_y\f$, it must be positive.
-     */
-    void compute_giles_matrices(
-            const real_t ref_rho,
-            const real_t ref_u,
-            const real_t ref_v,
-            const real_t ref_w,
-            const real_t ref_a,
-            const real_t normal_sign);
-
     /** The scenario in which the operator is used. */
     const scenario_definition &scenario;
 
-    /** The lower and upper isothermal boundary specification. */
+    /**
+     * The lower and upper isothermal boundary specification.
+     * Provides the reference values used for nonreflecting treatment.
+     */
     const isothermal_specification &isothermal;
-
-    /** Provides reference values used when computing the conditions. */
-    const operator_common_block &common;
 
     /**
      * Wavenumber-independent matrix
@@ -196,6 +162,66 @@ protected:
      * used by apply_operator().
      */
     Matrix5r inv_VL_S_RY;
+
+    /**
+     * Compute Medida's Giles-like nonreflecting boundary condition matrices
+     * given reference state information and the \ref scenario_definition
+     * provided at construction time.  Inflow vs outflow and subsonic vs
+     * supersonic conditions are determined using \c ref_v and \c normal_sign.
+     * Updates all of
+     * <ol>
+     *   <li>#VL_S_RY</li>
+     *   <li>#PG_BG_VL_S_RY_by_chi</li>
+     *   <li>#PG_CG_VL_S_RY_by_chi</li>
+     *   <li>#ImPG_CG_VL_S_RY</li>
+     *   <li>#invVL_S_RY</li>
+     * </ol>
+     * during invocation.
+     *
+     * @param ref_rho     Nondimensional reference density \f$rho\f$.
+     * @param ref_u       Nondimensional reference streamwise velocity \f$u\f$.
+     * @param ref_v       Nondimensional reference velocity \f$v\f$.
+     * @param ref_w       Nondimensional reference spanwise velocity \f$w\f$.
+     * @param ref_a       Nondimensional reference sound speed \f$a\f$.
+     * @param normal_sign Sign of a boundary-normal vector.
+     *                    For the boundary at \f$y=0\f$ this should be negative.
+     *                    For the boundary at \f$y=L_y\f$, it must be positive.
+     */
+    void compute_giles_matrices(
+            const real_t ref_rho,
+            const real_t ref_u,
+            const real_t ref_v,
+            const real_t ref_w,
+            const real_t ref_a,
+            const real_t normal_sign);
+
+    /**
+     * Invokes compute_giles_matrices() for lower boundary.
+     * Broken out for brevity and to document reference state choices.
+     */
+    void compute_giles_matrices_lower()
+    {
+        return compute_giles_matrices(isothermal.lower_rho,
+                                      isothermal.lower_u,
+                                      isothermal.lower_v,
+                                      isothermal.lower_w,
+                                      std::sqrt(isothermal.lower_T),
+                                      -1);
+    }
+
+    /**
+     * Invokes compute_giles_matrices() for upper boundary.
+     * Broken out for brevity and to document reference state choices.
+     */
+    void compute_giles_matrices_upper()
+    {
+        return compute_giles_matrices(isothermal.upper_rho,
+                                      isothermal.upper_u,
+                                      isothermal.upper_v,
+                                      isothermal.upper_w,
+                                      std::sqrt(isothermal.upper_T),
+                                      +1);
+    }
 
 private:
 

commit 965d0c7dc0f46a9fd4f44fa403a881d4b81a7f49

suzerain: draft of constraining freestream
Also should, with --undriven, again enable testing progress on #2399.
That has been broken since r39977 due to NaN isothermal->upper_rho.

git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40087 0bd85f0b-c151-43b9-a1e9-27231104551f

apps/perfect/constraint_treatment.hpp
@@ -96,8 +96,9 @@ public:
             const pencil_grid& dgrid,
             operator_common_block& common);
 
-    // Permit subscripting to access scalar equation-specific constraints
+    // Permit subscripting/iterating to access equation-specific constraints
     using implementation_defined::operator[];
+    using implementation_defined::size;
 
     /** Delegates invocation to #L */
     virtual void apply_mass_plus_scaled_operator(

apps/perfect/main_advance.cpp
@@ -108,6 +108,8 @@ suzerain::perfect::driver_advance::run(int argc, char **argv)
         ("solver",   boost::program_options::value(&solver_spec)
                          ->default_value(solver_spec),
                      "Use the specified algorithm for --implicit solves")
+        ("undriven", boost::program_options::bool_switch(),
+                     "Disable all constraint-based driving forces")
     ;
 
     // Initialize application and then process binary-specific options
@@ -198,9 +200,9 @@ suzerain::perfect::driver_advance::run(int argc, char **argv)
     // Prepare any necessary, problem-specific constraints
     shared_ptr<constraint_treatment> constrainer(new constraint_treatment(
                     scenario->Ma, *dgrid, common_block));
-    if        (grid->two_sided()) { // Channel problem
+    if        (grid->two_sided()) { // Channel per channel_treatment.tex
 
-        INFO0(who, "Establishing channel-like bulk state constraints");
+        INFO0(who, "Establishing driving, channel-like state constraints");
         (*constrainer)[ndx::rho].reset(
                 new constraint::reference_bulk(scenario->bulk_rho  , *b));
         (*constrainer)[ndx::mx ].reset(
@@ -208,12 +210,47 @@ suzerain::perfect::driver_advance::run(int argc, char **argv)
         (*constrainer)[ndx::e  ].reset(
                 new constraint::reference_bulk(scenario->bulk_rho_E, *b));
 
-    } else if (grid->one_sided()) { // Flat plate
+    } else if (grid->one_sided()) { // Flat plate per plate_treatment.tex
 
-        DEBUG0(who, "Disabling any channel-like, bulk state constraints");
-        scenario->bulk_rho   = numeric_limits<real_t>::quiet_NaN();
-        scenario->bulk_rho_u = numeric_limits<real_t>::quiet_NaN();
-        scenario->bulk_rho_E = numeric_limits<real_t>::quiet_NaN();
+        INFO0(who, "Computing mean freestream behavior per plate scenario");
+        using namespace std;
+        const real_t T_inf   = isothermal->upper_T;               // Brevity
+        const real_t u_inf   = sqrt(T_inf);                       // Eqn ( 6)
+        const real_t mx_inf  = pow(T_inf, scenario->beta);        // Eqn ( 7)
+        const real_t rho_inf = pow(T_inf, scenario->beta - 0.5);  // Eqn ( 8)
+        const real_t e_inf   = pow(T_inf, scenario->beta + 0.5)   // Eqn (12)
+                             * (
+                                   1 / (scenario->gamma*(scenario->gamma-1))
+                                 + scenario->Ma*scenario->Ma / 2
+                               );
+
+        INFO0(who, "Setting freestream reference state on upper boundary");
+        isothermal->upper_rho = rho_inf;
+        isothermal->upper_u   = u_inf;
+        DEBUG0(who, "Retaining reference upper_v = " << isothermal->upper_v);
+        DEBUG0(who, "Retaining reference upper_w = " << isothermal->upper_w);
+        DEBUG0(who, "Retaining reference upper_T = " << isothermal->upper_T);
+
+        if (scenario->bulk_rho) {
+            WARN0(who, "Removing channel-like bulk_rho setting");
+            scenario->bulk_rho   = numeric_limits<real_t>::quiet_NaN();
+        }
+        if (scenario->bulk_rho_u) {
+            WARN0(who, "Removing channel-like bulk_rho_u setting");
+            scenario->bulk_rho_u = numeric_limits<real_t>::quiet_NaN();
+        }
+        if (scenario->bulk_rho_E) {
+            WARN0(who, "Removing channel-like bulk_rho_E setting");
+            scenario->bulk_rho_E = numeric_limits<real_t>::quiet_NaN();
+        }
+
+        INFO0(who, "Establishing driving, freestream-like state constraints");
+        (*constrainer)[ndx::rho].reset(
+                new constraint::constant_upper(rho_inf, *b));
+        (*constrainer)[ndx::mx ].reset(
+                new constraint::constant_upper(mx_inf,  *b));
+        (*constrainer)[ndx::e  ].reset(
+                new constraint::constant_upper(e_inf,   *b));
 
     } else {
 
@@ -262,6 +299,15 @@ suzerain::perfect::driver_advance::run(int argc, char **argv)
         FATAL0(who, "Sanity error in operator selection");
         return EXIT_FAILURE;
 
+    }
+
+    // Use --undriven as a testing- and debugging-related tool.
+    // For example, to investigate nonreflecting boundary condition behavior.
+    if (options.variables()["undriven"].as<bool>()) {
+        INFO0(who, "Disabling all established constraints per --undriven flag");
+        for (std::size_t i = 0; i < constrainer->size(); ++i) {
+            (*constrainer)[i].reset(new constraint::disabled());
+        }
     }
 
     // Perform final housekeeping and then advance time as requested