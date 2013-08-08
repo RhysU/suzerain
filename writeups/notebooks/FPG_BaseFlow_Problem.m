555e6de79cab566775915fec0b24fdb72019bad1
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-08 19:54:17 +0000

    suzerain: new grad p computation
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40669 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/FPG_BaseFlow_Problem.m
+++ writeups/notebooks/FPG_BaseFlow_Problem.m
@@ -3,15 +3,10 @@ up = @(r,u,Ma,g0) -(u./r) .* (2 + Ma**2*(g0-1) - Ma**2*(g0-1)*u.**2) ...
                           ./ (2 + Ma**2*(g0-1) - Ma**2*(g0+1)*u.**2);
 
 % Computations for sound speed squared, density, and pressure given r, u
-a2  = @(u,Ma,g0) ...
-      1 + Ma**2 * (g0 - 1) * (1 - u.**2) / 2;
-
-rho = @(r,u,a2,Ma,g0,rho1) ...
-      rho1 * exp(-2*pi*Ma**2 * cumtrapz(r, r.*u.*up(r,u,Ma,g0) ./ a2));
-
-% p   = @(r,a2,rho,p1) ...
-%       p1 + 2*pi * cumtrapz(r, r.*a2.*)
-
+a2    = @(u,Ma,g0) 1 + Ma**2 * (g0 - 1) * (1 - u.**2) / 2;
+rho   = @(r,u,a2,Ma,g0,rho1) ...
+        rho1 * exp(-2*pi*Ma**2 * cumtrapz(r, r.*u.*up(r,u,Ma,g0) ./ a2));
+gradp = @(r,u,rho,Ma,g0) Ma**2 * rho.*u.*up(r,u,Ma,g0);
 
 % Establish tolerances, integration options, domain size, and parameters
 tol  = sqrt(eps);
@@ -22,13 +17,15 @@ Ma = 1; g0 = 1.4;
 
 % Subsonic converging nozzle with outflow R1 and inflow R2
 [sub_r, sub_u] = ode45(up, [R1 R2], -1/Ma/(1+tol), vopt, Ma, g0);
-sub_a2  = a2(sub_u, Ma, g0);
-sub_rho = rho(sub_r, sub_u, sub_a2, Ma, g0, 1);
+sub_a2    = a2(sub_u, Ma, g0);
+sub_rho   = rho(sub_r, sub_u, sub_a2, Ma, g0, 1);
+sub_gradp = gradp(sub_r, sub_u, sub_rho, Ma, g0);
 
 % Supersonic diverging nozzle with inflow R1 and outflow R2
 [sup_r, sup_u] = ode45(up, [R1 R2], 1/Ma + tol,    vopt, Ma, g0);
 sup_a2  = a2(sup_u, Ma, g0);
 sup_rho = rho(sup_r, sup_u, sup_a2, Ma, g0, 1);
+sup_gradp = gradp(sup_r, sup_u, sup_rho, Ma, g0);
 
 % Plot both results
 plot(sub_r, sub_u, '-', sup_r, sup_u, '-');
