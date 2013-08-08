44d4731fa683da7e93f4075909a57b935f56b946
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-08 21:00:29 +0000

    suzerain: fix pressure integration
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40671 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/FPG_BaseFlow_Problem.m
+++ writeups/notebooks/FPG_BaseFlow_Problem.m
@@ -3,31 +3,43 @@ up = @(r,u,Ma,g0) -(u./r) .* (2 + Ma**2*(g0-1) - Ma**2*(g0-1)*u.**2) ...
                           ./ (2 + Ma**2*(g0-1) - Ma**2*(g0+1)*u.**2);
 
 % Computations for sound speed squared, density, and pressure given r, u
-a2    = @(u,Ma,g0) 1 + Ma**2 * (g0 - 1) * (1 - u.**2) / 2;
-rho   = @(r,u,a2,Ma,g0,rho1) ...
-        rho1 * exp(-2*pi*Ma**2 * cumtrapz(r, r.*u.*up(r,u,Ma,g0) ./ a2));
-gradp = @(r,u,rho,Ma,g0) Ma**2 * rho.*u.*up(r,u,Ma,g0);
+a2  = @(u,Ma,g0) ...
+      1 + Ma**2 * (g0 - 1) * (1 - u.**2) / 2;
+rho = @(r,u,a2,Ma,g0,rho1) ...
+      rho1 * exp(-2*pi*Ma**2 * cumtrapz(r, r.*u.*up(r,u,Ma,g0) ./ a2));
+p   = @(r,u,rho,Ma,g0,p1) ...
+      p1 - 2*pi*Ma**2 * cumtrapz(r, r.*rho.*u.*up(r,u,Ma,g0));
 
 % Establish tolerances, integration options, domain size, and parameters
-tol  = sqrt(eps);
 pkg load odepkg;
-vopt = odeset("RelTol", tol, "AbsTol", tol, "InitialStep", sqrt(tol));
-R1 = 1; R2 = 4;
+tol = sqrt(eps);
+vopt = odeset('RelTol',      tol, 'AbsTol',  tol, ...
+              'InitialStep', tol, 'MaxStep', sqrt(sqrt(tol)));
+R1 = 1; R2 = 2;
 Ma = 1; g0 = 1.4;
 
-% Subsonic converging nozzle with outflow R1 and inflow R2
+% Solve initial value problem for subsonic nozzle
 [sub_r, sub_u] = ode45(up, [R1 R2], -1/Ma/(1+tol), vopt, Ma, g0);
-sub_a2    = a2(sub_u, Ma, g0);
-sub_rho   = rho(sub_r, sub_u, sub_a2, Ma, g0, 1);
-sub_gradp = gradp(sub_r, sub_u, sub_rho, Ma, g0);
 
-% Supersonic diverging nozzle with inflow R1 and outflow R2
+% Postprocess to obtain thermodynamic state and then plot
+sub_a2  = a2 (sub_u,                 Ma, g0   );
+sub_rho = rho(sub_r, sub_u, sub_a2,  Ma, g0, 1);
+sub_p   = p  (sub_r, sub_u, sub_rho, Ma, g0, 1);
+figure();
+plot(sub_r, sub_u, 'r-', sub_r, sub_rho, 'b-', sub_r, sub_p, 'g-');
+legend('velocity', 'density', 'pressure');
+title('Subsonic nozzle: outflow -> inflow')
+xlabel('radius');
+
+% Solve initial value problem for supersonic nozzle
 [sup_r, sup_u] = ode45(up, [R1 R2], 1/Ma + tol,    vopt, Ma, g0);
-sup_a2  = a2(sup_u, Ma, g0);
-sup_rho = rho(sup_r, sup_u, sup_a2, Ma, g0, 1);
-sup_gradp = gradp(sup_r, sup_u, sup_rho, Ma, g0);
 
-% Plot both results
-plot(sub_r, sub_u, '-', sup_r, sup_u, '-');
-xlabel('r'); ylabel('u');
-legend('Subsonic', 'Supersonic', 'location', 'southoutside');
+% Postprocess to obtain thermodynamic state and then plot
+sup_a2  = a2 (sup_u,                 Ma, g0   );
+sup_rho = rho(sup_r, sup_u, sup_a2,  Ma, g0, 1);
+sup_p   = p  (sup_r, sup_u, sup_rho, Ma, g0, 1);
+figure();
+plot(sup_r, sup_u, 'r-', sub_r, sup_rho, 'b-', sup_r, sup_p, 'g-');
+legend('velocity', 'density', 'pressure');
+title('Supersonic nozzle: inflow -> outflow')
+xlabel('radius');
