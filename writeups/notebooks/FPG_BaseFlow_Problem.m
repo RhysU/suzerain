94d5ca04e3ac1cb6d65906b08b6374531ad1b10c
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-11 20:21:28 +0000

    suzerain: return up, pp
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40692 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/FPG_BaseFlow_Problem.m
+++ writeups/notebooks/FPG_BaseFlow_Problem.m
@@ -4,7 +4,7 @@
 % Solve nozzle initial value problem for u(r) on [R1, R2] given u1(R1)
 % via an ODE integrator-ready implementation of velocity derivative u'(r, u).
 % Afterward, compute sound speed squared, density, and pressure from r, u.
-function [r, u, a2, rho, p] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1)
+function [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1)
 
   Ma2   = Ma*Ma;
   up    = @(r,u) -(u./r) .* (2 + Ma2*(gam0 - 1) - Ma2*(gam0 - 1)*u.**2) ...
@@ -16,7 +16,8 @@ function [r, u, a2, rho, p] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1)
   up    = up(r, u); % Shadow
   a2    = 1 + 0.5*Ma2*(gam0 - 1)*(1 - u.**2);
   rho   = rho1 * exp(-2*pi*Ma2 * cumtrapz(r, r.*u.*up ./ a2));
-  p     = p1 - 2*pi*Ma2 * cumtrapz(r, r.*rho.*u.*up);
+  pp    = - Ma2 * rho.*u.*up;
+  p     = p1 + 2*pi * cumtrapz(r, r.*pp);
 
   if 0 == nargout
     figure();
