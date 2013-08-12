df8b7b5d55b4931472b98c254875fbc3c7ea71df
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-12 20:29:55 +0000

    suzerain: break out nozzle.m
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40698 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/FPG_BaseFlow_Problem.m
+++ writeups/notebooks/FPG_BaseFlow_Problem.m
@@ -1,33 +1,6 @@
 % Script file loading necessary packages for use within Octave
 1; if exist('OCTAVE_VERSION') ~= 0; pkg load odepkg; end
 
-% Solve nozzle initial value problem for u(r) on [R1, R2] given u1(R1)
-% via an ODE integrator-ready implementation of velocity derivative u'(r, u).
-% Afterward, compute sound speed squared, density, and pressure from r, u.
-function [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1)
-
-  Ma2   = Ma*Ma;
-  up    = @(r,u) -(u./r) .* (2 + Ma2*(gam0 - 1) - Ma2*(gam0 - 1)*u.**2) ...
-                         ./ (2 + Ma2*(gam0 - 1) - Ma2*(gam0 + 1)*u.**2);
-  tol   = sqrt(eps);
-  vopt  = odeset('RelTol',      tol, 'AbsTol',  tol, ...
-                 'InitialStep', tol, 'MaxStep', sqrt(sqrt(tol)));
-  [r,u] = ode45(up, [R1 R2], u1, vopt);
-  up    = up(r, u); % Shadow
-  a2    = 1 + 0.5*Ma2*(gam0 - 1)*(1 - u.**2);
-  rho   = rho1 * exp(-2*pi*Ma2 * cumtrapz(r, r.*u.*up ./ a2));
-  pp    = - Ma2 * rho.*u.*up;
-  p     = p1 + 2*pi * cumtrapz(r, r.*pp);
-
-  if 0 == nargout
-    figure();
-    plot(r, u, 'r-', r, rho, 'b-', r, p, 'g-');
-    legend('Velocity', 'Density', 'Pressure', 'location', 'northwest');
-    xlabel('Radius');
-  end
-
-endfunction
-
 %% Define parameters for nozzle cases of interest
 Ma = 1; gam0 = 1.4; Rinner = 1; Router = 2;
 
