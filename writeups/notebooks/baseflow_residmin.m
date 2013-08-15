05f37bead7c0a7912657eade46698783ba6734f6
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-15 22:28:39 +0000

    suzerain: remove inequc process
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40806 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/baseflow_residmin.m
+++ writeups/notebooks/baseflow_residmin.m
@@ -1,7 +1,6 @@
 % Driver aspiring to target Ma_e, p_exi, and T_e quantities at (R0, dstar).
-function s = baseflow_residmin(dstar, gam0, Ma_e, p_exi, T_e,              ...
-                               tol = sqrt(eps), maxiter = 100, inequc = 0, ...
-                               debug = 0)
+function s = baseflow_residmin(dstar, gam0, Ma_e, p_exi, T_e,             ...
+                               tol = sqrt(eps), maxiter = 100, debug = 0)
   if exist('OCTAVE_VERSION') ~= 0; pkg load odepkg optim; end
 
   % Relative residuals of observations against targets for [Ma, R0, rho1, u1]
@@ -16,11 +15,6 @@ function s = baseflow_residmin(dstar, gam0, Ma_e, p_exi, T_e,              ...
                  'inequc', { zeros(4), ones(4,1), ... % NOP linear constraint
                              @(x) baseflow_h(Ma_e, x(1), x(2), x(3), x(4))  },
                  'TolFun', tol, 'MaxIter', maxiter, 'debug', debug);
-  if inequc
-    opt = optimset(opt, 'inequc',
-                   { zeros(4), ones(4,1), ... % NOP linear constraint required
-                   @(x) baseflow_h(Ma_e, x(1), x(2), x(3), x(4))  });
-  end
 
   % Solve the problem and convert relative residuals into optimization results
   [p, res, cvg, outp] = nonlin_residmin(f, pin, opt);
@@ -31,25 +25,15 @@ function s = baseflow_residmin(dstar, gam0, Ma_e, p_exi, T_e,              ...
              'Ma', p(1), 'R0', p(2), 'rho1', p(3), 'u1', p(4),
              'req', struct('Ma_e', Ma_e,   'p_exi', p_exi,  'T_e', T_e   ),
              'obs', struct('Ma_e', res(1), 'p_exi', res(2), 'T_e', res(3)),
-             'cvg', cvg, 'inequc', inequc, 'tol', tol,
-             'maxiter', maxiter, 'niter', outp.niter);
+             'cvg', cvg, 'tol', tol, 'maxiter', maxiter, 'niter', outp.niter);
 
   % Curry so that s.nozzle(Ly) computes results on segment (R0,0) to (R0,Ly)
   s.nozzle=@(Ly) nozzle(s.Ma,s.gam0,s.R0,sqrt(s.R0**2+Ly**2),s.u1,s.rho1,0);
 
 end
 
-% Compute vector of nozzle_edge results containing [tMa_e; tp_exi; tT_e].
+% Repackage nozzle_edge multiple return values into a column vector.
 function f = baseflow_f(dstar, gam0, Ma, R0, rho1, u1)
   [Ma_e, p_exi, T_e] = nozzle_edge(dstar, gam0, Ma, R0, rho1, u1);
   f = [Ma_e; p_exi; T_e];
-end
-
-% Specify h(x) >= 0 constraints using eps to accomplish h(x) > 0.
-function h = baseflow_h(Ma_e, Ma, R0, rho1, u1)
-  if Ma_e < 1
-    h = [1/Ma-u1-eps; -u1-eps]; % -1/Ma < u1 < 0 achieves at most sonic outflow
-  else
-    h = [u1-1/Ma-eps];          % u1 > 1/Ma achieves at least sonic inflow
-  end
 end
