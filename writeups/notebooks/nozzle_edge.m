e32fa08a863ce808f7fe9274acca20ecb1d8b847
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-15 17:19:54 +0000

    suzerain: better call out role of p1
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40783 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_edge.m
+++ writeups/notebooks/nozzle_edge.m
@@ -1,12 +1,12 @@
 % Solve a minimal nozzle problem computing Ma_e, p_exi, T_e at (R0, dstar).
 function [Ma_e, p_exi, T_e] = nozzle_edge(dstar, gam0, Ma, R0, rho1, u1)
 
-  R1 = max(eps, R0);           % Defensive as BFGS algorithm can try R0 == 0
-  R2 = sqrt(R0**2 + dstar**2); % Minimal domain with edge as final result
-  Ma_e = p_exi = T_e = NaN;    % Produce NaNs if nozzle(...) ODE solver fails
+  R1   = max(eps, R0);           % Defensive as BFGS algorithm can try R0 == 0
+  R2   = sqrt(R0**2 + dstar**2); % Minimal domain with edge as final result
+  Ma_e = p_exi = T_e = NaN;      % Produce NaNs if nozzle(...) ODE solver fails
+  p1   = NaN;                    % Absolute pressure is irrelevant to results
   try
-    % Absolute pressure is irrelevant for these quantities, hence p1 = 0
-    [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, 0);
+    [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1);
     Ma_e  =   Ma * r(end) * abs(u(end)) / (R2 * sqrt(a2(end)));
     p_exi = - R2 * abs(pp(end)) / (R0*Ma*Ma);
     T_e   =   a2(end);
