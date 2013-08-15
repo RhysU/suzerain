43a9a9e78b762c520a66c559b63658afe4b3d343
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-15 17:15:49 +0000

    suzerain: use nozzle_edge in baseflow_sqp
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40782 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/baseflow_sqp.m
+++ writeups/notebooks/baseflow_sqp.m
@@ -28,34 +28,27 @@ function s = baseflow_sqp(dstar, gam0, Ma_e, p_exi, T_e, maxiter, tol)
         = sqp(x, s.phi, [], s.h, lb, ub, maxiter, tol);
   s.Ma = s.x(1); s.R0 = s.x(2); s.rho1 = s.x(3); s.u1 = s.x(4);
 
-  % Curry so that s.nozzle(Ly) provides data on (R0,0) to (R0,Ly)
-  s.nozzle=@(Ly) nozzle(s.Ma,s.gam0,s.R0,sqrt(s.R0**2+Ly**2),s.u1,s.rho1,1);
+  % Compute the observed edge quantities at (R0,dstar)
+  s.obs = struct(); [s.obs.Ma_e, s.obs.p_exi, s.obs.T_e] ...
+        = nozzle_edge(s.dstar, s.gam0, s.Ma, s.R0, s.rho1, s.u1);
 
-  % Finally, compute the observed edge quantities at dstar
-  s.obs = struct(); [s.obs.phi,s.obs.Ma_e,s.obs.p_exi,s.obs.T_e] = s.phi(s.x);
+  % Finally, curry so that s.nozzle(Ly) provides data on (R0,0) to (R0,Ly)
+  s.nozzle=@(Ly) nozzle(s.Ma,s.gam0,s.R0,sqrt(s.R0**2+Ly**2),s.u1,s.rho1,1);
 end
 
-% Specify h(x) >= 0 constraints using eps to accomplish h(x) > 0 where needed.
-% For subsonic, -1/Ma < u1 < 0 achieves at most nearly sonic outflow.
-% For supersonic, u1 > 1/Ma achieves at least nearly sonic inflow.
+% Specify h(x) >= 0 constraints using eps to accomplish h(x) > 0.
 function h = baseflow_h(dstar, gam0, Ma_e, p_exi, T_e, x)
   Ma = x(1); R0 = x(2); rho1 = x(3); u1 = x(4);
-  h = merge(Ma_e < 1, [1/Ma-u1-eps; -u1-eps], [u1-1/Ma-eps]);
+  if Ma_e < 1
+    h = [1/Ma-u1-eps; -u1-eps]; % -1/Ma < u1 < 0 achieves at most sonic outflow
+  else
+    h = [u1-1/Ma-eps];          % u1 > 1/Ma achieves at least sonic inflow
+  end
 end
 
 % Compute phi(x) returning squared norm of mismatch vs tMa_e, tp_exi, tT_e.
-% Defends against R0 == eps as BFGS seemingly ignores lb <= x <= ub.
-function [phi, Ma_e, p_exi, T_e] = baseflow_phi(dstar, gam0, tMa_e, ...
-                                                tp_exi, tT_e, x)
+function phi = baseflow_phi(dstar, gam0, tMa_e, tp_exi, tT_e, x)
   Ma = x(1); R0 = x(2); rho1 = x(3); u1 = x(4);
-  R1 = max(eps, R0); R2 = sqrt(R0**2 + dstar**2);
-  try
-    [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, 1);
-    Ma_e  =   Ma*r(end)*abs(u(end)) / (R2*sqrt(a2(end)));
-    p_exi = - R2*abs(pp(end)) / (R0*Ma*Ma);
-    T_e   =   a2(end);
-    phi = (tMa_e - Ma_e)**2 + (tp_exi - p_exi)**2  + (tT_e - T_e)**2
-  catch
-    phi = realmax; Ma_e = p_exi = T_e = NaN;
-  end
+  [Ma_e, p_exi, T_e] = nozzle_edge(dstar, gam0, Ma, R0, rho1, u1);
+  phi = (tMa_e - Ma_e)**2 + (tp_exi - p_exi)**2  + (tT_e - T_e)**2
 end
