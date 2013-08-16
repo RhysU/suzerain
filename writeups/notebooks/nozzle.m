e4f902564ec1cd8e43ba81c6eafd2ddc42c53b36
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-16 14:40:43 +0000

    suzerain: expose ODE tolerances
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40816 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle.m
+++ writeups/notebooks/nozzle.m
@@ -1,10 +1,10 @@
 % Solve nozzle IVP for [u; rho; p] on [R1, R2] given [u1; rho1; p1]
 % via a coupled ODE-based approach.  Plots results when no values requested.
-function [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, ...
-                                             u1, rho1, p1, tol=sqrt(eps))
+function [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1, ...
+                                             reltol=sqrt(eps), abstol=sqrt(eps))
 
-  vopt     = odeset('RelTol',      tol, 'AbsTol',  tol, ...
-                    'InitialStep', tol, 'MaxStep', sqrt(sqrt(tol)));
+  vopt     = odeset('RelTol',      reltol,       'AbsTol',  abstol,
+                    'InitialStep', sqrt(abstol), 'MaxStep', sqrt(sqrt(abstol)));
   [r, x]   = ode45(@nozzle_f, [R1 R2], [u1 log(rho1) p1], vopt, Ma**2, gam0-1);
   u        =     x(:,1) ;
   rho      = exp(x(:,2));
@@ -21,13 +21,6 @@ function [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, ...
 
 end
 
-% Compute u' and a2 given r, u, Ma2=Ma**2, gam0m1=gam0-1
-function [up, a2] = nozzle_upa2(r, u, Ma2, gam0m1)
-  up = -(u./r) .* (2 + Ma2*gam0m1 - Ma2*(gam0m1  )*u.**2) ...
-               ./ (2 + Ma2*gam0m1 - Ma2*(gam0m1+2)*u.**2);
-  a2 = 1 + 0.5*Ma2*gam0m1*(1 - u.**2);
-end
-
 % Find [u; log rho; p]' given r, x=[u; log rho; p], Ma2=Ma**2, gam0m1=gam0-1
 function f = nozzle_f(r, x, Ma2, gam0m1)
   u = x(1); rho = exp(x(2)); p = x(3);       % Unpack
@@ -35,4 +28,11 @@ function f = nozzle_f(r, x, Ma2, gam0m1)
   logrhop  = -2*pi*Ma2 * r*u*up / a2;
   pp       = -2*pi     * r*rho*u*up;
   f = [up; logrhop; pp];                     % Pack
+end
+
+% Compute u' and a2 given r, u, Ma2=Ma**2, gam0m1=gam0-1
+function [up, a2] = nozzle_upa2(r, u, Ma2, gam0m1)
+  up = -(u./r) .* (2 + Ma2*gam0m1 - Ma2*(gam0m1  )*u.**2) ...
+               ./ (2 + Ma2*gam0m1 - Ma2*(gam0m1+2)*u.**2);
+  a2 = 1 + 0.5*Ma2*gam0m1*(1 - u.**2);
 end
--- writeups/notebooks/nozzle1.m
+++ writeups/notebooks/nozzle1.m
@@ -1,15 +1,13 @@
 % Solve nozzle initial value problem for u(r) on [R1, R2] given u1(R1)
-% via an ODE integrator-ready implementation of velocity derivative u'(r, u).
-% Afterward, compute sound speed squared, density, and pressure from r, u
-% as well as derivatives of u and p.  Plots results when no values requested.
-function [r, u, rho, p, a2, up, pp] = nozzle1(Ma, gam0, R1, R2, ...
-                                              u1, rho1, p1, tol=sqrt(eps))
+% via an ODE-based approach.  Plots results when no values requested.
+function [r, u, rho, p, a2, up, pp] = nozzle1(Ma, gam0, R1, R2, u1, rho1, p1, ...
+                                              reltol=sqrt(eps), abstol=sqrt(eps))
 
   Ma2   = Ma*Ma;
   up    = @(r,u) -(u./r) .* (2 + Ma2*(gam0 - 1) - Ma2*(gam0 - 1)*u.**2) ...
                          ./ (2 + Ma2*(gam0 - 1) - Ma2*(gam0 + 1)*u.**2);
-  vopt  = odeset('RelTol',      tol, 'AbsTol',  tol, ...
-                 'InitialStep', tol, 'MaxStep', sqrt(sqrt(tol)));
+  vopt  = odeset('RelTol',      reltol,       'AbsTol',  abstol,
+                 'InitialStep', sqrt(abstol), 'MaxStep', sqrt(sqrt(abstol)));
   [r,u] = ode45(up, [R1 R2], u1, vopt);                           % Full order
   up    = up(r, u);                                               % Shadow 'up'
   a2    = 1 + 0.5*Ma2*(gam0 - 1)*(1 - u.**2);                     % Full order
