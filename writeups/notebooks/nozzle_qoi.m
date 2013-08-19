9f2192a92d5e8b2e5dc53c58b4405d4bab2bfdaf
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-19 22:17:36 +0000

    suzerain: add T_w computation
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40858 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_qoi.m
+++ writeups/notebooks/nozzle_qoi.m
@@ -1,5 +1,5 @@
-% Solve a minimal nozzle problem computing Ma_e, p_exi, T_e at (R0, delta).
-function [Ma_e, p_exi, T_e] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1)
+% Solve a nozzle problem for Ma_e, p_exi, T_e at (R0, delta) and T_w at (R0, 0).
+function [Ma_e, p_exi, T_e, T_w] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1)
 
   R1   = max(eps, R0);           % Defensive as BFGS algorithm may try R0 == 0
   R2   = sqrt(R0**2 + delta**2); % Minimal domain with edge as final result
@@ -10,6 +10,7 @@ function [Ma_e, p_exi, T_e] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1)
     Ma_e  = Ma * r(end) * abs(u(end)) / (R2 * sqrt(a2(end)));
     p_exi = sign(u(end)) * R2 * pp(end) / (R0 * Ma * Ma);
     T_e   = a2(end);
+    T_w   = a2(1);
   catch
     warning('nozzle_qoi(%g, %g, %g, %g, %g, %g) NaNs', ...
             delta, gam0, Ma, R0, rho1, u1);
