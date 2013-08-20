58d4e6e20605a51eb720693eb3ca33968e6834ed
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-20 22:17:57 +0000

    suzerain: update guess on success
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40894 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/cev_laminar.m
+++ writeups/notebooks/cev_laminar.m
@@ -6,10 +6,12 @@ for i = 1:rows(d)
     tic
     try
         s(i) = nozzle_baseflow(d(i,2), d(i,3), d(i,4), d(i,5), d(i,6),
-                               guess, optimset('MaxIter', 50,
-                                               'debug', 1,
+                               guess, optimset('MaxIter', 100,
+                                               'TolFun', sqrt(eps),
                                                'weights', [1;1;1]));
-        guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
+        if s(i).cvg > 0
+            guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
+        end
         s(end)
     catch
         warning('nozzle_baseflow(%g, %g, %g, %g, %g) fails', ...
