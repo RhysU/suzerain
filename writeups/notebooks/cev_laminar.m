b325e5359eb4ca20eb1870a35b10b4367399fca2
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-20 19:30:52 +0000

    suzerain: cev_laminar draft improvement
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40886 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/cev_laminar.m
+++ writeups/notebooks/cev_laminar.m
@@ -1,11 +1,17 @@
 #!/usr/bin/env octave
-d = dlmread('cev_laminar.csv', ',', 1, 0);
 guess = [];
+d = dlmread('cev_laminar.csv', ',', 1, 0);
 clear s;
-for i = 10:rows(d)
+for i = 1:rows(d)
     tic
-    s(i) = nozzle_baseflow(d(i,2), d(i,3), d(i,4), d(i,5), d(i,6),
-                           sqrt(eps), 100, 1, guess);
+    try
+        s(i) = nozzle_baseflow(d(i,2), d(i,3), d(i,4), d(i,5), d(i,6),
+                             eps, 100, 0, guess);
+        guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
+        s(end)
+    catch
+        warning('nozzle_baseflow(%g, %g, %g, %g, %g) fails', ...
+                d(i,2), d(i,3), d(i,4), d(i,5), d(i,6));
+    end
     toc
-    guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
 end
