82082d7bbc74415e10556b15515b68a6703a7e19
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-20 21:01:47 +0000

    suzerain: use optimset for control
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40890 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_baseflow.m
+++ writeups/notebooks/nozzle_baseflow.m
@@ -1,11 +1,13 @@
 % Driver aspiring to target Ma_e, p_exi, and a2_e quantities at (R0, delta).
 % Returns struct s with parameters s.Ma, s.R0, r.rho1, and s.u1 creating
 % a flow producing observed values s.obs.Ma_e, s.obs.p_exi, and s.obs.a2_e.
-% Parameter 'pin' may be used to provide an initial guess [Ma; R0; rho1; u1].
-function s = nozzle_baseflow(delta, gam0, Ma_e, p_exi, a2_e,             ...
-                             tol = sqrt(eps), maxiter = 100, debug = 0, ...
-                             pin = [])
-  if exist('OCTAVE_VERSION') ~= 0; pkg load odepkg optim; end
+%
+% Parameter 'opt' supplies options to nonlin_residmin per optimset.
+% Useful options include TolFun, MaxIter, debug, and weights.
+% Parameter 'pin' optionally provides an initial guess [Ma; R0; rho1; u1].
+function s = nozzle_baseflow(delta, gam0, Ma_e, p_exi, a2_e, ...
+                             opt = optimset('MaxIter', 50), pin = [])
+  pkg load odepkg optim;
 
   % Residuals of observations against targets for [Ma, R0, rho1, u1]
   % Relative (instead of absolute) residuals used otherwise Ma_e swamps p_exi!
@@ -14,21 +16,21 @@ function s = nozzle_baseflow(delta, gam0, Ma_e, p_exi, a2_e,             ...
 
   % Establish initial guess as well as lower and upper bounds.
   % Guess for u1 = x(end) should be consistent with sub- vs supersonic Ma_e
-  if isempty(pin); pin =   [1;       1;       1;       sign(Ma_e-1)*Ma_e+eps]; end
-  opt = optimset('lbound', [eps;     eps;     eps;     -realmax             ],
-                 'ubound', [realmax; realmax; realmax; +realmax             ],
-                 'TolFun', tol, 'MaxIter', maxiter, 'debug', debug);
+  if isempty(pin); pin =        [1;   1;   1;    sign(Ma_e-1)*Ma_e+eps]; end
+  opt = optimset(opt, 'lbound', [eps; eps; eps; -inf                  ],
+                      'ubound', [inf; inf; inf;  inf                  ]);
 
   % Solve the problem and convert relative residuals into optimization results
   [p, res, cvg, outp] = nonlin_residmin(f, pin, opt);
-  res2 = norm(res, 2); res .*= target; res += target;
+  res2 = norm(res.*optimget(opt, 'weights', ones(size(target))), 2);
+  res  = res.*target + target;
 
   % Pack problem specification, solution, and runtime behavior into a struct
   s = struct('delta', delta, 'gam0', gam0,
-             'Ma', p(1), 'R0', p(2), 'rho1', p(3), 'u1', p(4), 'res2', res2,
+             'Ma', p(1), 'R0', p(2), 'rho1', p(3), 'u1', p(4),
              'req', struct('Ma_e', Ma_e,   'p_exi', p_exi,  'a2_e', a2_e   ),
              'obs', struct('Ma_e', res(1), 'p_exi', res(2), 'a2_e', res(3)),
-             'cvg', cvg, 'tol', tol, 'maxiter', maxiter, 'niter', outp.niter);
+             'res2', res2, 'cvg', cvg, 'niter', outp.niter);
 
   % Curry so s.nozzle(Ly) computes results on segment (R0,0) to (R0,Ly)
   % and so that s.qoi(Ly) computes quantities of interest on that segment
@@ -45,10 +47,10 @@ end
 
 %!demo
 %! % Find [Ma, R0, rho1, u1] producing given supersonic conditions at (R0,1)
+%! % Afterwards, s.nozzle(2) computes baseflow for use from (R0,0) to (R0,2)
 %! tic(), s = nozzle_baseflow(1, 1.4, 1.4, -0.02, 4.2), toc()
-%! % Now s.nozzle(2) computes baseflow suitable for use from (R0,0) to (R0,2)
 
 %!demo
 %! % Find [Ma, R0, rho1, u1] producing given subsonic conditions at (R0,1)
-%! tic(), s = nozzle_baseflow(1, 1.4, 0.4, -0.02, 4.2), toc()
-%! % Now s.nozzle(2) computes baseflow suitable for use from (R0,0) to (R0,2)
+%! opt = optimset('TolFun', eps, 'MaxIter', 100, 'debug', 1);
+%! tic(), s = nozzle_baseflow(1, 1.4, 0.4, -0.02, 4.2, opt), toc()
