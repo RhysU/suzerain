7094eb0d397c75c8e9c6fdec285effd6ce074da7
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-21 20:16:41 +0000

    suzerain: eliminate R1, R2
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40915 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_qoi.m
+++ writeups/notebooks/nozzle_qoi.m
@@ -1,11 +1,10 @@
-% Solve a minimal nozzle for Ma_e, p_exi, a2_e at (R0,delta) and a2_w at (R0,0).
+% Solve minimal nozzle for Ma_e, p_exi, a2_e at (R0,delta) and a2_w at (R0,0).
 function [Ma_e, p_exi, a2_e, a2_w] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1, p1)
-
-  [R1, R2] = deal(R0, realsqrt(R0**2 + delta**2));
   try
-    [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1);
-    Ma_e  = Ma * R0 * abs(u(end)) / (R2 * realsqrt(a2(end)));
-    p_exi = sign(u(end)) * R2 * pp(end) / (Ma**2 * R0);
+    [r, u, rho, p, a2, up, pp] ...
+          = nozzle(Ma, gam0, R0, realsqrt(R0**2 + delta**2), u1, rho1, p1);
+    Ma_e  = Ma * r(1) * abs(u(end)) / (r(end) * realsqrt(a2(end)));
+    p_exi = sign(u(end)) * r(end) * pp(end) / (Ma**2 * r(1));
     a2_e  = a2(end);
     a2_w  = a2(1);
   catch
@@ -13,5 +12,4 @@ function [Ma_e, p_exi, a2_e, a2_w] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1, p
             delta, gam0, Ma, R0, rho1, u1);
     Ma_e = p_exi = a2_e = a2_w = NaN;
   end
-
 end
