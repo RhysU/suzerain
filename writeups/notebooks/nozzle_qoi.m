1dee00c1225b24f649a9595aecd2565ad42bf303
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-21 19:24:23 +0000

    suzerain: use R2 not r(end)
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40911 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_qoi.m
+++ writeups/notebooks/nozzle_qoi.m
@@ -6,7 +6,7 @@ function [Ma_e, p_exi, a2_e, a2_w] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1, p
   Ma_e = p_exi = a2_e = a2_w = NaN;  % Produce NaNs if nozzle(...) fails
   try
     [r, u, rho, p, a2, up, pp] = nozzle(Ma, gam0, R1, R2, u1, rho1, p1);
-    Ma_e  = Ma * r(end) * abs(u(end)) / (R2 * sqrt(a2(end)));
+    Ma_e  = Ma * R2 * abs(u(end)) / (R2 * sqrt(a2(end)));
     p_exi = sign(u(end)) * R2 * pp(end) / (R0 * Ma * Ma);
     a2_e  = a2(end);
     a2_w  = a2(1);
