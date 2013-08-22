798dee62f6e0f6900951fae439f9b5d9d80ab883
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-22 22:01:47 +0000

    suzerain: add realizability comments
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40928 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/cev_laminar.m
+++ writeups/notebooks/cev_laminar.m
@@ -5,15 +5,16 @@ clear s;
 for i = 1:rows(d)
     tic
     try
-        s(i) = nozzle_baseflow(d(i,2), d(i,3), d(i,4), d(i,5), d(i,6),
+        s(i) = nozzle_baseflow(1.0, d(i,3), d(i,4), d(i,5), d(i,6),
                                guess, optimset('MaxIter', 100,
                                                'TolFun', sqrt(eps),
                                                'weights', [1;1;1]));
-        if s(i).cvg > 0
-            guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
-        end
+%       if s(i).cvg > 0
+%           guess = [s(i).Ma; s(i).R0; s(i).rho1; s(i).u1];
+%       end
         s(end)
     catch
+        s(i).cvg = 0
         warning('nozzle_baseflow(%g, %g, %g, %g, %g) fails', ...
                 d(i,2), d(i,3), d(i,4), d(i,5), d(i,6));
     end
