bb25095779c71e542aa200e2b753cdd746094a45
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-26 20:25:09 +0000

    suzerain: avoid R0 = 0 for BFGS
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@40971 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle_qoi.m
+++ writeups/notebooks/nozzle_qoi.m
@@ -2,7 +2,7 @@
 function [Ma_e, p_exi, a2_e, a2_w] = nozzle_qoi(delta, gam0, Ma, R0, rho1, u1, p1)
   try
     [r, u, rho, p, a2, up, pp] ...
-          = nozzle(Ma, gam0, R0, realsqrt(R0**2 + delta**2), u1, rho1, p1);
+          = nozzle(Ma, gam0, max(R0,eps), realsqrt(R0**2+delta**2), u1, rho1, p1);
     Ma_e  = Ma*r(1)*abs(u(end)) / (r(end)*realsqrt(a2(end)));
     p_exi = sign(u(end))*r(end)*delta*pp(end) / (Ma**2*r(1)*rho(end)*u(end)**2);
     a2_e  = a2(end);
