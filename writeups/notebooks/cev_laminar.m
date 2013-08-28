db752dbf39e6e2329527785182886af37582d46b
Rhys Ulerich <rhys@ices.utexas.edu>
2013-08-28 14:56:13 +0000

    suzerain: solver doing a respectable job on cev_laminar data
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41032 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/cev_laminar.m
+++ writeups/notebooks/cev_laminar.m
@@ -2,7 +2,7 @@
 
 % Load a collection of problems from disk using Octave-Forge dataframe
 pkg load dataframe
-d = dataframe('cev_laminar.csv');
+d = dataframe('cev_laminar.in');
 
 % Process problems in parallel solving with delta fixed to be one
 pkg load general
@@ -15,3 +15,23 @@ toc()
 
 % Convert the returned results into a dataframe
 s = dataframe(struct2cell(s).', 'colnames', fieldnames(s));
+
+% Save input with appended results into a new CSV-with-header file
+f = fopen('cev_laminar.out','w');
+dcols = columns(d);
+for j = 1:dcols
+  fprintf(f, '%s,', d.colnames(j))
+end
+scols = columns(s);
+for j = 1:scols
+  fprintf(f, '%s%s', s.colnames(j), merge(j != scols, ',', "\n"))
+end
+for i = 1:rows(d)
+  t      = strrep(mat2str(d.array(i,:)), ' ', ',');
+  t(end) = ',';
+  fputs(f, t(2:end));
+  t = strrep(mat2str(s.array(i,:)), ' ', ',');
+  t(end) = "\n";
+  fputs(f, t(2:end));
+end
+fclose(f);
