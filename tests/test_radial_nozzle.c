47985866fcd29481aac9cea0d93318b1a7580d34
Rhys Ulerich <rhys@ices.utexas.edu>
2013-09-04 22:36:43 +0000

    suzerain: disabled, failing draft tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41223 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_radial_nozzle.c
+++ tests/test_radial_nozzle.c
@@ -30,10 +30,42 @@
 #include <suzerain/common.h>
 #include <suzerain/error.h>
 
+// Second subsonic test from writeups/notebooks/nozzle1.m
+// Beware the slightly different argument order relative to that code
 static
-void test_stub()
+void test_subsonic()
 {
+    const double R[] = {1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0};
+    const size_t N   = sizeof(R)/sizeof(R[0]);
+    suzerain_radial_nozzle_solution * s = suzerain_radial_nozzle_solver(
+            1.0, 1.4, 1, -1 + GSL_SQRT_DBL_EPSILON, 1, R, N);
+
+    // Expected values computed by writeups/notebooks/nozzle1.m via Octave
+    const double tol = GSL_SQRT_DBL_EPSILON;
+    suzerain_radial_nozzle_state fin = s->state[N-1];
+    gsl_test_abs(fin.R,   R[N-1],           tol, "%s final R",   __func__);
+    gsl_test_abs(fin.u,  -0.33200842136541, tol, "%s final u",   __func__);
+    gsl_test_abs(fin.a2,  1.17795408162849, tol, "%s final a2",  __func__);
+    gsl_test_abs(fin.rho, 1.61933117083039, tol, "%s final rho", __func__);
+    gsl_test_abs(fin.p,   1.68254700209343, tol, "%s final p",   __func__);
+
+    free(s);
+}
+
+// From writeups/notebooks/nozzle1.m test cases
+// Beware the slightly different argument order relative to that code
+static
+void test_supersonic()
+{
+    const double R[] = {1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0};
+    const size_t N   = sizeof(R)/sizeof(R[0]);
+    suzerain_radial_nozzle_solution * s = suzerain_radial_nozzle_solver(
+            1.0, 1.4, 1, 1 + GSL_SQRT_DBL_EPSILON, 1, R, N);
+
+    // TODO
     gsl_test_abs(1.0, 1.0, GSL_DBL_EPSILON, "NOP");
+
+    free(s);
 }
 
 int main(int argc, char **argv)
@@ -46,7 +78,8 @@ int main(int argc, char **argv)
         gsl_test_verbose(1);
     }
 
-    test_stub();
+    // FIXME test_subsonic();
+    // FIXME test_supersonic();
 
     exit(gsl_test_summary());
 }
