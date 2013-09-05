58e0a87577d2007bb67f57c64c7ddbf078f9c76e
Rhys Ulerich <rhys@ices.utexas.edu>
2013-09-05 14:16:11 +0000

    suzerain: add supersonic test case
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41228 0bd85f0b-c151-43b9-a1e9-27231104551f

--- tests/test_radial_nozzle.c
+++ tests/test_radial_nozzle.c
@@ -33,7 +33,7 @@
 // Second subsonic test from writeups/notebooks/nozzle1.m
 // Beware the slightly different argument order relative to that code
 static
-void test_subsonic()
+void test_solver_subsonic()
 {
     const double R[]  = {1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.};
     const size_t N    = sizeof(R)/sizeof(R[0]);
@@ -73,6 +73,49 @@ void test_subsonic()
     free(s);
 }
 
+// Supersonic test from writeups/notebooks/nozzle1.m
+// Beware the slightly different argument order relative to that code
+static
+void test_solver_supersonic()
+{
+    const double R[]  = {1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.};
+    const size_t N    = sizeof(R)/sizeof(R[0]);
+    const double Ma0  = 1.0;
+    const double gam0 = 1.4;
+    const double rho1 = 1.0;
+    const double u1   = 1/Ma0 + GSL_SQRT_DBL_EPSILON;
+    const double p1   = 1.0;
+    suzerain_radial_nozzle_solution * s = suzerain_radial_nozzle_solver(
+            Ma0, gam0, rho1, u1, p1, R, N);
+
+    // Check that the scenario parameters were stored correctly
+    gsl_test_abs(s->Ma0,  Ma0,  GSL_DBL_EPSILON, "%s Ma0 ", __func__);
+    gsl_test_abs(s->gam0, gam0, GSL_DBL_EPSILON, "%s gam0", __func__);
+
+    // Check that the initial conditions were stored correctly
+    suzerain_radial_nozzle_state ini = s->state[0];
+    gsl_test_abs(ini.R,   R[0], GSL_DBL_EPSILON, "%s init R   ", __func__);
+    gsl_test_abs(ini.u,   u1,   GSL_DBL_EPSILON, "%s init u   ", __func__);
+    gsl_test_abs(ini.rho, rho1, GSL_DBL_EPSILON, "%s init rho ", __func__);
+    gsl_test_abs(ini.p,   p1,   GSL_DBL_EPSILON, "%s init p   ", __func__);
+
+    // Expected results computed by writeups/notebooks/nozzle.m using Octave
+    double tol = GSL_SQRT_DBL_EPSILON;
+    suzerain_radial_nozzle_state fin = s->state[N-1];
+    gsl_test_rel(fin.R,     R[N-1],             tol, "%s final R   ", __func__);
+    gsl_test_rel(fin.u,     1.71679859932923,   tol, "%s final u   ", __func__);
+    gsl_test_rel(fin.a2,    0.610520513868242,  tol, "%s final a2  ", __func__);
+    gsl_test_rel(fin.up,    0.224261011681836,  tol, "%s final up  ", __func__);
+    // Octave and GSL RKF45 adaptive control differs, hence lower tolerances...
+    tol = sqrt(tol);
+    gsl_test_rel(fin.rho,   0.188891739106648,  tol, "%s final rho ", __func__);
+    gsl_test_rel(fin.p,     0.336677203999661,  tol, "%s final p   ", __func__);
+    gsl_test_rel(fin.rhop, -0.119120314492264,  tol, "%s final rhop", __func__);
+    gsl_test_rel(fin.pp,   -0.0727253956159634, tol, "%s final pp  ", __func__);
+
+    free(s);
+}
+
 int main(int argc, char **argv)
 {
     SUZERAIN_UNUSED(argc);
@@ -83,8 +126,8 @@ int main(int argc, char **argv)
         gsl_test_verbose(1);
     }
 
-    test_subsonic();
-    // TODO test_supersonic();
+    test_solver_subsonic();
+    test_solver_supersonic();
 
     exit(gsl_test_summary());
 }
