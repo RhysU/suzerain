be377939df23733b8e9182604dc6e3e8206037a4
Rhys Ulerich <rhys@ices.utexas.edu>
2013-09-12 03:56:30 +0000

    suzerain: #1609 compute_field_L2xz draft
    Based on L^2_{xz} inner product of two discrete fields from
    writeups/L2.tex specialized for the L^2_{xz} of a single
    field against itself.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41442 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/l2.cpp
+++ suzerain/l2.cpp
@@ -155,4 +155,125 @@ compute_field_L2xyz(
     return retval;
 }
 
+//
+// Implementation very much resembles compute_field_L2xyz.
+// Begin by understanding that first and then compare the differences.
+//
+std::vector<field_L2xz>
+compute_field_L2xz(
+        const contiguous_state<4,complex_t> &state,
+        const grid_specification& grid,
+        const pencil_grid& dgrid,
+        const bsplineop& cop)
+{
+    // Ensure state storage meets this routine's assumptions
+    // Notice state.shape()[0] may be any value
+    using boost::numeric_cast;
+    assert(numeric_cast<int>(state.shape()[1]) == dgrid.local_wave_extent.y());
+    assert(numeric_cast<int>(state.shape()[2]) == dgrid.local_wave_extent.x());
+    assert(numeric_cast<int>(state.shape()[3]) == dgrid.local_wave_extent.z());
+
+    // Ensure we were handed collocation-based operator matrices
+    SUZERAIN_ENSURE(cop.get()->method==SUZERAIN_BSPLINEOP_COLLOCATION_GREVILLE);
+
+    // Only want non-dealiased X-direction modes to contribute to L2
+    // Compute wavenumber translation logistics for X direction
+    int fxb[2], fxe[2], mxb[2], mxe[2];
+    inorder::wavenumber_translate(grid.N.x(),
+                                  grid.dN.x(),
+                                  dgrid.local_wave_start.x(),
+                                  dgrid.local_wave_end.x(),
+                                  fxb[0], fxe[0], fxb[1], fxe[1],
+                                  mxb[0], mxe[0], mxb[1], mxe[1]);
+    // X contains only positive wavenumbers => second range must be empty
+    assert(fxb[1] == fxe[1]);
+    assert(mxb[1] == mxe[1]);
+
+    // Only want non-dealiased Z-direction modes to contribute to L2
+    // Compute wavenumber translation logistics for Z direction
+    // One or both ranges may be empty
+    int fzb[2], fze[2], mzb[2], mze[2];
+    inorder::wavenumber_translate(grid.N.z(),
+                                  grid.dN.z(),
+                                  dgrid.local_wave_start.z(),
+                                  dgrid.local_wave_end.z(),
+                                  fzb[0], fze[0], fzb[1], fze[1],
+                                  mzb[0], mze[0], mzb[1], mze[1]);
+
+    // Temporary complex-valued storage for matrix-vector products
+    VectorXc tmp;
+    tmp.setZero(grid.N.y());
+
+    // Contiguous temporary storage for accumulating and broadcasting results
+    // Fast columns store wall-normal swaths of data.
+    // Real-valued since cwiseAbs2 always produces real-valued results.
+    MatrixXXr buf(grid.N.y(), 2*state.shape()[0]);
+    Map<MatrixXXr> total2(buf.col(0).data(),
+                          grid.N.y(), state.shape()[0]);
+    Map<MatrixXXr> mean2 (buf.col(state.shape()[0]).data(),
+                          grid.N.y(), state.shape()[0]);
+
+    // Compute the local L2 contribution towards each L^2_xz norm squared
+    // Computation uses partial sums at each loop to reduce swamping which is
+    // more-or-less recursive summation using large partitioning factors.
+    VectorXr jsum, nsum, msum;
+    total2.setZero();
+    for (size_t k = 0; k < state.shape()[0]; ++k) {
+        jsum.setZero(grid.N.y());
+        for (int j = 0; j < 2; ++j) {
+            nsum.setZero(grid.N.y());
+            for (int n = mzb[j]; n < mze[j]; ++n) {
+                msum.setZero(grid.N.y());
+                for (int m = mxb[0]; m < mxe[0]; ++m) {
+                    const complex_t * u_mn
+                        = &state[k][0][m - dgrid.local_wave_start.x()]
+                                      [n - dgrid.local_wave_start.z()];
+                    if (m > 0 && m < grid.dN.x()/2) {
+                        // Conjugate symmetry uses root-2 so Abs2 below gives 2
+                        cop.accumulate(0, M_SQRT2, u_mn, 1, 0., tmp.data(), 1);
+                    } else {
+                        cop.accumulate(0, 1.0,     u_mn, 1, 0., tmp.data(), 1);
+                    }
+                    msum += tmp.cwiseAbs2();
+                }
+                nsum += msum;
+            }
+            jsum += nsum;
+        }
+        total2.col(k) += jsum;
+    }
+    total2 *= grid.L.x() * grid.L.z();
+
+    // Reduce total2 sum onto processor housing the zero-zero mode using
+    // mean2 as a scratch buffer to simulate MPI_IN_PLACE
+    SUZERAIN_MPICHKR(MPI_Reduce(total2.data(), mean2.data(),
+            total2.size(), mpi::datatype<real_t>(), // NB real_t
+            MPI_SUM, dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+    total2 = mean2;
+
+    // Compute the mean-only L^2 squared for each field using zero-zero modes
+    if (dgrid.has_zero_zero_modes()) {
+        for (size_t k = 0; k < state.shape()[0]; ++k) {
+            const complex_t * u_mn = &state[k][0][0][0];
+            cop.accumulate(0, 1.0, u_mn, 1, 0.0, tmp.data(), 1);
+            mean2.col(k) = tmp.cwiseAbs2();
+        }
+        mean2 *= grid.L.x() * grid.L.z();
+    }
+
+    // Broadcast total2 and mean2 values to all processors
+    SUZERAIN_MPICHKR(MPI_Bcast(buf.data(),
+            buf.size(), mpi::datatype<real_t>(), // NB real_t
+            dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+
+    // Obtain fluctuating2 = total2 - mean2 and pack the return structure
+    std::vector<field_L2xz> retval(state.shape()[0]);
+    for (size_t k = 0; k < state.shape()[0]; ++k) {
+        retval[k].mean2        = mean2.col(k);
+        retval[k].fluctuating2 = total2.col(k) - mean2.col(k);
+    }
+
+    return retval;
+}
+
 } // end namespace channel
