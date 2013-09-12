fcf5b9b72a53b2899217335d056cdd47662d15e4
Rhys Ulerich <rhys@ices.utexas.edu>
2013-09-12 22:05:04 +0000

    suzerain: add support/esio_fwd.hpp
    I want to decouple some of the headers from ESIO.
    Adding these forward declarations to that end.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41469 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/support/esio_fwd.hpp
@@ -0,0 +1,36 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+// Copyright (C) 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_SUPPORT_ESIO_FWD_HPP
+#define SUZERAIN_SUPPORT_ESIO_FWD_HPP
+
+/** @file
+ * Provides forward declarations for ESIO usage.
+ */
+
+#ifndef SUZERAIN_PARSED_BY_DOXYGEN
+struct esio_handle_s;
+typedef struct esio_handle_s* esio_handle;
+#endif
+
+#endif // SUZERAIN_SUPPORT_ESIO_FWD_HPP
