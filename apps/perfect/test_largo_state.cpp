ff7437cf67f7104550ab039c236f4b1e76ef311e
Rhys Ulerich <rhys@ices.utexas.edu>
2013-09-20 19:29:44 +0000

    suzerain: #2495 add largo_state tests
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41651 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/test_largo_state.cpp
+++ apps/perfect/test_largo_state.cpp
@@ -30,5 +30,96 @@ using suzerain::perfect::largo_state;
 
 BOOST_AUTO_TEST_CASE( size )
 {
-    BOOST_CHECK_EQUAL(sizeof(largo_state), 5*sizeof(double));
+    BOOST_REQUIRE_EQUAL(sizeof(largo_state), 5*sizeof(double));
+}
+
+BOOST_AUTO_TEST_CASE( default_construct_produces_trivial_state )
+{
+    largo_state s;
+    BOOST_CHECK_EQUAL(s.rho, 0);
+    BOOST_CHECK_EQUAL(s.mx,  0);
+    BOOST_CHECK_EQUAL(s.mx,  0);
+    BOOST_CHECK_EQUAL(s.mx,  0);
+    BOOST_CHECK_EQUAL(s.e,   0);
+    BOOST_CHECK(s.trivial());
+}
+
+// Used for as_is and rescale_one test cases, just below
+static void some_library_call_for_as_is(double *d)
+{
+    BOOST_CHECK_EQUAL(d[0], 0); d[0] += 1;
+    BOOST_CHECK_EQUAL(d[1], 1); d[1] += 1;
+    BOOST_CHECK_EQUAL(d[2], 2); d[2] += 1;
+    BOOST_CHECK_EQUAL(d[3], 3); d[3] += 1;
+    BOOST_CHECK_EQUAL(d[4], 4); d[4] += 1;
+}
+
+BOOST_AUTO_TEST_CASE( as_is )
+{
+    largo_state s(4, 1, 2, 3, 0);
+    BOOST_CHECK(!s.trivial());
+
+    BOOST_REQUIRE_EQUAL(s.rho, 0);
+    BOOST_REQUIRE_EQUAL(s.mx,  1);
+    BOOST_REQUIRE_EQUAL(s.my,  2);
+    BOOST_REQUIRE_EQUAL(s.mz,  3);
+    BOOST_REQUIRE_EQUAL(s.e,   4);
+
+    some_library_call_for_as_is(s.as_is());
+
+    BOOST_CHECK_EQUAL(s.rho, 1);
+    BOOST_CHECK_EQUAL(s.mx,  2);
+    BOOST_CHECK_EQUAL(s.my,  3);
+    BOOST_CHECK_EQUAL(s.mz,  4);
+    BOOST_CHECK_EQUAL(s.e,   5);
+}
+
+BOOST_AUTO_TEST_CASE( rescale_one )
+{
+    largo_state s(4, 1, 2, 3, 0);
+    BOOST_CHECK(!s.trivial());
+
+    BOOST_REQUIRE_EQUAL(s.rho, 0);
+    BOOST_REQUIRE_EQUAL(s.mx,  1);
+    BOOST_REQUIRE_EQUAL(s.my,  2);
+    BOOST_REQUIRE_EQUAL(s.mz,  3);
+    BOOST_REQUIRE_EQUAL(s.e,   4);
+
+    some_library_call_for_as_is(s.rescale(1));
+
+    BOOST_CHECK_EQUAL(s.rho, 1);
+    BOOST_CHECK_EQUAL(s.mx,  2);
+    BOOST_CHECK_EQUAL(s.my,  3);
+    BOOST_CHECK_EQUAL(s.mz,  4);
+    BOOST_CHECK_EQUAL(s.e,   5);
+}
+
+// Used for rescale_two test case, just below
+static void some_library_call_for_rescale_two(double *d)
+{
+    BOOST_CHECK_EQUAL(d[0], 0); d[0] += 1;
+    BOOST_CHECK_EQUAL(d[1], 1); d[1] += 1;
+    BOOST_CHECK_EQUAL(d[2], 2); d[2] += 1;
+    BOOST_CHECK_EQUAL(d[3], 3); d[3] += 1;
+    BOOST_CHECK_EQUAL(d[4], 8); d[4] += 1; // Note factor of two
+}
+
+BOOST_AUTO_TEST_CASE( rescale_two )
+{
+    largo_state s(4, 1, 2, 3, 0);
+    BOOST_CHECK(!s.trivial());
+
+    BOOST_REQUIRE_EQUAL(s.rho, 0);
+    BOOST_REQUIRE_EQUAL(s.mx,  1);
+    BOOST_REQUIRE_EQUAL(s.my,  2);
+    BOOST_REQUIRE_EQUAL(s.mz,  3);
+    BOOST_REQUIRE_EQUAL(s.e,   4);
+
+    some_library_call_for_rescale_two(s.rescale(2.0));
+
+    BOOST_CHECK_EQUAL(s.rho, 1  );
+    BOOST_CHECK_EQUAL(s.mx,  2  );
+    BOOST_CHECK_EQUAL(s.my,  3  );
+    BOOST_CHECK_EQUAL(s.mz,  4  );
+    BOOST_CHECK_EQUAL(s.e,   4.5);         // Note factor of two
 }
