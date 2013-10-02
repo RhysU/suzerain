b9d9f76355d29b53aa3a074e8fd8b7dbce53d6b0
Rhys Ulerich <rhys@ices.utexas.edu>
2013-10-02 16:13:16 +0000

    suzerain: #2963 stub bl.hpp/bl.cpp
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@41906 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/bl.hpp
@@ -0,0 +1,44 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+// Copyright (C) 2013 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_BL_HPP
+#define SUZERAIN_BL_HPP
+
+/** @file
+ * Compute boundary layer quantities of interest.
+ */
+
+#include <suzerain/common.hpp>
+#include <suzerain/bspline.hpp>
+
+namespace suzerain {
+
+namespace bl {
+
+// TODO Implement
+
+} // end namespace bl
+
+} // end namespace suzerain
+
+#endif // SUZERAIN_BL_HPP
