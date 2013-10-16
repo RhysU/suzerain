08b7bb0606347a0c7def0bd432ba36327bd15655
Victor Topalian <topalian@ices.utexas.edu>
2013-10-16 22:50:50 +0000

    largo: #2975 regression test for C generic interface
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42235 0bd85f0b-c151-43b9-a1e9-27231104551f

--- largo/tests/generic_bl_temporal_consistent_baseflow.c
+++ largo/tests/generic_bl_temporal_consistent_baseflow.c
@@ -95,7 +95,7 @@ FCT_BGN()
           };
 
           double dmean [] = \
-          {      1.0/ 10.0,  \
+          {      1.0/  5.0,  \
                 45.0      ,  \
                  1.0/100.0,  \
                  5.0/ 10.0,  \
@@ -104,44 +104,93 @@ FCT_BGN()
                  1.0/100.0
           };
 
-          double rms   [] = \
-          {   4.0/ 10000.0,  \
-             25.0/   100.0,  \
-             15.0/   100.0,  \
-              1.0/    10.0,  \
-            300.0         ,  \
-              0.0/100000.0,  \
-              0.0/100000.0
-          };
-
-          double drms  [] = \
-          {    42.0/ 1000.0, \
-               24.0/   10.0, \
-              153.0/  100.0, \
-               12.0/   10.0, \
-             3200.0        , \
-                0.0/10000.0, \
-                0.0/10000.0
-          };
-
           double mean_rqq [] = \
-          {            21.0/  2000.0, \
-                     8505.0/     4.0, \
-                       21.0/200000.0, \
-                       21.0/    80.0, \
-             178231200000.0         , \
-                       21.0/ 50000.0, \
-                       21.0/200000.0
+          {           21.0/  2000.0,  \
+                    8505.0/     4.0,  \
+                      21.0/200000.0,  \
+                      21.0/    80.0,  \
+            178231200000.0         ,  \
+                      21.0/ 50000.0,  \
+                      21.0/200000.0
           };
 
-          double dmean_rqq  [] = \
-          {             11.0/   100.0, \
-                     22275.0         , \
-                        11.0/ 10000.0, \
-                        11.0/     4.0, \
-             1867184000000.0         , \
-                         1.0/ 50000.0, \
-                         1.0/200000.0
+          double dmean_rqq [] = \
+          {            11.0/    50.0, \
+                     2025.0         , \
+                        1.0/ 10000.0, \
+                        1.0/     4.0, \
+             169744000000.0         , \
+                        1.0/  2500.0, \
+                        1.0/ 10000.0
+          };
+
+          double rms [] = \
+          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+          double drms [] = \
+          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+          double grDA [] = \
+          { 2.0/  100.0, \
+            1.0/   10.0, \
+            1.0/  100.0, \
+            3.0/  100.0, \
+            4.0/  100.0, \
+            1.0/ 1000.0, \
+            2.0/ 1000.0
+          };
+
+          double grDArms  [] = \
+          { 1.0/  1000.0, \
+            5.0/  1000.0, \
+            5.0/ 10000.0, \
+            2.0/  1000.0, \
+            1.0/  1000.0, \
+            1.0/100000.0, \
+            2.0/100000.0
+          };
+
+          double base [] = \
+          {     5.0/ 1000.0, \
+               25.0/   10.0, \
+                5.0/10000.0, \
+                2.0/  100.0, \
+            20000.0        , \
+                1.0/ 1000.0, \
+                5.0/10000.0
+          };
+
+          double dybase  [] = \
+          {      5.0/ 100.0, \
+                25.0       , \
+                 5.0/1000.0, \
+                 2.0/  10.0, \
+            200000.0       , \
+                 1.0/ 100.0, \
+                 5.0/1000.0
+          };
+
+          double dtbase [] = \
+          {     2.0/ 1000.0, \
+               15.0/   10.0, \
+                2.0/10000.0, \
+                1.0/  100.0, \
+            10000.0        , \
+                5.0/10000.0, \
+                2.0/10000.0
+          };
+
+          double dxbase [] = \
+          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+          double srcbase  [] = \
+          {    1.0/ 1000.0, \
+               5.0/   10.0, \
+               1.0/10000.0, \
+               5.0/ 1000.0, \
+            5000.0        , \
+               2.0/10000.0, \
+               1.0/10000.0
           };
 
           double srcmean [] = \
@@ -151,39 +200,39 @@ FCT_BGN()
           { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
           double srcmean_good [] = \
-          {    1.0/ 2000.0,         \
-               9.0/   40.0,         \
-               1.0/20000.0,         \
-               1.0/  400.0,         \
-            2060.0        ,         \
-               1.0/10000.0,         \
-               1.0/20000.0          \
+          {     -27.0/ 20000.0,    \
+               -713.0/   400.0,    \
+                -37.0/200000.0,    \
+               -271.0/ 20000.0,    \
+             -11670.0         ,    \
+                -57.0/100000.0,    \
+                -37.0/200000.0     \
           };
 
           double srcfull_good  [] = \
-          {       11.0/     20000.0, \
-                  19.0/        80.0, \
-                3989.0/    200000.0, \
-                 109.0/      4000.0, \
-                1884.0             , \
-                  11.0/    100000.0, \
-                  11.0/    200000.0  \
+          {     -297.0/  200000.0, \
+               -7787.0/    4000.0, \
+             -543089.0/20000000.0, \
+               -4847.0/  100000.0, \
+             -307937.0/      25.0, \
+                -627.0/ 1000000.0, \
+                -407.0/ 2000000.0  \
           };
-
-          double grDA [] = \
-          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-
-          double grDArms [] = \
-          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
           const double tolerance = 1.0E-7;
 
-          const char model[] = "bl_temporal_tensor-consistent";
+          const char model[] = "bl_temporal_consistent";
 
+          // Allocate generic workspace;
           largo_allocate (&generic_workspace, model, neq, ns, 0, "dns");
 
+          // Init growth rates
+          largo_init     (generic_workspace, grDelta, grDA, grDArms);
+
           // Compute prestep values;
-          largo_prestep_seta_innery  (generic_workspace, y*grDelta,
+          largo_prestep_baseflow  (generic_workspace,   base,  dybase,
+                                             dtbase, dxbase, srcbase);
+          largo_prestep_seta_innery  (generic_workspace, y,
                                              mean,  rms,  mean_rqq,
                                             dmean, drms, dmean_rqq);
           largo_prestep_seta_innerxz (generic_workspace, field);
@@ -209,11 +258,12 @@ FCT_BGN()
 
           // Check mean part
           // Tolerance for energy source adjusted manually
-          fct_chk_eq_dbl(srcmean[1-1]       , srcmean_good[1-1]       );
-          fct_chk_eq_dbl(srcmean[2-1]       , srcmean_good[2-1]       );
-          fct_chk_eq_dbl(srcmean[3-1]       , srcmean_good[3-1]       );
-          fct_chk_eq_dbl(srcmean[4-1]       , srcmean_good[4-1]       );
-          fct_chk_eq_dbl(srcmean[5-1]/10000., srcmean_good[5-1]/10000.);
+          // printf("%.16e, %.16e", srcmean[5-1], srcmean_good[5-1]);
+          fct_chk_eq_dbl(srcmean[1-1]        , srcmean_good[1-1]        );
+          fct_chk_eq_dbl(srcmean[2-1]/10.    , srcmean_good[2-1]/10.    );
+          fct_chk_eq_dbl(srcmean[3-1]        , srcmean_good[3-1]        );
+          fct_chk_eq_dbl(srcmean[4-1]        , srcmean_good[4-1]        );
+          fct_chk_eq_dbl(srcmean[5-1]/100000., srcmean_good[5-1]/100000.);
           for (unsigned int is=0; is < ns; ++is)
           {
             fct_chk_eq_dbl(srcmean[5+is]      , srcmean_good[5+is]);
@@ -221,11 +271,11 @@ FCT_BGN()
 
           // Check full part
           // Tolerance for energy source adjusted manually
-          fct_chk_eq_dbl(srcfull[1-1]       , srcfull_good[1-1]       );
-          fct_chk_eq_dbl(srcfull[2-1]       , srcfull_good[2-1]       );
-          fct_chk_eq_dbl(srcfull[3-1]       , srcfull_good[3-1]       );
-          fct_chk_eq_dbl(srcfull[4-1]       , srcfull_good[4-1]       );
-          fct_chk_eq_dbl(srcfull[5-1]/10000., srcfull_good[5-1]/10000.);
+          fct_chk_eq_dbl(srcfull[1-1]        , srcfull_good[1-1]        );
+          fct_chk_eq_dbl(srcfull[2-1]/10.    , srcfull_good[2-1]/10.    );
+          fct_chk_eq_dbl(srcfull[3-1]        , srcfull_good[3-1]        );
+          fct_chk_eq_dbl(srcfull[4-1]        , srcfull_good[4-1]        );
+          fct_chk_eq_dbl(srcfull[5-1]/100000., srcfull_good[5-1]/100000.);
           for (unsigned int is=0; is < ns; ++is)
           {
             fct_chk_eq_dbl(srcfull[5+is]      , srcfull_good[5+is]);
@@ -240,6 +290,8 @@ FCT_BGN()
           largo_init     (generic_workspace, grDelta, grDA, grDArms);
 
           // Compute prestep values;
+          largo_prestep_baseflow  (generic_workspace,   base,  dybase,
+                                             dtbase, dxbase, srcbase);
           largo_prestep_seta (generic_workspace, y, field,
                                            mean,   rms,  mean_rqq,
                                           dmean,  drms, dmean_rqq);
@@ -249,11 +301,11 @@ FCT_BGN()
 
           // Check sources
           // Tolerance for energy source adjusted manually
-          fct_chk_eq_dbl(srcfull[1-1]       , srcfull_good[1-1]);
-          fct_chk_eq_dbl(srcfull[2-1]       , srcfull_good[2-1]);
-          fct_chk_eq_dbl(srcfull[3-1]       , srcfull_good[3-1]);
-          fct_chk_eq_dbl(srcfull[4-1]       , srcfull_good[4-1]);
-          fct_chk_eq_dbl(srcfull[5-1]/10000., srcfull_good[5-1]/10000.);
+          fct_chk_eq_dbl(srcfull[1-1]        , srcfull_good[1-1]        );
+          fct_chk_eq_dbl(srcfull[2-1]/10.    , srcfull_good[2-1]/10.    );
+          fct_chk_eq_dbl(srcfull[3-1]        , srcfull_good[3-1]        );
+          fct_chk_eq_dbl(srcfull[4-1]        , srcfull_good[4-1]        );
+          fct_chk_eq_dbl(srcfull[5-1]/100000., srcfull_good[5-1]/100000.);
           for (unsigned int is=0; is < ns; ++is)
           {
             fct_chk_eq_dbl(srcfull[5+is]      , srcfull_good[5+is]);
