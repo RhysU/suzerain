eafff218c43c244b723cc77fcbabceec466b42b2
Victor Topalian <topalian@ices.utexas.edu>
2013-10-19 05:12:40 +0000

    largo: #2976 C generic regression test
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42254 0bd85f0b-c151-43b9-a1e9-27231104551f

--- largo/tests/generic_bl_spatiotemporal_consistent_baseflow.c
+++ largo/tests/generic_bl_spatiotemporal_consistent_baseflow.c
@@ -77,13 +77,13 @@ FCT_BGN()
           double grxDelta = grtDelta / uIw;
 
           double field  [] = \
-          {    11.0/ 1000.0,  \
-              485.0/  100.0,  \
-                2.0/   10.0,  \
-                3.0/   10.0,  \
-            41500.0        ,  \
-                2.0/10000.0,  \
-                1.0/10000.0,  \
+          {    11.0/  1000.0,  \
+              485.0/   100.0,  \
+                2.0/    10.0,  \
+                3.0/    10.0,  \
+            41500.0         ,  \
+               22.0/ 10000.0,  \
+               11.0/ 10000.0,  \
              4100.0
           };
 
@@ -93,13 +93,13 @@ FCT_BGN()
                 1.0/1000.0,  \
                 5.0/ 100.0,  \
             41200.0       ,  \
+                3.0/1000.0,  \
                 2.0/1000.0,  \
-                1.0/1000.0,  \
              4000.0
           };
 
           double dmean [] = \
-          {      1.0/ 10.0,  \
+          {      1.0/  5.0,  \
                 45.0      ,  \
                  1.0/100.0,  \
                  5.0/ 10.0,  \
@@ -109,34 +109,33 @@ FCT_BGN()
              42000.0
           };
 
-          double rms   [] =  \
-          {   4.0/ 10000.0,  \
-             25.0/   100.0,  \
-             15.0/   100.0,  \
-              1.0/    10.0,  \
-            300.0         ,  \
-              8.0/100000.0,  \
-              4.0/100000.0,  \
-              0.0
-          };
-
-          double drms  [] =  \
-          {    42.0/ 1000.0, \
-               24.0/   10.0, \
-              153.0/  100.0, \
-               12.0/   10.0, \
-             3200.0        , \
-               82.0/10000.0, \
-               41.0/10000.0, \
-                0.0
-          };
-
-          double mean_rqq [] = \
+          double rms   [] = \
           { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
-          double dmean_rqq  [] = \
+          double drms  [] = \
           { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
+          double mean_rqq [] =  \
+          {           21.0/  2000.0,  \
+                    8505.0/     4.0,  \
+                      21.0/200000.0,  \
+                      21.0/    80.0,  \
+            178231200000.0         ,  \
+                     189.0/200000.0,  \
+                      21.0/ 50000.0,  \
+                       0.0         
+          };
+
+          double dmean_rqq [] =  \
+          {           11.0/    50.0, \
+                    2025.0         , \
+                       1.0/ 10000.0, \
+                       1.0/     4.0, \
+            169744000000.0         , \
+                     -27.0/  5000.0, \
+                     -19.0/  5000.0, \
+                       0.0         
+          };
 
           double grxDA   [] = \
           {   1.0/ 23000.0, \
@@ -242,43 +241,32 @@ FCT_BGN()
           double srcmean [] = \
           { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
-          double srcrms  [] = \
-          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-
           double srcall  [] = \
           { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
           double srcmean_good [] = \
-          { -  7977.0/ 2300000.0,     \
-            - 78923.0/   18400.0,     \
-            - 15729.0/46000000.0,     \
-            - 16089.0/  920000.0,     \
-            -413010.0/      23.0,     \
-            - 35553.0/46000000.0,     \
-            - 15549.0/46000000.0
-          };
-
-          double srcrms_good  [] = \
-          {     131.0/   250000.0, \
-                301.0/    20000.0, \
-              20099.0/  2000000.0, \
-                 29.0/     2000.0, \
-                157.0/       10.0, \
-            -461241.0/500000000.0, \
-             -28827.0/ 62500000.0  \
+          { -    31.0/    8000.0,     \
+            - 17927.0/    3680.0,     \
+            -   803.0/ 1840000.0,     \
+            -  4789.0/  184000.0,     \
+            -548600.0/      23.0,     \
+            -   447.0/  287500.0,     \
+            -  4249.0/ 4600000.0
           };
 
           double srcall_good  [] = \
-          { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-
-          for (unsigned int i=0; i < neq; ++i)
-          {
-            srcall_good[i] = srcmean_good[i] + srcrms_good[i];
-          }
+          { -      341.0/       80000.0, \
+            -   978763.0/      184000.0, \
+            - 35720497.0/   460000000.0, \
+            -    71333.0/      575000.0, \
+            - 56932639.0/        2300.0, \
+            - 32061997.0/ 23000000000.0, \
+            -  4023811.0/  5750000000.0  \
+          };
 
           const double tolerance = 1.0E-12;
 
-          const char model[] = "bl_spatiotemporal";
+          const char model[] = "bl_spatiotemporal_consistent";
 
           largo_allocate (&generic_workspace, model, neq, ns, 0, "dns");
 
@@ -306,48 +294,40 @@ FCT_BGN()
           largo_zmomentum_setamean  (generic_workspace, 0.0, 1.0, &srcmean[4-1]);
           largo_energy_setamean     (generic_workspace, 0.0, 1.0, &srcmean[5-1]);
           largo_species_setamean    (generic_workspace, 0.0, 1.0, &srcmean[5-0]);
-//           for (int is=0; is < ns; ++is)
-//           {
-//             largo_bl_spatiotemporal_ispecies_setamean (generic_workspace, 0.0, 1.0, &srcmean[5+is], is+1);
-//           }
 
-          // Compute rms sources;
-          largo_continuity_setarms  (generic_workspace, 0.0, 1.0, &srcrms [1-1]);
-          largo_xmomentum_setarms   (generic_workspace, 0.0, 1.0, &srcrms [2-1]);
-          largo_ymomentum_setarms   (generic_workspace, 0.0, 1.0, &srcrms [3-1]);
-          largo_zmomentum_setarms   (generic_workspace, 0.0, 1.0, &srcrms [4-1]);
-          largo_energy_setarms      (generic_workspace, 0.0, 1.0, &srcrms [5-1]);
-          largo_species_setarms     (generic_workspace, 0.0, 1.0, &srcrms [5-0]);
-//           for (int is=0; is < ns; ++is)
-//           {
-//             largo_bl_spatiotemporal_ispecies_setarms (generic_workspace, 0.0, 1.0, &srcrms[5+is], is+1);
-//           }
+          // Compute full sources;
+          largo_continuity_seta     (generic_workspace, 0.0, 1.0, &srcall [1-1]);
+          largo_xmomentum_seta      (generic_workspace, 0.0, 1.0, &srcall [2-1]);
+          largo_ymomentum_seta      (generic_workspace, 0.0, 1.0, &srcall [3-1]);
+          largo_zmomentum_seta      (generic_workspace, 0.0, 1.0, &srcall [4-1]);
+          largo_energy_seta         (generic_workspace, 0.0, 1.0, &srcall [5-1]);
+          largo_species_seta        (generic_workspace, 0.0, 1.0, &srcall [5-0]);
 
           // Deallocate workspace
           largo_deallocate (&generic_workspace);
 
           // Check mean part
           // Tolerance for energy source adjusted manually
-          fct_chk_eq_dbl(srcmean[1-1]       , srcmean_good[1-1]);
-          fct_chk_eq_dbl(srcmean[2-1]/10.0  , srcmean_good[2-1]/10.0);
-          fct_chk_eq_dbl(srcmean[3-1]       , srcmean_good[3-1]);
-          fct_chk_eq_dbl(srcmean[4-1]       , srcmean_good[4-1]);
-          fct_chk_eq_dbl(srcmean[5-1]/10000., srcmean_good[5-1]/10000.);
+          fct_chk_eq_dbl(srcmean[1-1]        , srcmean_good[1-1]);
+          fct_chk_eq_dbl(srcmean[2-1]/10.0   , srcmean_good[2-1]/10.0);
+          fct_chk_eq_dbl(srcmean[3-1]        , srcmean_good[3-1]);
+          fct_chk_eq_dbl(srcmean[4-1]        , srcmean_good[4-1]);
+          fct_chk_eq_dbl(srcmean[5-1]/100000., srcmean_good[5-1]/100000.);
           for (unsigned int is=0; is < ns; ++is)
           {
             fct_chk_eq_dbl(srcmean[5+is]      , srcmean_good[5+is]);
           }
 
-          // Check rms part
+          // Check full source
           // Tolerance for energy source adjusted manually
-          fct_chk_eq_dbl(srcrms[1-1]     , srcrms_good[1-1]);
-          fct_chk_eq_dbl(srcrms[2-1]     , srcrms_good[2-1]);
-          fct_chk_eq_dbl(srcrms[3-1]     , srcrms_good[3-1]);
-          fct_chk_eq_dbl(srcrms[4-1]     , srcrms_good[4-1]);
-          fct_chk_eq_dbl(srcrms[5-1]/100., srcrms_good[5-1]/100.);
+          fct_chk_eq_dbl(srcall[1-1]     , srcall_good[1-1]);
+          fct_chk_eq_dbl(srcall[2-1]/100., srcall_good[2-1]/100.);
+          fct_chk_eq_dbl(srcall[3-1]     , srcall_good[3-1]);
+          fct_chk_eq_dbl(srcall[4-1]     , srcall_good[4-1]);
+          fct_chk_eq_dbl(srcall[5-1]/100., srcall_good[5-1]/100.);
           for (unsigned int is=0; is < ns; ++is)
           {
-            fct_chk_eq_dbl(srcrms[5+is]  , srcrms_good[5+is]);
+            fct_chk_eq_dbl(srcall[5+is]  , srcall_good[5+is]);
           }
 
 
@@ -356,7 +336,7 @@ FCT_BGN()
           largo_allocate (&generic_workspace, model, neq, ns, 0, "dns");
 
           // Init wall baseflow
-          largo_init_wall_baseflow(generic_workspace   \ 
+          largo_init_wall_baseflow(generic_workspace \ 
             ,wall_base ,wall_ddy_base ,wall_ddt_base \
                        ,wall_ddx_base ,wall_src_base \
             );
@@ -377,7 +357,7 @@ FCT_BGN()
           // Check sources
           // Tolerance for energy source adjusted manually
           fct_chk_eq_dbl(srcall[1-1]       , srcall_good[1-1]);
-          fct_chk_eq_dbl(srcall[2-1]/10.0  , srcall_good[2-1]/10.0);
+          fct_chk_eq_dbl(srcall[2-1]/100.0 , srcall_good[2-1]/100.0);
           fct_chk_eq_dbl(srcall[3-1]       , srcall_good[3-1]);
           fct_chk_eq_dbl(srcall[4-1]       , srcall_good[4-1]);
           fct_chk_eq_dbl(srcall[5-1]/10000., srcall_good[5-1]/10000.);
