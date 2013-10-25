0eb27fccc68a4bb59113a58b86a8b583591f3a04
Rhys Ulerich <rhys@ices.utexas.edu>
2013-10-25 17:00:43 +0000

    suzerain: add a run-everything script.
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42315 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/perfect/manual.gold/all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+## A testing script to run every gold solution case in parallel.
+## Failing test case names are displayed after all have finished.
+
+# Used to build and report failed cases
+tempfile() { tempprefix=$(basename "$0"); mktemp /tmp/${tempprefix}.XXXXXX; }
+FAILURES=$(tempfile)
+trap 'rm -f $FAILURES' EXIT
+
+# Scripts to test are every subdirectory with a check.sh
+SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
+for script in "$SCRIPTDIR"/*/check.sh
+do
+    ( "$script" || echo "$script" >> $FAILURES ) &
+done
+
+# Wait for all subshells to finish
+# Output any failures
+wait
+if test -s $FAILURES
+then
+    echo 'Failed cases were as follows: '
+    nl $FAILURES
+fi
+
+# Return success whenever $FAILURES is empty
+! test -s $FAILURES
