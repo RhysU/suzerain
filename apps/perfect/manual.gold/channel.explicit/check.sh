bbd9f9d4916229d28382cdd24c2433e24b2ceb99
Rhys Ulerich <rhys@ices.utexas.edu>
2013-10-25 17:00:40 +0000

    suzerain: add gold 1D channels for apps/perfect
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42314 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/perfect/manual.gold/channel.explicit/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu
+
+## A testing script to see if explicit channel advance matches gold solutions.
+## Small mismatch is acceptable.  Notice --use-system-epsilon can be
+## provided as a command line option so h5diff is less picky.
+
+SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
+rmmkcd() { rm -rfv "$1" && mkdir -vp "$1" && cd "$1"; }
+perfect_init=$(readlink -f perfect_init)
+perfect_advance=$(readlink -f perfect_advance)
+
+case=$(basename "$SCRIPTDIR")
+rmmkcd "gold/$case"
+"$perfect_init" --Ny=32 initial.h5 --htdelta=2
+"$SCRIPTDIR/../neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial.h5" initial.h5   | tee initial.diff
+"$perfect_advance" --explicit initial.h5  --status_nt=1 --advance_dt=0.000919
+"$SCRIPTDIR/../neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/restart0.h5" restart0.h5 | tee restart0.diff
+echo Success for $case
