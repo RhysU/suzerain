91994cd891a8294dbe61d1e0cf148e2019f735d7
Rhys Ulerich <rhys@ices.utexas.edu>
2013-11-06 19:54:05 +0000

    suzerain: improve manual.gold test runner
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42479 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/manual.gold/channel.rhome_xyz/check.sh
+++ apps/perfect/manual.gold/channel.rhome_xyz/check.sh
@@ -6,14 +6,22 @@ set -eu
 ## provided as a command line option so h5diff is less picky.
 
 SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
-rmmkcd() { rm -rfv "$1" && mkdir -vp "$1" && cd "$1"; }
+rmmkcd() { rm -rf "$1" && mkdir -vp "$1" && cd "$1"; }
 perfect_init=$(readlink -f perfect_init)
 perfect_advance=$(readlink -f perfect_advance)
+neno="$SCRIPTDIR/../neno"
 
 case=$(basename "$SCRIPTDIR")
 rmmkcd "gold/$case"
-"$perfect_init" --Ny=32 initial.h5 --htdelta=2
-"$SCRIPTDIR/../neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial.h5" initial.h5   | tee initial.diff
-"$perfect_advance" --implicit=rhome_xyz initial.h5  --status_nt=1 --advance_dt=0.000919
-"$SCRIPTDIR/../neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/restart0.h5" restart0.h5 | tee restart0.diff
+
+"$neno" "$perfect_init" --Ny=32 initial.h5 --htdelta=2 \
+    > init.log
+"$neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial.h5" initial.h5   \
+    > initial.diff
+
+"$neno" "$perfect_advance" --implicit=rhome_xyz initial.h5  --status_nt=1 --advance_dt=0.000919 \
+    > advance.log
+"$neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/restart0.h5" restart0.h5 \
+    > restart0.diff
+
 echo Success for $case
