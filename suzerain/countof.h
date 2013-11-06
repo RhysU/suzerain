f5bd1159fc992bb493763e1bc505275f877e43ae
Rhys Ulerich <rhys@ices.utexas.edu>
2013-11-06 18:55:47 +0000

    suzerain: correct licensing header
    This one was all me, which I'd like to reflect for its
    incorporation into tuna.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42474 0bd85f0b-c151-43b9-a1e9-27231104551f

--- suzerain/countof.h
+++ suzerain/countof.h
@@ -1,25 +1,9 @@
-/*--------------------------------------------------------------------------
- *
+/*
  * Copyright (C) 2011, 2012, 2013 Rhys Ulerich
- * Copyright (C) 2012, 2013 The PECOS Development Team
- * Please see http://pecos.ices.utexas.edu for more information on PECOS.
  *
- * This file is part of Suzerain.
- *
- * Suzerain is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Suzerain is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
- *
- *--------------------------------------------------------------------------
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 #ifndef SUZERAIN_COUNTOF_H
