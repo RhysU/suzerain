e5cf63c1085af71539680b1ae48aa56aeb5ce71c
Rhys Ulerich <rhys@ices.utexas.edu>
2013-11-08 14:56:58 +0000

    suzerain: #2992 spatiotemporal manual.gold solns
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42525 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ apps/perfect/manual.gold/bl.spatiotemporal.explicit/check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu
+
+## A testing script to see if explicit channel advance matches gold solutions.
+## Small mismatch is acceptable.  Notice --use-system-epsilon can be
+## provided as a command line option so h5diff is less picky.
+
+SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
+rmmkcd() { rm -rf "$1" && mkdir -vp "$1" && cd "$1"; }
+perfect_init=$(readlink -f perfect_init)
+perfect_advance=$(readlink -f perfect_advance)
+neno="$SCRIPTDIR/../neno"
+
+case=$(basename "$SCRIPTDIR")
+rmmkcd "gold/$case"
+
+"$neno" "$perfect_init" initial.h5 --k=6 --Nx=1 --Ny=36 --Nz=1 --htdelta=-3 --Re=3000 --Ma=1.5 --Pr=0.7 --Ly=1 --Re_x=3000 --largo_formulation=spatiotemporal --largo_grdelta=1e-1 \
+    > init.log
+"$neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial.h5" initial.h5   \
+    > initial.diff
+
+"$neno" "$perfect_advance" --explicit initial.h5 --status_nt=1 --advance_dt=0.000919 \
+    > advance.log
+"$neno" h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/restart0.h5" restart0.h5 \
+    > restart0.diff
+
+echo Success for $case
