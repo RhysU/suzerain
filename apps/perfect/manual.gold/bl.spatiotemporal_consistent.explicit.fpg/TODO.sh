cb84524df5f738a4ebaab739bdd69c8d1ee2f378
Rhys Ulerich <rhys@ices.utexas.edu>
2013-11-13 19:06:15 +0000

    suzerain: #2503 break out radial nozzle profile
    Is handy to be able to look at this intermediate step.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42615 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/manual.gold/bl.spatiotemporal_consistent.explicit.fpg/TODO.sh
+++ apps/perfect/manual.gold/bl.spatiotemporal_consistent.explicit.fpg/TODO.sh
@@ -14,17 +14,23 @@ case=$(basename "$SCRIPTDIR")
 rmmkcd "gold/$case"
 exec 1> >(tee ./output) 2>&1
 
+# Initialize the appropriate boundary layer grid lacking the radial nozzle settings
 "$perfect_init" initial.h5 --k=6 --Nx=1 --Ny=36 --Nz=1 --htdelta=-3 --Re=3000 --Ma=1.5 --Pr=0.7 --Ly=1 --Re_x=3000 --largo_formulation=spatiotemporal_consistent --largo_grdelta=1e-1
 h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial.h5" initial.h5
 
-# Radial nozzle matches conditions about 1 meter leeward of CEV stagnation
-"$perfect_advance" --explicit initial.h5 --status_nt=1 --advance_dt=0.000919 \
-                   --radial_nozzle_Ma0=3.94774352598265                      \
-                   --radial_nozzle_gam0=1.40926817855057                     \
-                   --radial_nozzle_rho1=1                                    \
-                   --radial_nozzle_u1=-0.154983103461198                     \
-                   --radial_nozzle_p1=1                                      \
+# Overwrite with radial nozzle baseflow conditions about 1 meter leeward of CEV stagnation
+"$perfect_advance" --explicit initial.h5 --advance_nt=0  \
+                   --restart_destination=initial\#.h5    \
+                   --radial_nozzle_Ma0=3.94774352598265  \
+                   --radial_nozzle_gam0=1.40926817855057 \
+                   --radial_nozzle_rho1=1                \
+                   --radial_nozzle_u1=-0.154983103461198 \
+                   --radial_nozzle_p1=1                  \
                    --radial_nozzle_R1=46.1242981386578
+h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/initial0.h5" initial0.h5
+
+# Now advance in time and check the result
+"$perfect_advance" --explicit initial0.h5 --status_nt=1 --advance_dt=0.000919
 h5diff -r -c -v --exclude-path /metadata_generated "$@" "$SCRIPTDIR/restart0.h5" restart0.h5
 
 echo Success for $case
