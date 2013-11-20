c2b39cc35e5315dcafb3eab0319300adecd1947b
Rhys Ulerich <rhys@ices.utexas.edu>
2013-11-20 15:54:29 +0000

    suzerain: use rho not logrhop, Ma!=1 verify,pp fix
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@42705 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/notebooks/nozzle.m
+++ writeups/notebooks/nozzle.m
@@ -5,20 +5,14 @@ function [r u rho p a2 up rhop pp] = nozzle(Ma, gam, R1, R2, u1, rho1,
                                             p1 = rho1/gam
                                                *(1+(gam-1)/2*Ma**2*(1-u1**2)),
                                             rtol=sqrt(eps), atol=sqrt(eps))
-  assert(u1**2 < 2 / Ma**2 / (gam-1) + 1,
+  [Ma2 gam1] = deal(Ma**2, gam-1);
+  assert(u1**2 < 2 / Ma2 / gam1 + 1,
          'Ma=%g, gam=%g, u1=%g imply a**2 <= 0', Ma, gam, u1);
-
-  vopt   = odeset('RelTol',      rtol,       'AbsTol',  atol,
-                  'InitialStep', sqrt(atol), 'MaxStep', sqrt(sqrt(atol)));
-  [r, x] = ode45(@nozzle_ode, [R1 R2], [u1 log(rho1) p1], vopt, Ma**2, gam-1);
-
-  u                 =     x(:,1) ;
-  rho               = exp(x(:,2));
-  p                 =     x(:,3) ;
-  [up, a2, logrhop] = nozzle_helper(r, u, Ma.**2, gam-1);
-  rhop              = logrhop.*rho;
-  pp                = -Ma.**2.*rho.*u.*up;
-
+  vopt  = odeset('RelTol',      rtol,       'AbsTol',  atol,
+                 'InitialStep', sqrt(atol), 'MaxStep', sqrt(sqrt(atol)));
+  [r x] = ode45(@nozzle_rhs, [R1 R2], [u1 rho1 p1], vopt, Ma2, gam1);
+  [u  rho  p    ] = deal(x(:,1), x(:,2), x(:,3));
+  [up rhop pp a2] = nozzle_details(r, u, rho, p, Ma2, gam1);
   if 0 == nargout
     figure();
     plot(r, u, '-', r, sqrt(a2), '-', r, rho, '-', r, p, 'g-');
@@ -28,30 +22,30 @@ function [r u rho p a2 up rhop pp] = nozzle(Ma, gam, R1, R2, u1, rho1,
   end
 end
 
-% ODEs [u; log rho; p]' given r, x=[u; log rho; p], Ma2=Ma**2, gamm1=gam-1
-function f = nozzle_ode(r, x, Ma2, gamm1)
-  [up, a2, logrhop] = nozzle_helper(r, x(1), Ma2, gamm1);
-  pp                = -Ma2.*exp(x(2)).*x(1).*up;
-  f                 = [up; logrhop; pp];
+% ODEs [u; rho; p]' given r, x=[u; rho; p], Ma2=Ma**2, gamm1=gam-1
+function f = nozzle_rhs(r, x, Ma2, gamm1)
+  [up, rhop, pp] = nozzle_details(r, x(1), x(2), x(3), Ma2, gamm1);
+  f              = [up; rhop; pp];
 end
 
-% Helper computing pointwise details given r, u, Ma2=Ma**2, gamm1=gam-1
-function [up, a2, logrhop] = nozzle_helper(r, u, Ma2, gamm1)
-  C       = (2./Ma2 + gamm1.*(1-u.**2));
-  up      = (u .* C) ./ (r .* (2*u.**2 - C));
-  a2      = 1 + 0.5*Ma2.*gamm1.*(1 - u.**2);
-  logrhop = -Ma2.*u.*up ./ a2;
+% Find pointwise solution details given state, Ma2=Ma**2, gamm1=gam-1
+function [up, rhop, pp, a2] = nozzle_details(r, u, rho, p, Ma2, gamm1)
+  u2   = u.**2;
+  C    = (2./Ma2 + gamm1.*(1 - u2));
+  up   = (u.*C) ./ (r.*(2*u2 - C));
+  pp   = -Ma2.*rho.*u.*up;
+  a2   = 1 + 0.5*Ma2.*gamm1.*(1 - u2);
+  rhop = pp ./ a2;
 end
 
 %!test
-%! % FIXME Test with Ma != 1.0
 %! % Does a solution satisfy steady governing equations in radial setting?
 %! % A verification test, including derivatives, against governing equations.
 %! % Ideal gas EOS will be approximately satisfied for "large enough" radii.
-%! pkg load odepkg; Ma=1.0; gam=1.4; Rin=5; Rout=Rin+1; u1=-2/7; rho1=9/10;
+%! pkg load odepkg; Ma=1.5; gam=1.4; Rin=10; Rout=Rin+1/2; u1=-2/7; rho1=9/10;
 %! [r u rho p a2 up rhop pp] = nozzle(Ma, gam, Rout, Rin, u1, rho1);
-%! assert(zeros(size(r))', (u.*rho./r+rho.*up+u.*rhop)', 10*eps);  # Mass
-%! assert(pp', (-rho.*u.*up)', 10*eps);                            # Momentum
+%! assert(zeros(size(r))', (u.*rho./r+rho.*up+u.*rhop)', 11*eps);  # Mass
+%! assert(pp', (-Ma.**2.*rho.*u.*up)', 10*eps);                    # Momentum
 %! assert(a2', (1 + Ma.**2.*(gam-1)./2.*(1-u.**2))', 10*eps);      # Energy
 %! assert((rho.*a2)', (gam.*p)', 10*eps);                          # Ideal EOS
 
