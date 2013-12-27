a4443ede03bca415f04aa79175815f97b1420144
Rhys Ulerich <rhys@ices.utexas.edu>
2013-12-27 15:35:58 +0000

    suzerain: get writeup.sh working
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43147 0bd85f0b-c151-43b9-a1e9-27231104551f

--- writeups/writeup.sh
+++ writeups/writeup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 # Use latexmk to build one writeup without the full Suzerain build system.
-# For example, 'writeup baseflow.tex' should build 'baseflow.pdf'.
-# Be aware that will tend to be exceedingly brittle.
+# For example, 'writeup.sh baseflow.tex' should build 'baseflow.pdf'.
+# Notably, 'writeup.sh baseflow.tex -pvc' starts a continuous previewer.
+# Be aware that this hack may be exceedingly brittle.
 set -eu
 target=$1
 shift
 SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
-TEXINPUTS="${TEXINPUTS:+$TEXINPUTS:}${SCRIPTDIR}:${SCRIPTDIR}/.." \
-    latexmk                                                       \
-    "-auxdir=${TMPDIR-/tmp}"                                      \
-    -dvi-                                                         \
-    -ps-                                                          \
-    -pdf                                                          \
-    -recorder                                                     \
-    "$@"                                                          \
-    -pdflatex="pdflatex --shell-escape %O %S" "$target"
+TEXINPUTS=":${TEXINPUTS:+$TEXINPUTS:}${SCRIPTDIR}:${SCRIPTDIR}/.." \
+BIBINPUTS=":${BIBINPUTS:+$BIBINPUTS:}${SCRIPTDIR}:${SCRIPTDIR}/.." \
+    latexmk -dvi- -ps- -pdf -recorder                              \
+    "$@"                                                           \
+    -pdflatex="pdflatex --shell-escape %O %S"                      \
+    "$SCRIPTDIR/$target"
