68fca392f7d86425f5648fe91b1921baf30424c7
Rhys Ulerich <rhys@ices.utexas.edu>
2013-12-31 17:39:34 +0000

    suzerain: #3020 match test cases
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43162 0bd85f0b-c151-43b9-a1e9-27231104551f

--- notebooks/nozzle_match.m
+++ notebooks/nozzle_match.m
@@ -17,16 +17,29 @@ function [R0 Ma0 u1 rho1 p1] = nozzle_match(delta, gam0, Ma_e, p_exi, T_e=0)
   endif
 end
 
-%!test
+%!test % Supersonic nozzle with hot edge
 %! [R0 Ma0 u1 rho1 p1] = nozzle_match(1, 1.4087, 1.1906, -0.025439, 4.0040);
 %! [Ma_e p_exi T_e]    = nozzle_qoi  (1, 1.4087, Ma0, R0, u1, rho1, p1);
 %! assert(Ma_e,   1.1906,   -0.002);
 %! assert(p_exi, -0.025439, -0.002);
 %! assert(T_e,    4.0040,   -0.002);
 
-%!test
+%!test % Subsonic nozzle with hot edge
 %! [R0 Ma0 u1 rho1 p1] = nozzle_match(1, 1.4088, 0.54927, -0.014755, 4.1541);
 %! [Ma_e p_exi T_e]    = nozzle_qoi  (1, 1.4088, Ma0, R0, u1, rho1, p1);
-%! assert(Ma_e,   0.54927,  -0.002);
-%! assert(p_exi, -0.014755, -0.002);
-%! assert(T_e,    4.1541,   -0.002);
+%! assert(Ma_e,   0.54927,  -0.001);
+%! assert(p_exi, -0.014755, -0.001);
+%! assert(T_e,    4.1541,   -0.001);
+
+%!test % Supersonic diffuser with cold edge with non-unit delta
+%! [R0 Ma0 u1 rho1 p1] = nozzle_match(0.5, 1.4, 1.5, +0.02, 0.50);
+%! [Ma_e p_exi T_e]    = nozzle_qoi  (0.5, 1.4, Ma0, R0, u1, rho1, p1);
+%! assert(Ma_e,   1.5,  -0.0015);
+%! assert(p_exi, +0.02, -0.0015);
+%! assert(T_e,    0.5,  -0.0015);
+
+%!test % Subsonic diffuser without prescribed edge temperature
+%! [R0 Ma0 u1 rho1 p1] = nozzle_match(1, 1.4, 0.5, +0.015);
+%! [Ma_e p_exi T_e]    = nozzle_qoi  (1, 1.4, Ma0, R0, u1, rho1, p1);
+%! assert(Ma_e,   0.5,   -0.0001);
+%! assert(p_exi, +0.015, -0.0001);
