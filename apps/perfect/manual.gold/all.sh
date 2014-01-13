afbafd661c7f9694b6b7a056e0fd3034703f8554
Rhys Ulerich <rhys@ices.utexas.edu>
2014-01-13 19:13:34 +0000

    suzerain: --use-system-epsilon by default
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43333 0bd85f0b-c151-43b9-a1e9-27231104551f

--- apps/perfect/manual.gold/all.sh
+++ apps/perfect/manual.gold/all.sh
@@ -6,24 +6,37 @@ set -eu
 ## Any flags, e.g. --use-system-epsilon, will be propagated.
 
 # Used to build and report failed cases
-tempfile() { tempprefix=$(basename "$0"); mktemp /tmp/${tempprefix}.XXXXXX; }
+tempfile() { tempprefix=$(basename "$0"); mktemp "/tmp/${tempprefix}.XXXXXX"; }
 FAILURES=$(tempfile)
 SUCCESSES=$(tempfile)
 trap 'rm -f "$SUCCESSES" "$FAILURES"' EXIT
+
+# Default arguments to pass to scripts whenever non provided by user
+defaultargs=--use-system-epsilon
 
 # Scripts to test are every subdirectory with a check.sh
 SCRIPTDIR="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )"; pwd )"
 for script in "$SCRIPTDIR"/*/check.sh
 do
-    ( "$SCRIPTDIR/neno" "$script" "$@"       \
-      && echo "$script" "$@" >> "$SUCCESSES" \
-      || echo "$script" "$@" >> "$FAILURES"  ) &
+    ( "$SCRIPTDIR/neno" "$script" "${@-$defaultargs}"        \
+      && echo "$script" "${@-defaultargs}" >> "$SUCCESSES"   \
+      || echo "$script" "${@-defaultargs}" >> "$FAILURES"  ) &
 done
 
 # Wait for all tests to finish
 wait
 
 # Output successes and failures
+if test $# -lt 1; then
+    echo "The following default options were provided to each case:"
+else
+    echo "The following options were provided to each case:"
+fi
+declare -i i=1
+for arg in "${@-$defaultargs}"; do
+    printf $'%6d  %s\n' "$i" "$arg"
+    i+=1
+done
 if test -s "$SUCCESSES"; then
     echo 'Successful cases were as follows: '
     sort "$SUCCESSES" | nl
