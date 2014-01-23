32cb577cd130642f174f91a12f149c1416a1d5f1
Rhys Ulerich <rhys@ices.utexas.edu>
2014-01-23 20:04:24 +0000

    suzerain: note meltdown in section 6
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43446 0bd85f0b-c151-43b9-a1e9-27231104551f

--- notebooks/radialflow_match.m
+++ notebooks/radialflow_match.m
@@ -1,20 +1,21 @@
 % Produce a flow matching the given boundary layer edge conditions.
 % If supplied, T_e specifies an edge temperature for the ideal gas EOS.
-function [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, Ma_e, p_exi, T_e=0)
+function [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, Ma_e, p_exi,
+                                                 T_e=0, theta=1)
 
   % Track candidates from zero, one, or two positive real roots for R0
   R0   = roots([ - abs(Ma_e.^2 - 1)*abs(p_exi),
                    delta,
                  - Ma_e.^2 * delta.^2 * abs(p_exi) * sign(Ma_e.^2 - 1) ]);
   R0   = sort(R0(arrayfun(@isreal,R0) & real(R0) > 0), 'descend');
-  Ma   = 1 ./ realsqrt(1/Ma_e.^2 + (gam - 1)*delta.^2./R0.^2/2);
+  Ma   = 1 ./ realsqrt((1/Ma_e.^2 + (gam - 1)*delta.^2./R0.^2/2)./theta);
   R    = realsqrt(R0.^2 + delta.^2);
   uR   = - R ./ R0 * sign(p_exi.*(Ma_e.^2 - 1));
   rhoR = ones(size(R0));
   pR   = merge(T_e == 0, ones(size(R0)), T_e.*Ma.^2.*rhoR./gam./Ma_e.^2);
 
   % Thin candidates down to realizable solution with largest R0
-  [ok iok] = max(T_e < gam.*Ma_e.^2.*(1/2 + 1./Ma.^2./(gam - 1)));
+  [ok iok] = max(T_e < gam.*Ma_e.^2.*(1/2 + theta./Ma.^2./(gam - 1)));
   if ok
     R0 = R0(iok); Ma   = Ma  (iok); R  = R (iok);
     uR = uR(iok); rhoR = rhoR(iok); pR = pR(iok);
@@ -27,33 +28,33 @@ function [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, Ma_e, p_exi, T_e=0)
 end
 
 %!test % Round trip: Supersonic nozzle with hot edge
-%! delta = 1; gam = 1.4087;
-%! [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, 1.1906, -0.025439, 4.0040);
-%! [r u rho p]          = radialflow(Ma, gam, R, R0, uR, rhoR, pR);         % R->R0
-%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));           % R0
-%! [Ma_e p_exi T_e]     = radialflow_qoi(delta, gam, Ma, R1, u1, rho1, p1); % R0->R
+%! d=1; g=1.4087; t=1;
+%! [Ma R0 R uR rhoR pR] = radialflow_match(d, g, 1.1906, -0.025439, 4.0040, t);
+%! [r u rho p]          = radialflow(Ma, g, R, R0, uR, rhoR, pR, t);
+%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));
+%! [Ma_e p_exi T_e]     = radialflow_qoi(d, g, Ma, R1, u1, rho1, p1, t);
 %! assert([Ma_e p_exi T_e], [1.1906, -0.025439, 4.0040], -sqrt(eps));
 
 %!test % Round trip: Subsonic nozzle with less hot edge
-%! delta = 1; gam = 1.4088;
-%! [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, 0.54927, -0.014755, 3.1541);
-%! [r u rho p]          = radialflow(Ma, gam, R, R0, uR, rhoR, pR);         % R->R0
-%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));           % R0
-%! [Ma_e p_exi T_e]     = radialflow_qoi(delta, gam, Ma, R1, u1, rho1, p1); % R0->R
+%! d=1; g=1.4088; t=1;
+%! [Ma R0 R uR rhoR pR] = radialflow_match(d, g, 0.54927, -0.014755, 4.1541, t);
+%! [r u rho p]          = radialflow(Ma, g, R, R0, uR, rhoR, pR, t);
+%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));
+%! [Ma_e p_exi T_e]     = radialflow_qoi(d, g, Ma, R1, u1, rho1, p1, t);
 %! assert([Ma_e p_exi T_e], [0.54927, -0.014755, 3.1541], -sqrt(eps));
 
 %!test % Round trip: Supersonic diffuser with cold edge with non-unit delta
-%! delta = 0.5; gam = 1.4;
-%! [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, 1.5, +0.02, 0.50);
-%! [r u rho p]          = radialflow(Ma, gam, R, R0, uR, rhoR, pR);         % R->R0
-%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));           % R0
-%! [Ma_e p_exi T_e]     = radialflow_qoi(delta, gam, Ma, R1, u1, rho1, p1); % R0->R
+%! d=0.5; g=1.4; t=1;
+%! [Ma R0 R uR rhoR pR] = radialflow_match(d, g, 1.5, +0.02, 0.50, t);
+%! [r u rho p]          = radialflow(Ma, g, R, R0, uR, rhoR, pR, t);
+%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));
+%! [Ma_e p_exi T_e]     = radialflow_qoi(d, g, Ma, R1, u1, rho1, p1, t);
 %! assert([Ma_e p_exi T_e], [1.5, +0.02, 0.50], -sqrt(eps));
 
 %!test % Round trip: Subsonic diffuser without prescribed edge temperature
-%! delta = 1; gam = 1.4;
-%! [Ma R0 R uR rhoR pR] = radialflow_match(delta, gam, 0.5, +0.015);
-%! [r u rho p]          = radialflow(Ma, gam, R, R0, uR, rhoR, pR);         % R->R0
-%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));           % R0
-%! [Ma_e p_exi T_e]     = radialflow_qoi(delta, gam, Ma, R1, u1, rho1, p1); % R0->R
+%! d=1; g=1.4; t=1;
+%! [Ma R0 R uR rhoR pR] = radialflow_match(d, g, 0.5, +0.015, 0, t);
+%! [r u rho p]          = radialflow(Ma, g, R, R0, uR, rhoR, pR, t);
+%! [R1 u1 rho1 p1]      = deal(r(end), u(end), rho(end), p(end));
+%! [Ma_e p_exi T_e]     = radialflow_qoi(d, g, Ma, R1, u1, rho1, p1, t);
 %! assert([Ma_e p_exi pR], [0.5, +0.015, 1.0], -sqrt(eps));
