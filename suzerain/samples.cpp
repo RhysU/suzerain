de001b6b0c1d9cb17d956a8200caba805b4c8cc5
Rhys Ulerich <rhys@ices.utexas.edu>
2014-02-03 18:50:35 +0000

    suzerain: #2736 quantities towards libsuzerain
    Begins to move much of the quantities logic into the libsuzerain
    under the name suzerain::samples, SUZERAIN_SAMPLES, etc.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43637 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/samples.cpp
@@ -0,0 +1,49 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2008-2014 Rhys Ulerich
+// Copyright (C) 2012-2014 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc samples.hpp
+ */
+
+#include "samples.hpp"
+
+namespace suzerain {
+
+samples::samples()
+    : t(std::numeric_limits<real_t>::quiet_NaN())
+{
+}
+
+samples::samples(real_t)
+    : t(t)
+{
+}
+
+samples::samples(real_t t,
+                 samples::storage_type::Index Ny)
+    : t(t)
+    , storage(storage_type::Zero(Ny, storage_type::ColsAtCompileTime))
+{
+}
+
+} // namespace suzerain
