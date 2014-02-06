72e6bd171bea4d8fa417746e50aea7fbe2d804d9
Rhys Ulerich <rhys@ices.utexas.edu>
2014-02-06 02:59:38 +0000

    suzerain: stub arsel into libprecise
    Want nice floating point behavior on these routines,
    hence libprecise.
    
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@43741 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/arsel.cpp
@@ -0,0 +1,38 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2012-2014 Rhys Ulerich
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc arsel.hpp
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+
+#include <suzerain/arsel.hpp>
+#include <suzerain/ar.hpp>
+
+namespace suzerain {
+
+// TODO
+
+} // namespace suzerain
---
+++ suzerain/arsel.hpp
@@ -0,0 +1,35 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2012-2014 Rhys Ulerich
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_ARSEL_HPP
+#define SUZERAIN_ARSEL_HPP
+
+/** @file
+ * Automatically fit autoregressive models to input signals.
+ * Serves as a high-level wrapper of functionality within \ref ar.hpp.
+ */
+
+namespace suzerain {
+
+} // end namespace suzerain
+
+#endif /* SUZERAIN_ARSEL_HPP */
