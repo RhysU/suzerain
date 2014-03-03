b8bf1d6e2bd9311bfc651f41424ed5b0e15f2b04
Rhys Ulerich <rhys@ices.utexas.edu>
2014-03-03 23:27:42 +0000

    suzerain: #3055 #3056 stub extrema API
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@44177 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ suzerain/extrema.cpp
@@ -0,0 +1,397 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2008-2014 Rhys Ulerich
+// Copyright (C) 2012-2014 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+/** @file
+ * @copydoc extrema.hpp
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <suzerain/config.h>
+#endif
+
+#include <suzerain/extrema.hpp>
+
+#include <suzerain/bspline.hpp>
+#include <suzerain/error.h>
+#include <suzerain/inorder.hpp>
+#include <suzerain/mpi_datatype.hpp>
+#include <suzerain/mpi.hpp>
+#include <suzerain/pencil_grid.hpp>
+#include <suzerain/specification_grid.hpp>
+#include <suzerain/state.hpp>
+
+namespace suzerain {
+
+//// std::vector<field_extrema_xyz>
+//// compute_field_extrema_xyz(
+////         const contiguous_state<4,complex_t> &state,
+////         const specification_grid& grid,
+////         const pencil_grid& dgrid,
+////         const bsplineop& gop)
+//// {
+////     // Ensure state storage meets this routine's assumptions
+////     // Notice state.shape()[0] may be any value
+////     using boost::numeric_cast;
+////     assert(numeric_cast<int>(state.shape()[1]) == dgrid.local_wave_extent.y());
+////     assert(numeric_cast<int>(state.shape()[2]) == dgrid.local_wave_extent.x());
+////     assert(numeric_cast<int>(state.shape()[3]) == dgrid.local_wave_extent.z());
+////
+////     // Ensure we were handed Galerkin field_extrema_xyz operator matrices
+////     SUZERAIN_ENSURE(gop.get()->method==SUZERAIN_BSPLINEOP_GALERKIN_L2);
+////
+////     // Only want non-dealiased X-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for X direction
+////     int fxb[2], fxe[2], mxb[2], mxe[2];
+////     inorder::wavenumber_translate(grid.N.x(),
+////                                   grid.dN.x(),
+////                                   dgrid.local_wave_start.x(),
+////                                   dgrid.local_wave_end.x(),
+////                                   fxb[0], fxe[0], fxb[1], fxe[1],
+////                                   mxb[0], mxe[0], mxb[1], mxe[1]);
+////     // X contains only positive wavenumbers => second range must be empty
+////     assert(fxb[1] == fxe[1]);
+////     assert(mxb[1] == mxe[1]);
+////
+////     // Only want non-dealiased Z-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for Z direction
+////     // One or both ranges may be empty
+////     int fzb[2], fze[2], mzb[2], mze[2];
+////     inorder::wavenumber_translate(grid.N.z(),
+////                                   grid.dN.z(),
+////                                   dgrid.local_wave_start.z(),
+////                                   dgrid.local_wave_end.z(),
+////                                   fzb[0], fze[0], fzb[1], fze[1],
+////                                   mzb[0], mze[0], mzb[1], mze[1]);
+////
+////     // Temporary storage for inner product computations
+////     //
+////     // Though u^H M u is always real for SPD M, floating point errors may
+////     // produce a slightly non-SPD M so we permit complex intermediate results
+////     // up until the return structure is packed.
+////     VectorXc tmp;
+////     tmp.setZero(grid.N.y());
+////
+////     // Contiguous temporary storage for accumulating and broadcasting results
+////     VectorXc buf(2*state.shape()[0]);
+////     Map<VectorXc> total2(buf.data() + 0,                state.shape()[0]);
+////     Map<VectorXc> mean2 (buf.data() + state.shape()[0], state.shape()[0]);
+////
+////     // Compute the local L2 contribution towards each L^2_xyz norm squared
+////     // Computation uses partial sums at each loop to reduce swamping which is
+////     // more-or-less recursive summation using large partitioning factors.
+////     total2.setZero();
+////     for (size_t k = 0; k < state.shape()[0]; ++k) {
+////         complex_t jsum = 0;
+////         for (int j = 0; j < 2; ++j) {
+////             complex_t nsum = 0;
+////             for (int n = mzb[j]; n < mze[j]; ++n) {
+////                 complex_t msum = 0;
+////                 for (int m = mxb[0]; m < mxe[0]; ++m) {
+////                     const complex_t * u_mn
+////                         = &state[k][0][m - dgrid.local_wave_start.x()]
+////                                       [n - dgrid.local_wave_start.z()];
+////                     gop.accumulate(0, 1.0, u_mn, 1, 0.0, tmp.data(), 1);
+////                     complex_t dot = blas::dot(grid.N.y(), u_mn,       1,
+////                                                           tmp.data(), 1);
+////                     if (m > 0 && m < grid.dN.x()/2) {
+////                         dot *= 2;
+////                     }
+////                     msum += dot;
+////                 }
+////                 nsum += msum;
+////             }
+////             jsum += nsum;
+////         }
+////         total2[k] += jsum;
+////     }
+////     total2 *= grid.L.x() * grid.L.z();
+////
+////     // Reduce total2 sum onto processor housing the zero-zero mode using
+////     // mean2 as a scratch buffer to simulate MPI_IN_PLACE.
+////     //
+////     // Reduction operation is complex-valued, but OpenMPI pre-1.7.3 bombs unless
+////     // we keep it real: https://svn.open-mpi.org/trac/ompi/ticket/3127.
+////     SUZERAIN_MPICHKR(MPI_Reduce(total2.data(), mean2.data(),
+////             (sizeof(complex_t)/sizeof(real_t))*total2.size(),
+////             mpi::datatype<real_t>(), MPI_SUM,
+////             dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+////     total2 = mean2;
+////
+////     // Compute the mean-only L^2 squared for each field using zero-zero modes
+////     if (dgrid.has_zero_zero_modes()) {
+////         for (size_t k = 0; k < state.shape()[0]; ++k) {
+////             const complex_t * u_mn = &state[k][0][0][0];
+////             gop.accumulate(0, 1.0, u_mn, 1, 0.0, tmp.data(), 1);
+////             mean2[k] = blas::dot(grid.N.y(), u_mn, 1, tmp.data(), 1);
+////         }
+////         mean2 *= grid.L.x() * grid.L.z();
+////     }
+////
+////     // Broadcast total2 and mean2 values to all processors
+////     //
+////     // OpenMPI 1.6.5 hates broadcasting complex data but real values are okay:
+////     // https://svn.open-mpi.org/trac/ompi/ticket/4323
+////     SUZERAIN_MPICHKR(MPI_Bcast(buf.data(),
+////             (sizeof(complex_t)/sizeof(real_t))*buf.size(),
+////             mpi::datatype<real_t>(), dgrid.rank_zero_zero_modes,
+////             MPI_COMM_WORLD));
+////
+////     // Obtain fluctuating2 = total2 - mean2 and pack the return structure
+////     std::vector<field_extrema_xyz> retval(state.shape()[0]);
+////     for (size_t k = 0; k < retval.size(); ++k) {
+////         retval[k].mean        = std::sqrt(std::abs(mean2[k]            ));
+////         retval[k].fluctuating = std::sqrt(std::abs(total2[k] - mean2[k]));
+////     }
+////
+////     return retval;
+//// }
+////
+//// //
+//// // Implementation very much resembles compute_field_extrema_xyz.
+//// // Begin by understanding that first and then compare the differences.
+//// //
+//// std::vector<field_extrema_xz>
+//// compute_field_extrema_xz(
+////         const contiguous_state<4,complex_t> &state,
+////         const specification_grid& grid,
+////         const pencil_grid& dgrid,
+////         const bsplineop& cop)
+//// {
+////     // Ensure state storage meets this routine's assumptions
+////     // Notice state.shape()[0] may be any value
+////     using boost::numeric_cast;
+////     assert(numeric_cast<int>(state.shape()[1]) == dgrid.local_wave_extent.y());
+////     assert(numeric_cast<int>(state.shape()[2]) == dgrid.local_wave_extent.x());
+////     assert(numeric_cast<int>(state.shape()[3]) == dgrid.local_wave_extent.z());
+////
+////     // Ensure we were handed collocation-based operator matrices
+////     SUZERAIN_ENSURE(cop.get()->method==SUZERAIN_BSPLINEOP_COLLOCATION_GREVILLE);
+////
+////     // Only want non-dealiased X-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for X direction
+////     int fxb[2], fxe[2], mxb[2], mxe[2];
+////     inorder::wavenumber_translate(grid.N.x(),
+////                                   grid.dN.x(),
+////                                   dgrid.local_wave_start.x(),
+////                                   dgrid.local_wave_end.x(),
+////                                   fxb[0], fxe[0], fxb[1], fxe[1],
+////                                   mxb[0], mxe[0], mxb[1], mxe[1]);
+////     // X contains only positive wavenumbers => second range must be empty
+////     assert(fxb[1] == fxe[1]);
+////     assert(mxb[1] == mxe[1]);
+////
+////     // Only want non-dealiased Z-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for Z direction
+////     // One or both ranges may be empty
+////     int fzb[2], fze[2], mzb[2], mze[2];
+////     inorder::wavenumber_translate(grid.N.z(),
+////                                   grid.dN.z(),
+////                                   dgrid.local_wave_start.z(),
+////                                   dgrid.local_wave_end.z(),
+////                                   fzb[0], fze[0], fzb[1], fze[1],
+////                                   mzb[0], mze[0], mzb[1], mze[1]);
+////
+////     // Temporary complex-valued storage for matrix-vector products
+////     ArrayXc tmp;
+////     tmp.setZero(grid.N.y());
+////
+////     // Contiguous temporary storage for accumulating and broadcasting results
+////     // Fast columns store wall-normal swaths of data.
+////     // Real-valued since cwiseAbs2 always produces real-valued results.
+////     ArrayXXr buf(grid.N.y(), 2*state.shape()[0]);
+////     ArrayXXr::ColsBlockXpr total2 = buf.leftCols (state.shape()[0]);
+////     ArrayXXr::ColsBlockXpr mean2  = buf.rightCols(state.shape()[0]);
+////
+////     // Compute the local L2 contribution towards each L^2_xz norm squared
+////     // Computation uses partial sums at each loop to reduce swamping which is
+////     // more-or-less recursive summation using large partitioning factors.
+////     ArrayXr jsum, nsum, msum;
+////     total2.setZero();
+////     for (size_t k = 0; k < state.shape()[0]; ++k) {
+////         jsum.setZero(grid.N.y());
+////         for (int j = 0; j < 2; ++j) {
+////             nsum.setZero(grid.N.y());
+////             for (int n = mzb[j]; n < mze[j]; ++n) {
+////                 msum.setZero(grid.N.y());
+////                 for (int m = mxb[0]; m < mxe[0]; ++m) {
+////                     const complex_t * u_mn
+////                         = &state[k][0][m - dgrid.local_wave_start.x()]
+////                                       [n - dgrid.local_wave_start.z()];
+////                     if (m > 0 && m < grid.dN.x()/2) {
+////                         // Conjugate symmetry uses root-2 so Abs2 below gives 2
+////                         cop.accumulate(0, M_SQRT2, u_mn, 1, 0., tmp.data(), 1);
+////                     } else {
+////                         cop.accumulate(0, 1.0,     u_mn, 1, 0., tmp.data(), 1);
+////                     }
+////                     msum += tmp.abs2();
+////                 }
+////                 nsum += msum;
+////             }
+////             jsum += nsum;
+////         }
+////         total2.col(k) += jsum;
+////     }
+////     total2 *= grid.L.x() * grid.L.z();
+////
+////     // Reduce total2 sum onto processor housing the zero-zero mode using
+////     // mean2 as a scratch buffer to simulate MPI_IN_PLACE
+////     SUZERAIN_MPICHKR(MPI_Reduce(total2.data(), mean2.data(),
+////             total2.size(), mpi::datatype<real_t>(),
+////             MPI_SUM, dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+////     total2 = mean2;
+////
+////     // Compute the mean-only L^2 squared for each field using zero-zero modes
+////     if (dgrid.has_zero_zero_modes()) {
+////         for (size_t k = 0; k < state.shape()[0]; ++k) {
+////             const complex_t * u_mn = &state[k][0][0][0];
+////             cop.accumulate(0, 1.0, u_mn, 1, 0.0, tmp.data(), 1);
+////             mean2.col(k) = tmp.cwiseAbs2();
+////         }
+////         mean2 *= grid.L.x() * grid.L.z();
+////     }
+////
+////     // Broadcast total2 and mean2 values to all processors
+////     SUZERAIN_MPICHKR(MPI_Bcast(buf.data(),
+////             buf.size(), mpi::datatype<real_t>(),
+////             dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+////
+////     // Obtain fluctuating2 = total2 - mean2 and pack the return structure
+////     std::vector<field_extrema_xz> retval(state.shape()[0]);
+////     for (size_t k = 0; k < retval.size(); ++k) {
+////         retval[k].mean        = mean2.col(k).sqrt();
+////         retval[k].fluctuating = (total2.col(k) - mean2.col(k)).sqrt();
+////     }
+////
+////     return retval;
+//// }
+////
+//// //
+//// // Implementation very much resembles previous compute_field_extrema_xz
+//// // with this routine assuming B-spline mass matrix already applied
+//// //
+//// std::vector<field_extrema_xz>
+//// compute_field_extrema_xz(
+////         const contiguous_state<4,complex_t> &state,
+////         const specification_grid& grid,
+////         const pencil_grid& dgrid)
+//// {
+////     // Ensure state storage meets this routine's assumptions
+////     // Notice state.shape()[0] may be any value
+////     using boost::numeric_cast;
+////     assert(numeric_cast<int>(state.shape()[1]) == dgrid.local_wave_extent.y());
+////     assert(numeric_cast<int>(state.shape()[2]) == dgrid.local_wave_extent.x());
+////     assert(numeric_cast<int>(state.shape()[3]) == dgrid.local_wave_extent.z());
+////
+////     // Only want non-dealiased X-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for X direction
+////     int fxb[2], fxe[2], mxb[2], mxe[2];
+////     inorder::wavenumber_translate(grid.N.x(),
+////                                   grid.dN.x(),
+////                                   dgrid.local_wave_start.x(),
+////                                   dgrid.local_wave_end.x(),
+////                                   fxb[0], fxe[0], fxb[1], fxe[1],
+////                                   mxb[0], mxe[0], mxb[1], mxe[1]);
+////     // X contains only positive wavenumbers => second range must be empty
+////     assert(fxb[1] == fxe[1]);
+////     assert(mxb[1] == mxe[1]);
+////
+////     // Only want non-dealiased Z-direction modes to contribute to L2
+////     // Compute wavenumber translation logistics for Z direction
+////     // One or both ranges may be empty
+////     int fzb[2], fze[2], mzb[2], mze[2];
+////     inorder::wavenumber_translate(grid.N.z(),
+////                                   grid.dN.z(),
+////                                   dgrid.local_wave_start.z(),
+////                                   dgrid.local_wave_end.z(),
+////                                   fzb[0], fze[0], fzb[1], fze[1],
+////                                   mzb[0], mze[0], mzb[1], mze[1]);
+////
+////     // Contiguous temporary storage for accumulating and broadcasting results
+////     // Fast columns store wall-normal swaths of data.
+////     // Real-valued since cwiseAbs2 always produces real-valued results.
+////     ArrayXXr buf(grid.N.y(), 2*state.shape()[0]);
+////     ArrayXXr::ColsBlockXpr total2 = buf.leftCols (state.shape()[0]);
+////     ArrayXXr::ColsBlockXpr mean2  = buf.rightCols(state.shape()[0]);
+////
+////     // Compute the local L2 contribution towards each L^2_xz norm squared
+////     // Computation uses partial sums at each loop to reduce swamping which is
+////     // more-or-less recursive summation using large partitioning factors.
+////     ArrayXr jsum, nsum, msum;
+////     total2.setZero();
+////     for (size_t k = 0; k < state.shape()[0]; ++k) {
+////         jsum.setZero(grid.N.y());
+////         for (int j = 0; j < 2; ++j) {
+////             nsum.setZero(grid.N.y());
+////             for (int n = mzb[j]; n < mze[j]; ++n) {
+////                 msum.setZero(grid.N.y());
+////                 for (int m = mxb[0]; m < mxe[0]; ++m) {
+////                     Map<const ArrayXc> u_mn(
+////                             &state[k][0][m - dgrid.local_wave_start.x()]
+////                                         [n - dgrid.local_wave_start.z()],
+////                             grid.N.y());
+////                     if (m > 0 && m < grid.dN.x()/2) { // Conjugate symmetry
+////                         msum += 2*u_mn.abs2();
+////                     } else {
+////                         msum +=   u_mn.abs2();
+////                     }
+////                 }
+////                 nsum += msum;
+////             }
+////             jsum += nsum;
+////         }
+////         total2.col(k) += jsum;
+////     }
+////     total2 *= grid.L.x() * grid.L.z();
+////
+////     // Reduce total2 sum onto processor housing the zero-zero mode using
+////     // mean2 as a scratch buffer to simulate MPI_IN_PLACE
+////     SUZERAIN_MPICHKR(MPI_Reduce(total2.data(), mean2.data(),
+////             total2.size(), mpi::datatype<real_t>(),
+////             MPI_SUM, dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+////     total2 = mean2;
+////
+////     // Compute the mean-only L^2 squared for each field using zero-zero modes
+////     if (dgrid.has_zero_zero_modes()) {
+////         for (size_t k = 0; k < state.shape()[0]; ++k) {
+////             Map<const ArrayXc> u_mn(&state[k][0][0][0], grid.N.y());
+////             mean2.col(k) = u_mn.cwiseAbs2();
+////         }
+////         mean2 *= grid.L.x() * grid.L.z();
+////     }
+////
+////     // Broadcast total2 and mean2 values to all processors
+////     SUZERAIN_MPICHKR(MPI_Bcast(buf.data(),
+////             buf.size(), mpi::datatype<real_t>(),
+////             dgrid.rank_zero_zero_modes, MPI_COMM_WORLD));
+////
+////     // Obtain fluctuating2 = total2 - mean2 and pack the return structure
+////     std::vector<field_extrema_xz> retval(state.shape()[0]);
+////     for (size_t k = 0; k < retval.size(); ++k) {
+////         retval[k].mean        = mean2.col(k).sqrt();
+////         retval[k].fluctuating = (total2.col(k) - mean2.col(k)).sqrt();
+////     }
+////
+////     return retval;
+//// }
+
+} // end namespace suzerain
---
+++ suzerain/extrema.hpp
@@ -0,0 +1,99 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2008-2014 Rhys Ulerich
+// Copyright (C) 2012-2014 The PECOS Development Team
+// Please see http://pecos.ices.utexas.edu for more information on PECOS.
+//
+// This file is part of Suzerain.
+//
+// Suzerain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Suzerain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Suzerain.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------
+
+#ifndef SUZERAIN_EXTREMA_HPP
+#define SUZERAIN_EXTREMA_HPP
+
+/** @file
+ * Compute pointwise extrema of quantities on distributed data.
+ */
+
+#include <suzerain/common.hpp>
+
+#include <suzerain/state_fwd.hpp>
+
+namespace suzerain {
+
+class bsplineop;
+class specification_grid;
+class pencil_grid;
+
+
+/**
+ * Holds information on pointwise extrema within a scalar field.
+ */
+struct field_extrema_xyz {
+
+    real_t min;  /**< Global pointwise minimum. */
+    real_t max;  /**< Global pointwise maximum. */
+
+};
+
+/**
+ * Compute pointwise extrema for all given scalar fields.
+ */
+std::vector<field_extrema_xyz>
+compute_field_extrema_xyz(
+        const contiguous_state<4,complex_t> &state,
+        const specification_grid& grid,
+        const pencil_grid& dgrid,
+        const bsplineop& gop);
+
+/**
+ * Holds information on the pointwise extrema within a scalar field as a
+ * function of wall-normal $y$ collocation point.
+ */
+struct field_extrema_xz {
+
+    ArrayXr min;  /**< Pointwise minimum versus collocation point index. */
+    ArrayXr max;  /**< Pointwise maximum versus collocation point index. */
+
+};
+
+/**
+ * Compute pointwise extrema of all given scalar fields from \c state
+ * represented as Fourier coefficients in $x$ and $z$ \e and B-spline
+ * coefficients in $y$ at the B-spline collocation points defining the mass
+ * operator in \c cop.
+ */
+std::vector<field_extrema_xz>
+compute_field_extrema_xz(
+        const contiguous_state<4,complex_t> &state,
+        const specification_grid& grid,
+        const pencil_grid& dgrid,
+        const bsplineop& cop);
+
+/**
+ * Compute pointwise extrema of all given scalar fields from \c state
+ * represented as Fourier coefficients in $x$ and $z$ \e but collocation point
+ * values in $y$.
+ */
+std::vector<field_extrema_xz>
+compute_field_extrema_xz(
+        const contiguous_state<4,complex_t> &state,
+        const specification_grid& grid,
+        const pencil_grid& dgrid);
+
+} // end namespace suzerain
+
+#endif // SUZERAIN_EXTREMA_HPP
