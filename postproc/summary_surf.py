e154d437060a038645501a7e8a61703baa281301
Rhys Ulerich <rhys@ices.utexas.edu>
2014-03-17 14:46:28 +0000

    suzerain: #3076 add summary_surf.py
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@44365 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ postproc/summary_surf.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""Usage: summary_surf.py [-t TITLE] H5SUMMARY DATASET...
+Produce surface plots for each named DATASET in the file H5SUMMARY.
+
+Options:
+    -t TITLE Set the specified plot title.
+
+File H5SUMMARY must have 1D /y and /t datasets to provide the meshgrid.
+Each DATASET must have extents matching the /y and /t dataset sizes.
+"""
+import getopt
+import h5py
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+import numpy as np
+import sys
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def surf(y, t, ytplane):
+    fig = plt.figure()
+    ax  = fig.add_subplot(111, projection='3d')
+    Y,T = np.meshgrid(y, t)
+    ax.plot_surface(Y, T, ytplane)
+    return ax
+
+def main(argv=None):
+
+    # Permit interactive use
+    if argv is None:
+        argv = sys.argv
+
+    # Parse and check incoming command line arguments
+    title = None
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "ht", ["help"])
+        except getopt.error, msg:
+            raise Usage(msg)
+        for o, a in opts:
+            if o == "-t":
+                title = a
+            elif o in ("-h", "--help"):
+                print __doc__
+                return 0
+        if len(args) < 2:
+            print >>sys.stderr, "Too few arguments.  See --help."
+            return 1
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        return 2
+
+    # Push interactive mode off (in case we get used from IPython)
+    was_interactive = plt.isinteractive()
+    plt.interactive(False)
+
+    # Open the HDF5 file and plot each dataset in turn
+    h5file = h5py.File(args[0], 'r')
+    y      = h5file['/y']
+    t      = h5file['/t']
+    for dataset in args[1:]:
+        ax = surf(y, t, h5file[dataset])
+        ax.set_xlabel('Wall-normal distance')
+        ax.set_ylabel('Simulation time')
+        if title:
+            ax.set_title(title)
+    plt.show()
+
+    # Pop interactive mode
+    plt.interactive(was_interactive)
+
+if __name__ == "__main__":
+    sys.exit(main())
