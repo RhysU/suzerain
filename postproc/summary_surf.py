8f933c8d51a65c4ebc00a0bcd51cc3c9dd6e1a28
Rhys Ulerich <rhys@ices.utexas.edu>
2014-03-18 15:30:19 +0000

    suzerain: permit downsampling
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@44378 0bd85f0b-c151-43b9-a1e9-27231104551f

--- postproc/summary_surf.py
+++ postproc/summary_surf.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-"""Usage: summary_surf.py [-t TITLE] H5SUMMARY DATASET...
+"""Usage: summary_surf.py [OPTIONS...] H5SUMMARY DATASET...
 Produce surface plots for each named DATASET in the file H5SUMMARY.
 
 Options:
-    -t TITLE Set the specified plot title.
+    -c CSTRIDE    Downsample by providing cstride=CSTRIDE to plot_surface
+    -h            Display this help message and exit
+    -l LINEWIDTH  Set a non-zero linewidth=LINEWIDTH to plot_surface.
+    -r RSTRIDE    Downsample by providing rstride=RSTRIDE to plot_surface
+    -t TITLE      Set the specified plot title
 
 File H5SUMMARY must have 1D /y and /t datasets to provide the meshgrid.
 Each DATASET must have extents matching the /y and /t dataset sizes.
@@ -40,18 +44,27 @@ def main(argv=None):
         argv = sys.argv
 
     # Parse and check incoming command line arguments
-    title = None
+    title     = None
+    cstride   = 1
+    rstride   = 1
+    linewidth = 0
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "ht:", ["help"])
+            opts, args = getopt.getopt(argv[1:], "c:hl:r:t:", ["help"])
         except getopt.error, msg:
             raise Usage(msg)
         for o, a in opts:
-            if o == "-t":
-                title = a
+            if   o == "-c":
+                cstride = int(a)
             elif o in ("-h", "--help"):
                 print __doc__
                 return 0
+            elif o == "-l":
+                linewidth = int(a)
+            elif o == "-r":
+                rstride = int(a)
+            elif o == "-t":
+                title = a
         if len(args) < 2:
             print >>sys.stderr, "Too few arguments.  See --help."
             return 1
@@ -69,15 +82,17 @@ def main(argv=None):
     t      = h5file['/t']
     for dataset in args[1:]:
         fig, ax, surf, cbar = surface(y, t, h5file[dataset], {
-                                       'cstride'  : 1
-                                     , 'rstride'  : 1
+                                       'cstride'  : cstride
+                                     , 'rstride'  : rstride
                                      , 'cmap'     : matplotlib.cm.RdBu_r
-                                     , 'linewidth': 0
+                                     , 'linewidth': linewidth
                                      })
         ax.set_xlabel('Wall-normal distance')
         ax.set_ylabel('Simulation time')
         if title:
             ax.set_title(title)
+
+    plt.tight_layout()
     plt.show()
 
     # Pop interactive mode
