8de9d01acb88fd47f3ac86ceecbc8fde8af4836d
Rhys Ulerich <rhys@ices.utexas.edu>
2014-03-19 21:03:56 +0000

    suzerain: closing #3078, removing FIXME
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@44414 0bd85f0b-c151-43b9-a1e9-27231104551f

--- largo/largo/largo_bl_spatiotemporal_consistent.f90
+++ largo/largo/largo_bl_spatiotemporal_consistent.f90
@@ -621,7 +621,6 @@ contains
       auxp%ddx_base_p   = ddx_base(auxp%ip_base)
     end if
 
-!!$ FIXME Handle appropriately per Redmine ticket #3078
     auxp%src_base_rho  = src_base(irho )
     auxp%src_base_rhoU = src_base(irhoU)
     auxp%src_base_rhoV = src_base(irhoV)
