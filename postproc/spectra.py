suzerain: #2998 substract mean contribution to one-dimensional spectra and fix scaling
git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@44883 0bd85f0b-c151-43b9-a1e9-27231104551f

postproc/spectra.py
@@ -59,15 +59,21 @@ def process(kx, kz, Lx, Lz, Nx, Nz, Rkx, Rkz, bar, y, Ns, sn, **kwargs):
     # Compute spectra from Rkx using conjugate-symmetry of Rkx
     Ekx = Rkx.copy()
     Ekx[:, 1:, :] += np.conj(Rkx[:, 1:,:])
+    # Substract mean contribution
+    Ekx[:,0,:] -= bar_pairs
     assert np.max(np.abs(np.imag(Ekx))) == 0
-    Ekx = np.real(Ekx)
+    # Convert to real and multiply by FT to FS factor
+    Ekx = np.real(Ekx) * Lx / (2*np.pi)
 
     # Compute spectra from Rkz by adding reflected negative wavenumbers
     Ekz            = Rkz[:, 0:(Nz/2+1), :].copy()
     Ekz[:, 1:, :] += Rkz[:, -1:-(Nz/2+1):-1,:]
+    # Substract mean contribution
+    Ekz[:,0,:] -= bar_pairs
     # FIXME: find better criterion for tolerance of assert line below
     assert np.max(np.abs(np.imag(Ekz))) < 1E-10 #np.finfo(Ekz.dtype).eps
-    Ekz = np.real(Ekz)
+    # Convert to real and multiply by FT to FS factor
+    Ekz = np.real(Ekz) * Lz / (2*np.pi)
 
     # Helper to shorten the following few statements
     def scalarpairs(d):