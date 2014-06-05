30152754e26c9084700d410e420ec8f79f786720
Rhys Ulerich <rhys@ices.utexas.edu>
2014-06-05 00:49:45 +0000

    suzerain: add time extraction script
    git-svn-id: https://svn.ices.utexas.edu/repos/pecos/suzerain/trunk@45301 0bd85f0b-c151-43b9-a1e9-27231104551f

---
+++ postproc/datatimes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Produce a tab-separated table of filepath vs sorted times
+# from HDF5 files containing time as a scalar in "/t"
+set -eu
+
+# Used to accumulate intermediate output
+tmp1="$(mktemp)"
+tmp2="$(mktemp)"
+trap "rm -f \"$tmp1\" \"$tmp2\"" EXIT
+
+# Extract time information from each file.
+# h5dump could not make this much more awkward, could it?
+# Especially since this does some initial newline hinky-ness.
+while [[ $# -gt 0 ]]; do
+    if [[ -f "$1" ]]; then
+        h5dump -m %24.16f -o >(cat>>"$tmp1") -yd "/t" "$1" 1>/dev/null
+        echo "$1" >> "$tmp2"
+    else
+        echo "$0: bad filepath \'$1\'" 1>&2
+    fi
+    shift
+done
+
+# Sort the combined results to standard output,
+# computing time differences between adjacent rows
+# to facilitate looking for "errant" samples
+paste <(tail -n +2 "$tmp1") "$tmp2"    \
+    | sort -t$'\t' -n                  \
+    | awk -F$'\t' '{$3=$1-t;print;t=$1}'
